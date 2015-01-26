e191a7f9e216f3024b66ee8600beff36ea5bd3ce
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-26 13:13:41 +0000

    beheerpagina dashboard

--- icclear/application/icclear/views/admin/beheer.php
+++ icclear/application/icclear/views/admin/beheer.php
@@ -5,7 +5,7 @@
 </div>
 
 <div class="row">
-    <div class="col-md-2 col-sm-2">
+    <div class="col-md-2">
         
         
         <div class="row">
@@ -28,11 +28,8 @@
     </div>
 
     
-    <div class="col-md-10 col-sm-10">
+    <div class="col-md-10">
 
-
-        <div class="row">
-            <div class="col-md-12 col-sm-12">
 
 
                 <div class="panel panel-default" role="tablist">
@@ -88,10 +85,6 @@
 
                 </div>
 
-
-
-            </div>
-        </div>
         
         
     </div>
