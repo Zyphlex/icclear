d8f6b5b4e1518fda4ba7c69d561d03c361d36d9f
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-26 13:19:52 +0000

    beheerpagina dashboard

--- icclear/application/icclear/views/admin/beheer.php
+++ icclear/application/icclear/views/admin/beheer.php
@@ -5,88 +5,75 @@
 </div>
 
 <div class="row">
-    <div class="col-md-2">
-        
-        
-        <div class="row">
+    <div class="col-md-3">        
             <div class="panel panel-default">
                 <div class="panel-body">
                     <h4>Algemeen</h4>
-                    
+
                     <ul class="">
-                    <li><a href="#">Hotels Beheren</a></li>
-                    <li><a href="#">Landen Beheren</a></li>
-                    <li><a href="#">Gebruikers Beheren</a></li>
-                    <li><a href="#">F.A.Q. Beheren</a></li>
+                        <li><a href="#">Hotels Beheren</a></li>
+                        <li><a href="#">Landen Beheren</a></li>
+                        <li><a href="#">Gebruikers Beheren</a></li>
+                        <li><a href="#">F.A.Q. Beheren</a></li>
                     </ul>
-                    
+
                 </div>
-            </div>  
+            </div> 
+    </div>
+
+
+    <div class="col-md-9">
+        <div class="panel panel-default" role="tablist">
+
+            <div class="panel-heading" role="tab" id="collapseListGroupHeading1">
+                <h4 class="panel-title">
+                    <span class="pointer" href="#collapseListGroup1" aria-expanded="false" aria-controls="collapseListGroup1">
+                        Conferentie selecteren om te beheren
+                    </span>
+                </h4>
+            </div>
+            
+            <div id="collapseListGroup1"  role="tabpanel" aria-labelledby="collapseListGroupHeading1">
+                <div class="panel-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Row</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email</th>
+                                <th>Beheer</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>John</td>
+                                <td>Carter</td>
+                                <td>johncarter@mail.com</td>
+                                <td><span class="btn btn-default">Beheren</span></td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Peter</td>
+                                <td>Parker</td>
+                                <td>peterparker@mail.com</td>
+                                <td><span class="btn btn-default">Beheren</span></td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>John</td>
+                                <td>Rambo</td>
+                                <td>johnrambo@mail.com</td>
+                                <td><span class="btn btn-default">Beheren</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
         </div>
-        
-        
     </div>
 
-    
-    <div class="col-md-10">
 
-
-
-                <div class="panel panel-default" role="tablist">
-
-                        <div class="panel-heading" role="tab" id="collapseListGroupHeading1">
-                            <h4 class="panel-title">
-                                <span class="pointer" href="#collapseListGroup1" aria-expanded="false" aria-controls="collapseListGroup1">
-                                    Conferentie selecteren om te beheren
-                                </span>
-                            </h4>
-                        </div>
-                        <div id="collapseListGroup1"  role="tabpanel" aria-labelledby="collapseListGroupHeading1">
-                            
-                            <div class="panel-body">
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th>Row</th>
-                                        <th>First Name</th>
-                                        <th>Last Name</th>
-                                        <th>Email</th>
-                                        <th>Beheer</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>John</td>
-                                        <td>Carter</td>
-                                        <td>johncarter@mail.com</td>
-                                        <td><span class="btn btn-default">Beheren</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Peter</td>
-                                        <td>Parker</td>
-                                        <td>peterparker@mail.com</td>
-                                        <td><span class="btn btn-default">Beheren</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>John</td>
-                                        <td>Rambo</td>
-                                        <td>johnrambo@mail.com</td>
-                                        <td><span class="btn btn-default">Beheren</span></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                            </div>
-                            
-                        </div>
-
-                </div>
-
-        
-        
-    </div>
-
-    
 </div>
