8992f8365900d3d3c984cd36be8fa44345986398
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-26 14:55:56 +0000

    beheerpagina dashboard

--- icclear/application/icclear/views/admin/beheer.php
+++ icclear/application/icclear/views/admin/beheer.php
@@ -56,7 +56,7 @@
                                 <td><?php echo $conferentie->naam ?></td>
                                 <td><?php echo $conferentie->stad ?></td>
                                 <td><?php echo $conferentie->beginDatum ?></td>
-                                <td><a class="btn btn-default">Beheren</a></td>
+                                <td><?php echo anchor('dashboard', 'Beheren'); ?><a class="btn btn-default">Beheren</a></td>
                             </tr>
                             <?php $teller++; } ?>
                         </tbody>
