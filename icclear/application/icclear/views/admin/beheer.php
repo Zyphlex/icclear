48773866956822547b06cf93f2956e753be33f4a
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-26 20:57:58 +0000

    admin master

--- icclear/application/icclear/views/admin/beheer.php
+++ icclear/application/icclear/views/admin/beheer.php
@@ -1,29 +1,3 @@
-<div class="row">
-    <div class="col-md-12">
-        <h1>Admin Dashboard</h1>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-md-2">        
-            <div class="panel panel-default">
-                <div class="panel-heading" role="tab" id="collapseListGroupHeading1">
-                    <h4 class="panel-title">
-                        Algemeen
-                    </h4>
-                </div>
-                
-                <div class="panel-body">
-                    <ul class="nav nav-pills nav-stacked">
-                        <li><a href="#">Hotels</a></li>
-                        <li><a href="#">Landen</a></li>
-                        <li><a href="#">Gebruikers</a></li>
-                        <li><a href="#">F.A.Q.</a></li>
-                    </ul>
-
-                </div>
-            </div> 
-    </div>
 
 
     <div class="col-md-10">
@@ -66,6 +40,3 @@
 
         </div>
     </div>
-
-
-</div>
