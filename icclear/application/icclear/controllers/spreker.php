0e8931acc456933a1e848a4b32987bb414de170f
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-26 14:02:39 +0000

    nav

---
+++ icclear/application/icclear/controllers/spreker.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Spreker extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        $data['user']  = $this->authex->getUserInfo();        
+        $data['title'] = 'IC Clear - Sprekers'; 
+        $data['active'] = 'spreker';
+
+        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'spreker/overzicht', 'footer' => 'main_footer');
+        $this->template->load('main_master', $partials, $data); 
+    }
+    
+  
+    
+}
+
+?>
