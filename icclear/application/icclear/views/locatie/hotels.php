2d9b95c67631dd2a6a0330c5bfecaab8bb00b7fd
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 08:15:09 +0000

    Anchor

--- icclear/application/icclear/views/locatie/hotels.php
+++ icclear/application/icclear/views/locatie/hotels.php
@@ -11,7 +11,7 @@
         echo '<div class="panel-body">';
         echo '<p>' . $hotel->straat . ' ' . $hotel->nummer . ' </p>';   
         echo '<p>' . $hotel->postcode . ' ' . $hotel->gemeente . ' </p>';   
-        echo '<p>' . $hotel->website . ' </p>';   
+        echo '<p>' . anchor($hotel->website,$hotel->website) . ' </p>';   
         echo '</div>';
         echo '</div>  ';
         echo '</div>';
