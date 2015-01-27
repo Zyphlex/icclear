bc1dcac6e7f077aa9747bce747c5d3a7f6295e43
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 18:52:04 +0000

    Formulier conferentie toevoegen

--- icclear/application/icclear/views/admin/conferentie/toevoegen.php
+++ icclear/application/icclear/views/admin/conferentie/toevoegen.php
@@ -15,13 +15,15 @@
             
         </div>
 
-        <div class="row">            
+        <div class="row">     
+            <p>
             <div class="col-md-2">
                 <label for="begindatum">Begin datum:</label>
             </div>            
             <div class="col-md-4">
                 <input type="date" id="begindatum" name="begindatum" class="form-control"/>
             </div>
+        </p>
             
             <div class="col-md-6  border-left">
                 <label for="einddatum">Eind datum:</label>
