252ffa4b6a4bb5f653d8e143317a9f6e6fa38991
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 14:28:24 +0000

    buttons

--- icclear/application/icclear/views/admin/dashboard.php
+++ icclear/application/icclear/views/admin/dashboard.php
@@ -3,12 +3,12 @@
         
         <h1>Conferentie <?php echo $conferentie->naam ?> beheren.</h1>     
         
-        <p>DASHBOARD</p>
+        <h4>DASHBOARD</h4>
         <?php echo $conferentie->id ?>
         <?php echo $conferentie->naam ?>
         
         <form action="">
-            <input type="button" value="Emails"/>
-            <input type="button" value="Aankondigingen" />
+            <input type="button" value="Emails" class="btn btn-default"/>
+            <input type="button" value="Aankondigingen" class="btn btn-default"/>
         </form>
     </div>
