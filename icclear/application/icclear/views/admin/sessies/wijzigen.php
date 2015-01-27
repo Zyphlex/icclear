5e102abb553a097a97cb4d0a4afa44ee2c2e527c
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 13:04:51 +0000

    sessie wijzigen.php

--- icclear/application/icclear/views/admin/sessies/wijzigen.php
+++ icclear/application/icclear/views/admin/sessies/wijzigen.php
@@ -2,18 +2,25 @@
 
     <h1>Sessies wijzigen.</h1>   
     
-                <label for="onderwerp">Onderwerp:</label>    
-            
-                <input type="text" name="onderwerp" value="<?php $sessie->onderwerp ?>" id="onderwerp" size="30" class="form-control"  />    
-            
+    <?php
+        $attributes = array('name' => 'myform');
+        echo form_open('home/aanmelden', $attributes);
+    ?>
+        <label for="onderwerp">Onderwerp:</label>    
+        <input type="text" name="onderwerp" value="<?php $sessie->onderwerp ?>" id="onderwerp" size="30" class="form-control"  />    
 
-                <label for="omschrijving">Omschrijving:</label>    
-           
-                <input type="text" name="omschrijving" value="<?php $sessie->omschrijving ?>" id="omschrijving" size="30" class="form-control"  />    
-           
-                <input type="hidden" name="sessieId" id="sessieId" value="<?php $sessie->id ?>"/>
-                
-                <inut type="submit" value="Opslaan"/>
-                <a href="javascript:history.go(-1);" class="btn btn-warning">Annuleren</a>
+
+        <label for="omschrijving">Omschrijving:</label>    
+
+        <input type="text" name="omschrijving" value="<?php $sessie->omschrijving ?>" id="omschrijving" size="30" class="form-control"  />    
+
+        <input type="hidden" name="sessieId" id="sessieId" value="<?php $sessie->id ?>"/>
+
+        <p>
+            <input type="submit" value="Opslaan"/>
+            <a href="javascript:history.go(-1);" class="btn btn-danger">Annuleren</a>
+        </p>
+
+    <?php echo form_close(); ?>
     
 </div>
