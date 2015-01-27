ba4f9262c55756062665fc48dfb8ed91567fa2f5
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 09:10:35 +0000

    admin dashboard

--- icclear/application/icclear/views/admin/dashboard.php
+++ icclear/application/icclear/views/admin/dashboard.php
@@ -1,10 +1,10 @@
 
     <div class="col-md-10">       
         
-            <h1>Conferentie <?php echo $conferentie->naam ?> beheren.</h1>     
+        <h1>Conferentie <?php echo $conferentie->naam ?> beheren.</h1>     
         
         <p>DASHBOARD</p>
-        <?php echo $gebruiker->id ?>
-        <?php echo $gebruiker->naam ?>
+        <?php echo $conferentie->id ?>
+        <?php echo $conferentie->naam ?>
 
     </div>
