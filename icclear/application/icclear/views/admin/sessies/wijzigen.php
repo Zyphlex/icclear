51ce6982f36a6a1309abf485bcdb7ad3b54e38ef
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 12:51:18 +0000

    registreer

--- icclear/application/icclear/views/admin/sessies/wijzigen.php
+++ icclear/application/icclear/views/admin/sessies/wijzigen.php
@@ -3,29 +3,29 @@
     <h1>Sessies wijzigen.</h1>   
 
     <div class="row">
-            <div class="col-md-4">      
+            <div class="col-md-3">      
                 <label for="naam">Gebruikersnaam:</label>    
             </div>
 
-            <div class="col-md-8">
+            <div class="col-md-4">
                 <input type="text" name="naam" value="" id="naam" size="30" class="form-control"  />    
             </div>
         </div>
 
         <div class="row">     
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <label for="password">Wachtwoord:</label>    
             </div>
 
-            <div class="col-md-8">
+            <div class="col-md-4">
                 <input type="password" name="password" value="" id="password" size="30" class="form-control"  />    
             </div>
         </div>
         
         <div class="row">     
-            <div class="col-md-4"></div>
+            <div class="col-md-3"></div>
 
-            <div class="col-md-8">  
+            <div class="col-md-4">  
                 <a href="<?php echo base_url(); ?>index.php/logon/vergeten" data-dismiss="modal" data-toggle="modal" data-target="#myModal2">Wachtwoord vergeten?</a>
             </div>
         </div>
