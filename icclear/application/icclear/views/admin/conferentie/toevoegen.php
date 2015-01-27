67dec869baea01090e1f5d6a5838dd7f74959544
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 17:46:28 +0000

    Formulier conferentie toevoegen

--- icclear/application/icclear/views/admin/conferentie/toevoegen.php
+++ icclear/application/icclear/views/admin/conferentie/toevoegen.php
@@ -1,10 +1,17 @@
 
         <div class="col-md-10">
             <h1>Conferentie wijzigen</h1>
-            <div class="panel">
-                <div id="body">
-                    <p>gegevens dingen</p>
-                </div>
+            
+            
+            <p>gegevens dingen</p>
+                
+            <form action="conferentie/opslaan" method="post">
+                
+                <label for="naam">Naam:</label>
+                <input class="form-control" type="text" id="naam" name="naam"/>
+                
+                
+            </form>
+            
 
-            </div>
         </div>
