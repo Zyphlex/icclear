89864adfbe7b7cae43f524fd902a5efad5d1710b
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 13:57:47 +0000

    sessie wijzigen.php

--- icclear/application/icclear/views/admin/sessies/wijzigen.php
+++ icclear/application/icclear/views/admin/sessies/wijzigen.php
@@ -7,6 +7,7 @@
         echo form_open('sessie/opslaan', $attributes);
     ?>
         
+    <div class="row">
         <div class="col-md-3">
         <label for="onderwerp">Onderwerp:</label>   
         </div>
@@ -14,7 +15,9 @@
         <div class="col-md-3">
         <input type="text" name="onderwerp" value="<?php echo $sessie->onderwerp ?>" id="onderwerp" size="30" class="form-control"  />    
         </div>
+    </div>
     
+    <div class="row">
         <div class="col-md-3">
         <label for="omschrijving">Omschrijving:</label>    
         </div>
@@ -22,21 +25,23 @@
         <div class="col-md-3">
         <input type="text" name="omschrijving" value="<?php echo $sessie->omschrijving ?>" id="omschrijving" size="30" class="form-control"  />    
         </div>
+    </div>
     
+    <div class="row">
         <div class="col-md-3">
             <p>Spreker:<?php echo $sessie->id ?></p>
         </div>
     </div>
     
     <input type="hidden" name="sessieId" id="sessieId" value="<?php echo $sessie->id ?>"/>
-    
+    <div class="row">
         <div class="col-md-3">
             <p>
                 <input type="submit" value="Opslaan" class="btn btn-default"/>
                 <a href="javascript:history.go(-1);" class="btn btn-danger">Annuleren</a>
             </p>
         </div>
-    
+    </div>
         
     <?php echo form_close(); ?>
     
