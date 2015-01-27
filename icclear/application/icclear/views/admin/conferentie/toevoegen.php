784677d60529f8f91a07d94990d1e1ab4123093c
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 18:48:09 +0000

    Formulier conferentie toevoegen

--- icclear/application/icclear/views/admin/conferentie/toevoegen.php
+++ icclear/application/icclear/views/admin/conferentie/toevoegen.php
@@ -26,19 +26,28 @@
         </div>
         
         <div class="row">            
-            <div class="col-md-4">
-                <label for="begindatum">Begin datum:</label>
-                <input type="date" id="begindatum" name="begindatum" class="form-control"/>
+            <div class="col-md-6">
+                <label for="land">Begin datum:</label>
+                <select id="land" name="land" class="form-controls">
+                    <option></option>
+                </select>
+            </div>            
+        </div>
+        
+        <div class="row"> 
+            <div class="col-md-4  border-left">
+                <label for="stad">Stad</label>
+                <input type="text" id="stad" name="stad" class="form-control"/>
             </div>
             
             <div class="col-md-4  border-left">
-                <label for="einddatum">Eind datum:</label>
-                <input type="date" id="einddatum" name="einddatum" class="form-control"/>
+                <label for="straat">Straat</label>
+                <input type="text" id="straat" name="straat" class="form-control"/>
             </div>
             
             <div class="col-md-4  border-left">
-                <label for="einddatum">Eind datum:</label>
-                <input type="date" id="einddatum" name="einddatum" class="form-control"/>
+                <label for="nr">Nr.</label>
+                <input type="number" id="nr" name="nr" class="form-control"/>
             </div>
         </div>
         
