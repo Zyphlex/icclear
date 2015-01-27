a54f3ab5081ec02c70ccefcb537efda52fd64504
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 09:34:26 +0000

    sessie overzicht

---
+++ icclear/application/icclear/models/zaal_model.php
@@ -0,0 +1,17 @@
+<?php
+
+class Zaal_model extends CI_Model {
+
+    function __construct() {
+        parent::__construct();
+    }
+    
+    function get($id) {
+        $this->db->where('id', $id);
+        $query = $this->db->get('zaal');
+        return $query->row();
+    }
+    
+
+}
+?>
