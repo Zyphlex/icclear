6f24180652ca8f3c05b93eaa907231953fdfe456
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 13:08:02 +0000

    in divs

--- icclear/application/icclear/views/planning/overzicht.php
+++ icclear/application/icclear/views/planning/overzicht.php
@@ -1,46 +1,51 @@
-<?php
-$id = 0;
-foreach ($sessies as $dag) {
-    if ($dag->conferentiedagId != $id) {
-        echo "\n" . '<h1>Dag ' . $dag->conferentiedagId . '</h1>' . "\n";
-        ?>
-        <table class = "table">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Onderwerp</th>
-                    <th>Omschrijving</th>
-                    <th>Beginuur</th>
-                    <th>Einduur</th>
-                    <th>Zaal</th>
-                    <th>Spreker</th>
-                </tr>
-            </thead>  
-            <tbody>
-                <?php
-                $id = $dag->conferentiedagId;
-                ?>                
-                <?php
-                $teller = 1;
-                foreach ($sessies as $sessie) {
-                    if ($dag->conferentiedagId == $sessie->conferentiedagId) {
-                        echo '<tr>' . "\n";
-                        echo '<td>' . $teller . '</td>' . "\n";
-                        echo '<td>' . $sessie->onderwerp . '</td>' . "\n";
-                        echo '<td>' . $sessie->omschrijving . '</td>' . "\n";
-                        echo '<td>' . $sessie->planning->beginUur . '</td>' . "\n";
-                        echo '<td>' . $sessie->planning->eindUur . '</td>' . "\n";
-                        echo '<td>' . $sessie->zaal->naam . '</td>' . "\n";
-                        echo '<td>' . $sessie->spreker->voornaam . ' ' . $sessie->spreker->familienaam . '</td>' . "\n";
-                        echo '</tr>' . "\n";
-                        $teller++;
-                    }
-                }
-                ?>
-            </tbody>
-        </table>                        
+<div class='row'>
+    <div class='col-md-12'>
         <?php
-    }
-}
-?>
+        $id = 0;
+        foreach ($sessies as $dag) {
+            if ($dag->conferentiedagId != $id) {
+                echo "\n" . '<h1>Dag ' . $dag->conferentiedagId . '</h1>' . "\n";
+                ?>
+                <table class = "table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Onderwerp</th>
+                            <th>Omschrijving</th>
+                            <th>Beginuur</th>
+                            <th>Einduur</th>
+                            <th>Zaal</th>
+                            <th>Spreker</th>
+                        </tr>
+                    </thead>  
+                    <tbody>
+                        <?php
+                        $id = $dag->conferentiedagId;
+                        ?>                
+                        <?php
+                        $teller = 1;
+                        foreach ($sessies as $sessie) {
+                            if ($dag->conferentiedagId == $sessie->conferentiedagId) {
+                                echo '<tr>' . "\n";
+                                echo '<td>' . $teller . '</td>' . "\n";
+                                echo '<td>' . $sessie->onderwerp . '</td>' . "\n";
+                                echo '<td>' . $sessie->omschrijving . '</td>' . "\n";
+                                echo '<td>' . $sessie->planning->beginUur . '</td>' . "\n";
+                                echo '<td>' . $sessie->planning->eindUur . '</td>' . "\n";
+                                echo '<td>' . $sessie->zaal->naam . '</td>' . "\n";
+                                echo '<td>' . $sessie->spreker->voornaam . ' ' . $sessie->spreker->familienaam . '</td>' . "\n";
+                                echo '</tr>' . "\n";
+                                $teller++;
+                            }
+                        }
+                        ?>
+                    </tbody>
+                </table>                        
+                <?php
+            }
+        }
+        ?>
 
+
+    </div>    
+</div>
