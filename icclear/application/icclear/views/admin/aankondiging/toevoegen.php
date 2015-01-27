992423aaba129bab5208e889d4e0fc6f3a62f288
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 18:35:11 +0000

    aankondigingen

--- icclear/application/icclear/views/admin/aankondiging/toevoegen.php
+++ icclear/application/icclear/views/admin/aankondiging/toevoegen.php
@@ -32,7 +32,7 @@
         </div>      
     </div>
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-lg-offset-4">
             <input type="submit" value="Toevoegen" class="btn btn-default"/>
     <?php echo anchor('admin/', 'Annuleer', 'class="btn btn-default"'); ?>
         </div>
