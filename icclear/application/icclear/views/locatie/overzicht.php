5bf0a119ac2e6f7ecc64c832dc86d11fe03e4fa2
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 08:19:04 +0000

    locatie verfijnd

--- icclear/application/icclear/views/locatie/overzicht.php
+++ icclear/application/icclear/views/locatie/overzicht.php
@@ -1,31 +1,37 @@
 
-        <h2>Conferentiegebouwen</h2>
-        <div class="row">  
-            <?php
-            foreach ($gebouwen as $gebouw) {
-                echo '<div class="col-md-6">';
-                echo '<div class="panel panel-default">';
-                echo '<div class="panel-body">';
-                echo '<h4>' . $gebouw->naam . ' </h4>';
-                echo '   <p>Extra text</p> ';
-                echo '</div>';
-                echo '</div>  ';
-                echo '</div>';
-            }
-            ?>
-        </div>
-        <h2>Routes</h2>
-        <div class="row">  
-            <?php
-            foreach ($routes as $route) {
-                echo '<div class="col-md-6">';
-                echo '<div class="panel panel-default">';
-                echo '<div class="panel-body">';
-                echo '<h4>' . $route->beschrijving . ' </h4>';
-                echo '   <p>Extra text</p> ';
-                echo '</div>';
-                echo '</div>  ';
-                echo '</div>';
-            }
-            ?>
-        </div>  
+<h2>Conferentiegebouwen</h2>
+<div class="row">  
+    <?php
+    foreach ($gebouwen as $gebouw) {
+        echo '<div class="col-md-6">';
+        echo '<div class="panel panel-default">';
+        echo '<div class = "panel-heading">';
+        echo '<h3 class = "panel-title">' . $gebouw->naam . '</h3>';
+        echo '</div>';
+        echo '<div class="panel-body">';
+        echo '<p>' . $gebouw->naam . ' </p>';
+        echo '<p>' . $gebouw->straat . ' ' . $gebouw->nummer . ' </p>';
+        echo '<p>' . $gebouw->postcode . ' ' . $gebouw->gemeente . ' </p>';
+        echo '</div>';
+        echo '</div>  ';
+        echo '</div>';
+    }
+    ?>
+</div>
+<h2>Routes</h2>
+<div class="row">  
+    <?php
+    foreach ($routes as $route) {
+        echo '<div class="col-md-6">';
+        echo '<div class="panel panel-default">';
+        echo '<div class = "panel-heading">';
+        echo '<h3 class = "panel-title">' . $route->vertrekPunt . '</h3>';
+        echo '</div>';
+        echo '<div class="panel-body">';
+        echo '<p>' . $route->beschrijving . ' </p>';        
+        echo '</div>';
+        echo '</div>  ';
+        echo '</div>';
+    }
+    ?>
+</div>  
