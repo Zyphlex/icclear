b9251e74c31bcee530006bfda6f14906ebe3e4b3
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 19:18:46 +0000

    update

--- icclear/application/icclear/views/admin/gebruiker/wijzigen.php
+++ icclear/application/icclear/views/admin/gebruiker/wijzigen.php
@@ -97,8 +97,6 @@
                                             </span>
                                         </div>
                                         
-                                        }
-                                        ?>
                                     </div>
                                 </div>
                             </div>
