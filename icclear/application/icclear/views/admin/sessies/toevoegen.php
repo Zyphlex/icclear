cdb4277547c03a247db96f5ab1664dd3381573a1
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 12:26:15 +0000

    sessies

--- icclear/application/icclear/views/admin/sessies/toevoegen.php
+++ icclear/application/icclear/views/admin/sessies/toevoegen.php
@@ -1,13 +1,9 @@
-<div class="col-md-10">
+<div class="col-md-10">    
+    
+    <h1>Sessies toevoegen.</h1>   
+    
     <div class="panel panel-default" role="tablist">
 
-        <div class="panel-heading" role="tab" id="collapseListGroupHeading1">
-            <h4 class="panel-title">
-                <span href="#collapseListGroup1" aria-expanded="false" aria-controls="collapseListGroup1">
-                    Sessie toevoegen.
-                </span>
-            </h4>
-        </div>
 
         <div id="collapseListGroup1"  role="tabpanel" aria-labelledby="collapseListGroupHeading1">
             <div class="panel-body">
