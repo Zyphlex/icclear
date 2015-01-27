9cb3307d6711de941846596fbb845b7f5e51c320
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 21:25:35 +0000

    emails

---
+++ icclear/application/icclear/controllers/email.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Email extends CI_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        $data['user']  = $this->authex->getUserInfo();
+        
+        $data['title'] = 'IC Clear - Emails.';         
+        $data['active'] = 'admin';
+                
+        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'email/opstellen', 'footer' => 'main_footer');
+        $this->template->load('main_master', $partials, $data);
+    }
+
+    // TEST
+}
---
+++ icclear/application/icclear/views/admin/email/opstellen.php
@@ -0,0 +1,43 @@
+<div class="col-md-10">    
+    <?php
+    $attributes = array('name' => 'myform', 'id' => 'myform');
+    echo form_open('email/verzend', $attributes);
+    ?>             
+    <div class="row">
+        <div class="col-md-6">  
+            <div class="row">
+                <div class="col-md-4">   
+                    <label for="titel">
+                        Titel:
+                    </label>
+                </div>
+                <div class="col-md-8">   
+                    <input type="text" name="titel" id="titel" class="form-control" required="required">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">   
+                    <label for="inhoud">
+                        Inhoud:
+                    </label>
+                </div>
+                <div class="col-md-8">   
+                    <textarea name="inhoud" id="inhoud" class="form-control" required="required" rows="4" cols="50">
+                        
+                    </textarea>
+                </div>
+            </div>
+            <input type="hidden" name="gebruiker" value="<?php echo $user->id ?>"/>
+            <input type="hidden" name="conferentie" value="<?php echo $conferentieId; ?>"/>
+        </div>      
+    </div>
+    <div class="row" style="margin-top: 15px;">
+        <div class="col-md-1"> </div>
+        <div class="col-md-3">
+            <input type="submit" value="Toevoegen" class="btn btn-default"/>
+    <?php echo anchor('admin/', 'Annuleer', 'class="btn btn-default"'); ?>
+        </div>
+    </div>    
+    <?php echo form_close(); ?>                                     
+</div>
+
