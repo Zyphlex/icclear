10dadb7ada147a34e005a6aa160c13ce685a0768
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 08:13:05 +0000

    hotelpanels

--- icclear/application/icclear/views/locatie/hotels.php
+++ icclear/application/icclear/views/locatie/hotels.php
@@ -1,17 +1,18 @@
 
-        <h2>Hotels</h2>
-        <div class="row">  
-            <?php
-            foreach ($hotels as $hotel) {
-                echo '<div class="col-md-4">';
-                echo '<div class="panel panel-default">';
-                echo '<div class="panel-body">';
-                echo '<h4>' . $hotel->naam . ' </h4>';
-                echo '   <p>Extra text</p> ';
-                echo '</div>';
-                echo '</div>  ';
-                echo '</div>';
-            }
-            ?>
-        </div>
-    
+<h2>Hotels</h2>
+<div class="row">  
+    <?php
+    foreach ($hotels as $hotel) {
+        echo '<div class="col-md-6">';
+        echo '<div class="panel panel-info">';
+        echo '<div class = "panel-heading">';
+        echo '<h3 class = "panel-title">' . $hotel->naam   . '</h3>';
+        echo '</div>';
+        echo '<div class="panel-body">';
+        echo '<h2>' . $hotel->straat . ' ' . $hotel->nummer . ' </h2>';        
+        echo '</div>';
+        echo '</div>  ';
+        echo '</div>';
+    }
+    ?>
+</div>
