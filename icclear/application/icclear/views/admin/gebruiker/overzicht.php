8b2b87b0fd554a6db39824d1d4f9b9cec06177d6
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 12:00:21 +0000

    beheer knop

--- icclear/application/icclear/views/admin/gebruiker/overzicht.php
+++ icclear/application/icclear/views/admin/gebruiker/overzicht.php
@@ -1,12 +1,15 @@
 
 
     <div class="col-md-10">
+        
+        <h1>Gebruikers beheren.</h1>  
+        
         <div class="panel panel-default" role="tablist">
 
             <div class="panel-heading" role="tab" id="collapseListGroupHeading1">
                 <h4 class="panel-title">
                     <span href="#collapseListGroup1" aria-expanded="false" aria-controls="collapseListGroup1">
-                        Gebruiker selecteren om te beheren
+                        Gebruikers
                     </span>
                 </h4>
             </div>
@@ -16,7 +19,6 @@
                     <table class="table">
                         <thead>
                             <tr>
-                                <th>#</th>
                                 <th>Familienaam</th>
                                 <th>Voornaam</th>
                                 <th>Email</th>
@@ -29,7 +31,8 @@
                                 <td><?php echo $gebruiker->familienaam ?></td>
                                 <td><?php echo $gebruiker->voornaam ?></td>
                                 <td><?php echo $gebruiker->emailadres ?></td>
-                                <td><?php echo anchor('gebruiker/wijzig/' . $gebruiker->id, 'Wijzigen','class="btn btn-default"'); ?><?php echo anchor('gebruiker/verwijder/' . $gebruiker->id, 'Verwijderen','class="btn btn-default"'); ?></td>
+                                <td><?php echo anchor('gebruiker/wijzig/' . $gebruiker->id, 'Wijzigen','class="btn btn-default"'); ?>
+                                    <?php echo anchor('gebruiker/verwijder/' . $gebruiker->id, 'Verwijderen','class="btn btn-default"'); ?></td>
                             </tr>
                             <?php } ?>
                         </tbody>
@@ -40,4 +43,7 @@
             </div>
 
         </div>
+        
+        <?php echo anchor('gebruiker/toevoegen', 'Nieuwe gebruiker toevoegen','class="btn btn-default"'); ?> 
+        
     </div>
