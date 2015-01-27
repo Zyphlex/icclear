cb254a24eb15782c4e3db551b1a94f21b41fe6dc
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 18:55:59 +0000

    Formulier conferentie toevoegen

--- icclear/application/icclear/views/admin/conferentie/toevoegen.php
+++ icclear/application/icclear/views/admin/conferentie/toevoegen.php
@@ -8,7 +8,7 @@
         <div class="row">            
             <div class="col-md-6">
                 <p>
-                    <label for="naam">Naam:</label>
+                    <label for="naam" class="form-label">Naam:</label>
                     <input class="form-control" type="text" id="naam" name="naam"/>
                 </p>
             </div>
@@ -17,22 +17,19 @@
 
         <div class="row">            
             <div class="col-md-6 form-group">
-                <label for="begindatum">Begin datum:</label>
-                
+                <label for="begindatum" class="form-label">Begin datum:</label>                
                 <input type="date" id="begindatum" name="begindatum" class="form-control"/>
             </div>
             
             <div class="col-md-2  border-left">
-                <label for="einddatum">Eind datum:</label>
-            </div>            
-            <div class="col-md-4">
+                <label for="einddatum" class="form-label">Eind datum:</label>
                 <input type="date" id="einddatum" name="einddatum" class="form-control"/>
             </div>
         </div>
         
         <div class="row">            
             <div class="col-md-6">
-                <label for="land">Stad</label>
+                <label for="land" class="form-label">Stad</label>
                 <select id="land" name="land" class="form-control">
                     <option>België</option>
                 </select>
@@ -41,17 +38,17 @@
         
         <div class="row"> 
             <div class="col-md-4">
-                <label for="stad">Stad</label>
+                <label for="stad" class="form-label">Stad</label>
                 <input type="text" id="stad" name="stad" class="form-control"/>
             </div>
             
             <div class="col-md-4 border-left">
-                <label for="straat">Straat</label>
+                <label for="straat" class="form-label">Straat</label>
                 <input type="text" id="straat" name="straat" class="form-control"/>
             </div>
             
             <div class="col-md-4">
-                <label for="nr">Nr.</label>
+                <label for="nr" class="form-label">Nr.</label>
                 <input type="number" id="nr" name="nr" class="form-control"/>
             </div>
         </div>
