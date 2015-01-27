146584445c59ceb264265bba75ddb9d13862eca9
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 21:28:00 +0000

    emails

--- icclear/application/icclear/controllers/email.php
+++ icclear/application/icclear/controllers/email.php
@@ -7,6 +7,17 @@ class Email extends CI_Controller {
     
     public function __construct() {
         parent::__construct();
+        $this->load->helper(array('form', 'url'));
+        if (!$this->authex->loggedIn()) {
+            redirect('logon/aanmelden');
+            //voorlopig
+        } else {
+            $user = $this->authex->getUserInfo();
+            if ($user->typeId < 3) {
+                redirect('logon/aanmelden');
+                //voorlopig
+            }
+        }
     }
 
     public function index() {
@@ -14,9 +25,9 @@ class Email extends CI_Controller {
         
         $data['title'] = 'IC Clear - Emails.';         
         $data['active'] = 'admin';
-                
-        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'admin/email/opstellen', 'footer' => 'main_footer');
-        $this->template->load('main_master', $partials, $data);
+                        
+        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/email/opstellen', 'footer' => 'main_footer');
+        $this->template->load('admin_master', $partials, $data);
     }
 
     // TEST
