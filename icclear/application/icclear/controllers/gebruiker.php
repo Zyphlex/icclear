db0ad9c93d4afe8bdf4d60a85c8e0b6dec4e8a5e
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 18:22:16 +0000

    update

--- icclear/application/icclear/controllers/gebruiker.php
+++ icclear/application/icclear/controllers/gebruiker.php
@@ -7,7 +7,7 @@ class Gebruiker extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->helper(array('form', 'url', 'helper'));
+        $this->load->helper(array('form', 'url', 'notation'));
         if (!$this->authex->loggedIn()) {
             redirect('logon/aanmelden');
             //voorlopig
