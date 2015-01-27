27e942aec0d5772013959a6da0738e7316f55a42
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 15:22:58 +0000

    conferentie beheren

---
+++ icclear/application/icclear/controllers/conferentie.php
@@ -0,0 +1,58 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Conferentie extends CI_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper(array('form', 'url'));
+        if (!$this->authex->loggedIn()) {
+            redirect('logon/aanmelden');
+            //voorlopig
+        } else {
+            $user = $this->authex->getUserInfo();
+            if ($user->typeId < 3) {
+                redirect('logon/aanmelden');
+                //voorlopig
+            }
+        }
+    }
+    
+    public function index() {
+        $data['user']  = $this->authex->getUserInfo();
+        
+        $data['conferentieId'] = $this->session->userdata('conferentieId');
+        $data['title'] = 'IC Clear - Beheer';        
+        $data['active'] = 'admin';        
+                
+        $this->load->model('conferentie_model');
+        $data['conferenties'] = $this->conferentie_model->getAll();
+
+        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/beheer', 'footer' => 'main_footer');
+        $this->template->load('admin_master', $partials, $data);
+    }
+    
+
+     public function toevoegen() {
+        $this->session->set_userdata('conferentieId', $id);  
+        
+        $data['user']  = $this->authex->getUserInfo();    
+        
+        $data['conferentieId'] = $this->session->userdata('conferentieId');
+        $data['title'] = 'IC Clear - ';         
+        $data['active'] = 'admin';                
+        
+        $this->load->model('conferentie_model');
+        $data['conferentie'] = $this->conferentie_model->get($id);
+        
+        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/dashboard', 'footer' => 'main_footer');
+        $this->template->load('admin_master', $partials, $data);
+    }
+    
+    
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ icclear/application/icclear/views/admin/aankondiging/overzicht.php
@@ -0,0 +1,8 @@
+
+    <div class="col-md-10">
+       <?php
+            echo "ID: $conferentieId";
+        ?> 
+    </div>
+
+        
