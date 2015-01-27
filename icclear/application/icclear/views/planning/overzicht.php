a2089ae53c756ab0bda354aa7887391eb4a8bdef
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 10:35:38 +0000

    gebruikerIdSpreker

--- icclear/application/icclear/views/planning/overzicht.php
+++ icclear/application/icclear/views/planning/overzicht.php
@@ -1,30 +1,33 @@
-<table class="table">
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Onderwerp</th>
-            <th>Omschrijving</th>
-            <th>Beginuur</th>
-            <th>Einduur</th>
-            <th>Zaal</th>
-            <th>Spreker</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        $teller = 1;
-        foreach ($sessies as $sessie) {
-            echo '<tr>';
-            echo '<td>' . $teller . '</td>';
-            echo '<td>' . $sessie->onderwerp . '</td>';
-            echo '<td>' . $sessie->omschrijving . '</td>';
-            echo '<td>' . $sessie->planning->beginUur . '</td>';
-            echo '<td>' . $sessie->planning->eindUur . '</td>'; 
-            echo '<td>' . $sessie->zaal->naam .'</td>';
-            echo '<td>' . $sessie->spreker->voornaam . ' ' . $sessie->spreker->familienaam  .'</td>';
-            echo '</tr>';
-            $teller++;
-        }
-        ?>                                         
-    </tbody>
-</table>
+<?php
+foreach ($sessies as $sessie) {
+    echo '<h1>' . $sessie->conferentiedagId. '</h1>';
+    echo '<table class = "table">';
+    echo '<thead>';
+    echo '<tr>';
+    echo '<th>#</th>';
+    echo '<th>Onderwerp</th>';
+    echo '<th>Omschrijving</th>';
+    echo '<th>Beginuur</th>';
+    echo '<th>Einduur</th>';
+    echo '<th>Zaal</th>';
+    echo '<th>Spreker</th>';
+    echo '</tr>';
+    echo '</thead>';
+    echo '<tbody>';
+    $teller = 1;
+    foreach ($sessies as $sessie) {
+        echo '<tr>';
+        echo '<td>' . $teller . '</td>';
+        echo '<td>' . $sessie->onderwerp . '</td>';
+        echo '<td>' . $sessie->omschrijving . '</td>';
+        echo '<td>' . $sessie->planning->beginUur . '</td>';
+        echo '<td>' . $sessie->planning->eindUur . '</td>';
+        echo '<td>' . $sessie->zaal->naam . '</td>';
+        echo '<td>' . $sessie->spreker->voornaam . ' ' . $sessie->spreker->familienaam . '</td>';
+        echo '</tr>';
+        $teller++;
+    }                                             
+echo '</tbody>';
+echo '</table>';
+}
+?>
