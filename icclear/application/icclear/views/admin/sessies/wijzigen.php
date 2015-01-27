4b0bcb96391215289b027c0d946f68b98ef0ef28
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-27 13:56:47 +0000

    sessie wijzigen.php

--- icclear/application/icclear/views/admin/sessies/wijzigen.php
+++ icclear/application/icclear/views/admin/sessies/wijzigen.php
@@ -6,20 +6,43 @@
         $attributes = array('name' => 'myform');
         echo form_open('sessie/opslaan', $attributes);
     ?>
-        <label for="onderwerp">Onderwerp:</label>    
+        
+    <div class="row">
+        <div class="col-md-3">
+        <label for="onderwerp">Onderwerp:</label>   
+        </div>
+        
+        <div class="col-md-3">
         <input type="text" name="onderwerp" value="<?php echo $sessie->onderwerp ?>" id="onderwerp" size="30" class="form-control"  />    
-
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-3">
         <label for="omschrijving">Omschrijving:</label>    
-
+        </div>
+        
+        <div class="col-md-3">
         <input type="text" name="omschrijving" value="<?php echo $sessie->omschrijving ?>" id="omschrijving" size="30" class="form-control"  />    
-
-        <input type="hidden" name="sessieId" id="sessieId" value="<?php echo $sessie->id ?>"/>
-
-        <p>
-            <input type="submit" value="Opslaan" class="btn btn-default"/>
-            <a href="javascript:history.go(-1);" class="btn btn-danger">Annuleren</a>
-        </p>
-
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-3">
+            <p>Spreker:<?php echo $sessie->id ?></p>
+        </div>
+    </div>
+    
+    <input type="hidden" name="sessieId" id="sessieId" value="<?php echo $sessie->id ?>"/>
+    <div class="row">
+        <div class="col-md-3">
+            <p>
+                <input type="submit" value="Opslaan" class="btn btn-default"/>
+                <a href="javascript:history.go(-1);" class="btn btn-danger">Annuleren</a>
+            </p>
+        </div>
+    </div>
+        
     <?php echo form_close(); ?>
     
 </div>
