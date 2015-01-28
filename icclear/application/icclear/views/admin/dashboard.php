3e20f9787b37b2e36327220aefb3052a07c5fdff
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 15:09:12 +0000

    id weg

--- icclear/application/icclear/views/admin/dashboard.php
+++ icclear/application/icclear/views/admin/dashboard.php
@@ -1,10 +1,9 @@
 
 <div class="col-md-10">       
 
-    <h1>Conferentie <?php echo $conferentie ?> beheren.</h1>     
+    <h1>Conferentie <?php echo $conferentie ?> beheren</h1>     
 
-    <h4>DASHBOARD</h4>
-    <h5>ID: <?php echo $conferentieId ?></h5>
+    <h4>DASHBOARD</h4>    
     <h5>NAAM: <?php echo $conferentie ?></h5>                 
     <?php echo anchor('email/', 'Emails', 'class="btn btn-default"'); ?>
     <?php echo anchor('aankondiging/', 'Aankondigingen', 'class="btn btn-default"'); ?>
