c7775e885e2f811ec95ad3ccebb254801bc7368c
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 20:23:06 +0000

    faq controller getAll()

--- icclear/application/icclear/controllers/faq.php
+++ icclear/application/icclear/controllers/faq.php
@@ -16,7 +16,7 @@ class Faq extends CI_Controller {
         $data['active'] = 'admin';
 
         $this->load->model('faq_model');
-        $data['vragen'] = $this->faq_model->getFaq();
+        $data['vragen'] = $this->faq_model->getAll();
 
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'faq/faq', 'footer' => 'main_footer');
         $this->template->load('main_master', $partials, $data);
