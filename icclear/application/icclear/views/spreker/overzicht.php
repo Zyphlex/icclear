d9432f4225ed1a42944eb6b1601f050cbc15e3df
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 17:24:32 +0000

    palceholder sprekers

--- icclear/application/icclear/views/spreker/overzicht.php
+++ icclear/application/icclear/views/spreker/overzicht.php
@@ -6,15 +6,13 @@
 
 <div class="row"> 
     <div class="col-md-12">
-        <?php foreach ($sprekers as $spreker) { ?>
-            <div class="col-md-4">
+        <?php foreach ($sprekers as $spreker) { ?>   
+        <div class="col-md-4">
                 <div class="panel panel-default">
                     <div class="row">
                         <div class="panel-body">
                             <div class="col-md-5">
-                                <div class="thumbnail">
-                                    <img src="http://dummyimage.com/60x60/d4c1d4/ffffff&text=PLACEHOLDER" alt="placeholder image" title="placeholder">
-                                </div>
+                                <img src="http://dummyimage.com/60x60/d4c1d4/ffffff&text=PLACEHOLDER" alt="placeholder image" title="placeholder">
                             </div>
                             <div class="col-md-7">
                                <h4><?php $spreker->voornaam . ' ' . $spreker->familienaam ?></h4>
