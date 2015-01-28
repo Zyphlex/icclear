00a0598a681da6d591c42a058af59adbb04c512d
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 18:11:19 +0000

    sprekers conferentie

--- icclear/application/icclear/views/spreker/overzicht.php
+++ icclear/application/icclear/views/spreker/overzicht.php
@@ -15,10 +15,8 @@
                                 <img src="http://dummyimage.com/110x110/d4c1d4/ffffff&text=PLACEHOLDER" alt="placeholder image" title="placeholder">
                             </div>
                             <div class="col-md-7">
-                                <h4><?php echo$spreker->voornaam . ' ' . $spreker->familienaam ?></h4>                                
-                                <?php foreach ($spreker->sessie as $sessie) { ?>   
-                                <p><?php echo $sessie->onderwerp ?></p>
-                                <?php } ?>
+                                <h4><?php echo$spreker->voornaam . ' ' . $spreker->familienaam ?></h4>   
+                                <p><?php echo $spreker->sessie->onderwerp ?></p>
                             </div>                        
                         </div>      
                     </div> 
