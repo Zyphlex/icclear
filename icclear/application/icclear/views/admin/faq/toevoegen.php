896a73bdd9783f32e7c87ba8df2174f3ac4916e8
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 22:09:30 +0000

    FAQ beheer css

--- icclear/application/icclear/views/admin/faq/toevoegen.php
+++ icclear/application/icclear/views/admin/faq/toevoegen.php
@@ -1,45 +1,46 @@
-<div class="row">
-    <div class="col-md-12">
-        <h1>Geselecteerde FAQ toevoegen</h1>
+<div class="col-md-10">  
+
+    <div class="row">
+        <div class="col-md-12">
+            <h1>FAQ toevoegen</h1>
+        </div>
     </div>
-</div>
-<div class="col-md-10">                      
-                <?php
-                $attributes = array('name' => 'myform', 'id' => 'myform');
-                echo form_open('faqbeheer/insert', $attributes);                
-                ?>           
-                    <div class="row">
-                        <div class="col-md-6">  
+    <?php
+    $attributes = array('name' => 'myform', 'id' => 'myform');
+    echo form_open('faqbeheer/insert', $attributes);
+    ?>           
+    <div class="row">
+        <div class="col-md-6">  
 
-                            <div class="row">
-                                <div class="col-md-4">   
-                                    <?php echo form_label('Vraag:', 'vraag'); ?>
-                                </div>
+            <div class="row">
+                <div class="col-md-4">   
+                    <?php echo form_label('Vraag:', 'vraag'); ?>
+                </div>
 
-                                <div class="col-md-8">
-                                    <?php echo form_input(array('name' => 'vraag', 'id' => 'vraag', 'class' => 'form-control', 'required' => 'required')); ?>
-                                </div>
-                            </div>
+                <div class="col-md-8">
+                    <?php echo form_input(array('name' => 'vraag', 'id' => 'vraag', 'class' => 'form-control', 'required' => 'required')); ?>
+                </div>
+            </div>
 
-                            <div class="row">
-                                <div class="col-md-4">   
-                                    <label for="antwoord">
-                                        Antwoord:
-                                    </label>
-                                </div>
+            <div class="row">
+                <div class="col-md-4">   
+                    <label for="antwoord">
+                        Antwoord:
+                    </label>
+                </div>
 
-                                <div class="col-md-8">   
-                                    <textarea name="antwoord" id="antwoord" class="form-control" required="required" rows="3"></textarea>
-                                </div>
-                            </div>
-                            
-                        </div>
+                <div class="col-md-8">   
+                    <textarea name="antwoord" id="antwoord" class="form-control" required="required" rows="3"></textarea>
+                </div>
+            </div>
 
-                    </div>
+        </div>
 
-                <?php echo anchor('faq/beheer', 'Annuleer', 'class="btn btn-default"'); ?>
-                <?php echo form_submit('opslaan', 'Opslaan', 'class="btn btn-primary"'); ?>
+    </div>
 
-                <?php echo form_close(); ?>
-           
+    <?php echo anchor('faq/beheer', 'Annuleer', 'class="btn btn-default"'); ?>
+    <?php echo form_submit('opslaan', 'Opslaan', 'class="btn btn-primary"'); ?>
+
+    <?php echo form_close(); ?>
+
 </div>
--- icclear/application/icclear/views/admin/faq/wijzigen.php
+++ icclear/application/icclear/views/admin/faq/wijzigen.php
@@ -1,52 +1,54 @@
-<div class="row">
-    <div class="col-md-12">
-        <h1>Geselecteerde FAQ wijzigen</h1>
+<div class="col-md-10">  
+
+    <div class="row">
+        <div class="col-md-12">
+            <h1>FAQ wijzigen</h1>
+        </div>
     </div>
+
+    <?php
+    $attributes = array('name' => 'myform', 'id' => 'myform');
+    echo form_open('faqbeheer/update', $attributes);
+    echo form_hidden('id', $id);
+    ?>
+
+    <div class="row">
+        <div class="col-md-6">  
+
+            <div class="row">
+                <div class="col-md-4">   
+                    <?php echo form_label('Vraag:', 'vraag'); ?>
+                </div>
+
+                <div class="col-md-8">
+                    <?php echo form_input(array('name' => 'vraag', 'id' => 'vraag', 'value' => $vraag->vraag, 'class' => 'form-control', 'required' => 'required')); ?>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4">   
+                    <label for="antwoord">
+                        Antwoord:
+                    </label>
+                </div>
+
+                <div class="col-md-8">   
+                    <textarea name="antwoord" id="antwoord" class="form-control" required="required" rows="3"><?php echo $vraag->antwoord; ?></textarea>
+                </div>
+            </div>
+
+        </div>
+        
+    </div>
+
+    <?php echo anchor('faq/beheer', 'Annuleer', 'class="btn btn-default"'); ?>
+    <?php echo form_submit('opslaan', 'Opslaan', 'class="btn btn-primary"'); ?>
+
+    <?php echo form_close(); ?>
+
 </div>
 
-                <?php
-                $attributes = array('name' => 'myform', 'id' => 'myform');
-                echo form_open('faqbeheer/update', $attributes);
-                echo form_hidden('id', $id);
-                ?>
-                       
-                    <div class="row">
-                        <div class="col-md-6">  
 
-                            <div class="row">
-                                <div class="col-md-4">   
-                                    <?php echo form_label('Vraag:', 'vraag'); ?>
-                                </div>
 
-                                <div class="col-md-8">
-                                    <?php echo form_input(array('name' => 'vraag', 'id' => 'vraag', 'value' => $vraag->vraag, 'class' => 'form-control', 'required' => 'required')); ?>
-                                </div>
-                            </div>
 
-                            <div class="row">
-                                <div class="col-md-4">   
-                                    <label for="antwoord">
-                                        Antwoord:
-                                    </label>
-                                </div>
 
-                                <div class="col-md-8">   
-                                    <textarea name="antwoord" id="antwoord" class="form-control" required="required" rows="3"><?php echo $vraag->antwoord; ?></textarea>
-                                </div>
-                            </div>
-                            
-                        </div>
-
-          <?php echo anchor('faq/beheer', 'Annuleer', 'class="btn btn-default"'); ?>
-                <?php echo form_submit('opslaan', 'Opslaan', 'class="btn btn-primary"'); ?>
-
-                <?php echo form_close(); ?>
-
-                    </div>
-
-            
-
-              
-           
-
-       
