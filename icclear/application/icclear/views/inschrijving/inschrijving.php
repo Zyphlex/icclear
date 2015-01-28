3d501a8df283209666dbacfbd28b4561b8801491
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 19:36:27 +0000

    inschrijven view aangepast

--- icclear/application/icclear/views/inschrijving/inschrijving.php
+++ icclear/application/icclear/views/inschrijving/inschrijving.php
@@ -1,46 +1,30 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html"; charset="UTF-8">
-        <title><?php echo $title ?></title>
-        <style>
-            #waarschuwing{
-                color:red;
-                font-weight: bold;
-                font-size: 16px;
-            }
-        </style>
-    </head>
-    <body>
-        <?php
-        $attributes = array('name' => 'myform');
-        echo form_open('inschrijven/inschrijven', $attributes);
-        ?>        
-        
+<?php
+$attributes = array('name' => 'myform');
+echo form_open('inschrijven/inschrijven', $attributes);
+?>        
+<div class="row">
+    <div class="col-md-12">
         <h1>Inschrijven conferentie</h1>
-        
-        <?php
-        if ($user == null)
-        {
-            echo '<p id="waarschuwing">Opgelet! U moet aangemeld zijn om dit formulier in te dienen.</p>';
-        }
-        ?>
-        
+
+        <?php if ($user == null) { ?>            
+            <div class="alert alert-danger alert-dismissible" role="alert">Opgelet! U moet aangemeld zijn om dit formulier in te dienen.</p>
+        <?php } ?>
+
         <p>Door dit formulier in te vullen schrijft u zichzelf in voor de volgende conferentie en de geselecteerde opties.</p>
-        
-        <p>
-            De conferentie vindt plaats van <?php echo toDDMMYYYY($conferentie->beginDatum); ?> tot en met <?php echo toDDMMYYYY($conferentie->eindDatum); ?>.
+        <p> 
+            De conferentie vindt plaats van <?php echo toDDMMYYYY($conferentie->beginDatum); ?> 
+            tot en met <?php echo toDDMMYYYY($conferentie->eindDatum); ?>.
         </p>
-        
         <p>U gaat akkoord met de voorwaarden en prijzen.</p>
-        
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col-md-12">
+        <form action="inschrijven">
         <h3>Selecteer formule</h3>
-        
+
         <table class="table">
             <tr>
                 <th>Formule</th>
@@ -48,24 +32,21 @@ and open the template in the editor.
                 <th>Early-bird korting</th>
                 <td></td>
             </tr>
-        <?php
-            foreach ($conferentieOnderdelen as $conferentieOnderdeel)
-            {
-                if ($conferentieOnderdeel->conferentie->statusId == '2')
-                {
+            <?php
+            foreach ($conferentieOnderdelen as $conferentieOnderdeel) {
+                if ($conferentieOnderdeel->conferentie->statusId == '2') {
                     echo '<tr><td>' . $conferentieOnderdeel->omschrijving . '</td>';
                     echo '<td>' . $conferentieOnderdeel->prijs . '</td>';
                     echo '<td>' . $conferentieOnderdeel->korting . ' %</td>';
                     echo '<td><input type="radio" name="formule" value="' . $conferentieOnderdeel->id . '"/></td></tr>';
-                    
                 }
             }
-        ?>
-            
+            ?>
+
         </table>
-        
+
         <h3>Extra activiteiten</h3>
-        
+
         <table class="table">
             <tr>
                 <th>Activiteit</th>
@@ -73,46 +54,42 @@ and open the template in the editor.
                 <th></th>
                 <th>Aantal Personen (max. 10)</th>
             </tr>
-        <?php
-            foreach ($activiteiten as $activiteit)
-            {
-                if ($activiteit->conferentie->statusId == '2')
-                {
+            <?php
+            foreach ($activiteiten as $activiteit) {
+                if ($activiteit->conferentie->statusId == '2') {
                     echo '<tr><td>' . $activiteit->naam . '</td>';
                     echo '<td>' . $activiteit->prijs . '</td>';
                     ?>
-            <td><input type="checkbox" name="aanwezig<?php echo $activiteit->id;?>" id="aanwezig<?php echo $activiteit->id;?>" value ="<?php echo $activiteit->id;?>"/></td>
-            <td><input type="number" name="aantalPersonen" id="aantalPersonen" max="10"/></td>
+                    <td><input type="checkbox" name="aanwezig<?php echo $activiteit->id; ?>" id="aanwezig<?php echo $activiteit->id; ?>" value ="<?php echo $activiteit->id; ?>"/></td>
+                    <td><input type="number" name="aantalPersonen" id="aantalPersonen" max="10"/></td>
                     <?php
-                    
                 }
             }
-        ?>
-            
+            ?>
+
         </table>
-        
+
         <h3>Betaling</h3>
-        
+
         <input type="checkbox" name="factuur" id="factuur"/>
         <label>Ja, ik wil een factuur ontvangen.</label>
-        
+
         <br/>
         <br/>
-        
+
         <input type="radio" name="methode" value="VISA"/><label>VISA</label><br/>
         <input type="radio" name="methode" value="Mastercard"/><label>Mastercard</label><br/>
         <input type="radio" name="methode" value="PayPal"/><label>PayPal</label><br/>
         <input type="radio" name="methode" value="Overschrijving"/><label>Overschrijving</label><br/>
-        
-        <a href="<?php echo base_url();?>icclear.php/home" class="btn btn-default">Annuleren</a>
-        
-        <?php
-        if ($user == null) {
-            echo '<input type="button" value="U bent niet aangemeld" class="btn btn-default disabled"/>';
-        } else {
-            echo '<input type="submit" value="Bevestigen en betalen" class="btn btn-default"/>';
-        }
+
+        <a href="<?php echo base_url(); ?>icclear.php/home" class="btn btn-default">Annuleren</a>
+
+        <?php if ($user == null) { ?>
+            <input type="button" value="U bent niet aangemeld" class="btn btn-default disabled"/>
+        <?php } else { ?>
+            <input type="submit" value="Bevestigen en betalen" class="btn btn-default"/>
+        <?php } ?>
         ?>
-        
-    </body>
-</html>
+        </form>
+    </div>
+</div>
