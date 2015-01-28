b3bfb413243360650246475e26aee2741c22701d
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 14:04:11 +0000

    route opmaak

--- icclear/application/icclear/views/locatie/routes.php
+++ icclear/application/icclear/views/locatie/routes.php
@@ -1,3 +1,18 @@
+<script src="https://maps.googleapis.com/maps/api/js"></script>
+<script>
+  function initialize() {
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+      center: new google.maps.LatLng(44.5403, -78.5463),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+  }
+  
+  google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+
 <div class='row'>
     <div class='col-md-12'>
         <h1>Routes</h1>
@@ -5,18 +20,23 @@
 </div>
 
 <div class="row">  
-    <div class="col-md-12">
-        <?php foreach ($routes as $route) { ?>
-            <div class="col-md-6">
-                <div class="panel panel-default">
-                    <div class = "panel-heading">
-                        <h3><?php $route->vertrekPunt ?></h3>
-                    </div>
-                    <div class="panel-body">
-                        <p><?php $route->beschrijving ?></p>        
-                    </div>
-                </div>  
+    <?php foreach ($routes as $route) { ?>
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="panel panel-default">
+                        <div class = "panel-heading">
+                            <h3><?php $route->vertrekPunt ?></h3>
+                        </div>
+                        <div class="panel-body">
+                            <p><?php $route->beschrijving ?></p>        
+                        </div>
+                    </div>  
+                </div>
+                <div class="col-md-5">
+                    <div id="map-canvas"></div>
+                </div>
             </div>
-        <?php } ?>
-    </div>
+        </div>
+    <?php } ?>
 </div>
