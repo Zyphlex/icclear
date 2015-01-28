6b914169f23bf158529c55bd640c233c2c435174
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 21:17:16 +0000

    voorstel view css upload knop

--- icclear/application/icclear/views/spreker/voorstel.php
+++ icclear/application/icclear/views/spreker/voorstel.php
@@ -38,3 +38,47 @@
         <textarea id="biografie" name="biografie" rows="10" class="form-control"></textarea>
     </div>
 </div>
+    
+    
+    
+    
+<div class="row">
+    <div class="col-md-8">
+        <h2>Sessie voorstel</h2>
+        <p>
+            Praesent sed est id leo molestie malesuada. Pellentesque interdum elit eu neque facilisis, eget elementum ante tempor. 
+            Nunc dictum venenatis magna non tincidunt. Vestibulum vitae faucibus odio, et posuere est. Sed ac nisi ex. 
+            Donec porttitor vitae sapien nec laoreet. Maecenas dignissim dignissim justo a maximus.
+        </p>        
+    </div> 
+</div>
+
+<div class="row">
+    <div class="col-md-2">
+        <label for="sessieonderwerp" class="control-label">Onderwerp: </label>
+    </div>
+    <div class="col-md-2 fileUpload btn btn-default">
+        <input id="sessieonderwerp" type="text" name="sessieonderwerp" class="form-control"/>
+    </div>
+</div>
+    
+    <br/>
+    
+<div class="row">
+    <div class="col-md-2">
+        <label for="biografie">Biografie: </label>
+    </div>
+    <div class="col-md-8">
+        <textarea id="biografie" name="biografie" rows="10" class="form-control"></textarea>
+    </div>
+</div>
+    
+<div class="row">
+    <div class="col-md-2">
+        <label for="sessiebijlage" class="control-label">Bijlage: </label>
+    </div>
+    <div class="col-md-2 fileUpload btn btn-default">
+        <span>Bestand selecteren</span>
+        <input id="sessiebijlage" type="file" name="sessiebijlage" class="upload"/>
+    </div>
+</div>
