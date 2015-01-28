44e7345d4d65c0e6ac37071c23aeb59e954104f7
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 13:39:11 +0000

    venue per conf

--- icclear/application/icclear/views/locatie/overzicht.php
+++ icclear/application/icclear/views/locatie/overzicht.php
@@ -8,14 +8,13 @@
         <?php foreach ($gebouwen as $gebouw) { ?>
             <div class="col-md-6">
                 <div class="panel panel-default">
-                    <div class = "panel-heading">
-                        
+                    <div class = "panel-heading">                        
+                        <h3><?php echo $gebouw->gebouw->naam ?></h3>
                     </div>
                     <div class = "panel-body">
                             <div class="thumbnail">
                                 <img src="http://placehold.it/350x150" alt="placeholder image" title="placeholder">
                             </div>
-                        <h3><?php echo $gebouw->gebouw->naam ?></h3>
                         <p><?php echo $gebouw->gebouw->gemeente ?> (<?php echo $gebouw->gebouw->postcode ?>)</p>
                         <p><?php echo $gebouw->gebouw->straat ?> <?php echo $gebouw->gebouw->nummer ?></p>
                     </div>
