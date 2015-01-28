c472923a14e9e918248bce2e008b1ead4b56f960
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 14:19:56 +0000

    ingeschreven gebruikers en google map

--- icclear/application/icclear/views/locatie/routes.php
+++ icclear/application/icclear/views/locatie/routes.php
@@ -19,7 +19,9 @@
                     </div>  
                 </div>
                 <div class="col-md-5">
-                    <div id="map-canvas"></div>
+                    <div>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2502.135651564536!2d4.9664933999999885!3d51.16128850000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c14c00ecab0a95%3A0x804150638f80ce7d!2sKleinhoefstraat%2C+2440+Geel!5e0!3m2!1snl!2sbe!4v1422358483331" width="450" height="295" frameborder="0" style="border:0"></iframe>
+                    </div>
                 </div>
             </div>
         </div>
--- icclear/application/icclear/views/main_master.php
+++ icclear/application/icclear/views/main_master.php
@@ -42,22 +42,6 @@
     
   <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
   <script src="<?php echo base_url() . APPPATH; ?>js/bootstrap.js"></script>  
-  <script src="https://maps.googleapis.com/maps/api/js"></script>
-    <script>
-      function initialize() {
-        var mapCanvas = document.getElementById('map-canvas');
-        var mapOptions = {
-          center: new google.maps.LatLng(44.5403, -78.5463),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions);
-      }
-
-      google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
-
-  
   
 </body>
 </html>
