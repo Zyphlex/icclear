f43e46f1294c2dab37c0271e2750f0f68fced3c6
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 13:32:07 +0000

    venue per conf

--- icclear/application/icclear/views/locatie/overzicht.php
+++ icclear/application/icclear/views/locatie/overzicht.php
@@ -1,16 +1,19 @@
-
-<h2>Conferentiegebouwen</h2>
-<div class="row">  
-    <div class="col-md-12">
+<div class='row'>
+    <div class='col-md-12'>
+        <h1>Conferentie gebouwen</h1>
+    </div>
+</div>
+    
+<div class="col-md-12">
     <?php foreach ($gebouwen as $gebouw) { ?>
         <div class="col-md-6">
             <div class="panel panel-default">
-            <div class = "panel-heading">
-                <h3><?php echo $gebouw->gebouw->naam ?></h3>
-            </div>
+                <div class = "panel-heading">
+                    <h3><?php echo $gebouw->gebouw->naam ?></h3>
+                </div>
                 <div class="panel-body">
-                <p><?php echo $gebouw->gebouw->gemeente ?> (<?php echo $gebouw->gebouw->postcode ?>)</p>
-                <p><?php echo $gebouw->gebouw->straat ?> <?php echo $gebouw->gebouw->nummer ?></p>
+                    <p><?php echo $gebouw->gebouw->gemeente ?> (<?php echo $gebouw->gebouw->postcode ?>)</p>
+                    <p><?php echo $gebouw->gebouw->straat ?> <?php echo $gebouw->gebouw->nummer ?></p>
                 </div>
             </div>  
         </div>
