a4de253755dde256e26ce924fe27073da2dbd4f1
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 21:17:45 +0000

    voorstel view css upload knop

--- icclear/application/icclear/views/main_master.php
+++ icclear/application/icclear/views/main_master.php
@@ -28,6 +28,10 @@
     <script type="text/javascript">
         var site_url = '<?php echo site_url();?>';
 	var img_url = '<?php echo base_url() . APPPATH; ?>';
+        
+    $( "#foto" ).onchange(function() {
+        $( "#bestandsnaam" ).html($(this).value);
+    });
     </script>
 </head>
 
