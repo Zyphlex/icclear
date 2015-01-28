416d1fcc044eac13b77622e265c6ba471d1c829a
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 14:46:33 +0000

    gebruikers

--- icclear/application/icclear/controllers/gebruiker.php
+++ icclear/application/icclear/controllers/gebruiker.php
@@ -128,6 +128,8 @@ class Gebruiker extends CI_Controller {
     public function gebruikersConferentie()
     {
         $data['user'] = $this->authex->getUserInfo();
+        $data['conferentieId'] = $this->session->userdata('conferentieId');
+        
         $data['title'] = 'IC Clear - Beheer';
         $data['active'] = 'admin';
         
