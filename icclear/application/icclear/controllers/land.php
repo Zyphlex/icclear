4ab47f7e598c8235c44c4a8744c482a4b0503cea
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 08:08:21 +0000

    getLand() naar getAll()

---
+++ icclear/application/icclear/controllers/land.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Land extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        $data['user']  = $this->authex->getUserInfo();        
+        $data['title'] = 'IC Clear - Home'; 
+        $data['active'] = 'admin';
+        
+        $this->load->model('land_model');
+        $data['landen'] = $this->land_model->getAll();
+
+        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'land/overzicht', 'footer' => 'main_footer');
+        $this->template->load('main_master', $partials, $data);
+    }       
+
+    
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
