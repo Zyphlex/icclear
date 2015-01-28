a35f37f8f0357bdfa8a6723a76a5f9f6e237e645
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 20:13:51 +0000

    voorstel

--- icclear/application/icclear/views/spreker/voorstel.php
+++ icclear/application/icclear/views/spreker/voorstel.php
@@ -9,13 +9,17 @@
         <h2>Profielfoto en biografie</h2>
     </div> 
 </div>
-<br>
+
+<div class="row">
+    
+</div>
+
 <div class="row">
     <div class="col-md-8">
         <input type="file" name="userfile" size="20" />
 
         <br /><br />
 
-        <input type="submit" value="upload" />
+        <input type="submit" value="Uploaden" />
     </div>
 </div>
