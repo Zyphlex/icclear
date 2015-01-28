8fc45fc6023c90053847ff5efb321aec77ff00d9
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 08:13:12 +0000

    gebruiker nieuw

--- icclear/application/icclear/views/admin/gebruiker/overzicht.php
+++ icclear/application/icclear/views/admin/gebruiker/overzicht.php
@@ -5,15 +5,6 @@
         <h1>Gebruikers beheren.</h1>  
         
         <div class="panel panel-default" role="tablist">
-
-            <div class="panel-heading" role="tab" id="collapseListGroupHeading1">
-                <h4 class="panel-title">
-                    <span href="#collapseListGroup1" aria-expanded="false" aria-controls="collapseListGroup1">
-                        Gebruikers
-                    </span>
-                </h4>
-            </div>
-            
             <div id="collapseListGroup1"  role="tabpanel" aria-labelledby="collapseListGroupHeading1">
                 <div class="panel-body">
                     <table class="table">
