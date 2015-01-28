27338901a49b064d34b745788da64af53c987987
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 13:03:45 +0000

    venue per conf

--- icclear/application/icclear/views/locatie/overzicht.php
+++ icclear/application/icclear/views/locatie/overzicht.php
@@ -6,7 +6,6 @@
         echo '<div class="col-md-6">';
         echo '<div class="panel panel-default">';
         echo '<div class = "panel-heading">';
-        echo '<h3 class = "panel-title">' . $gebouw->naam . '</h3>';
         echo '</div>';
         echo '<div class="panel-body">';        
         echo '<p>' . $gebouw->zaal->gebouwId . ' ' . $gebouw->gebouw->nummer . ' </p>';
