06abf497ca673bb61088b3129c1f1a4fb469b366
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 19:55:44 +0000

    voorstel

---
+++ icclear/application/icclear/views/spreker/voorstel.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <meta http-equiv="Conten-Type" content="text-html; charset=UTF-8">
+        <title>Deel x Oefening y</title>
+    </head>
+    <body>
+        <?php
+        // -------------------------------------------------------------------------------
+        // Deel x Oefening y
+        // -------------------------------------------------------------------------------
+        // Thomas More Kempen 2 TI 3
+        // -------------------------------------------------------------------------------
+        // php pagina: oefeningyy.php
+        //
+        // doel van de oefening:
+        //
+        // -------------------------------------------------------------------------------
+        // auteur: Abderrahmane Ikrou
+        // -------------------------------------------------------------------------------
+        
+        if ($user == null) {
+            echo '<p>je moet aangemeld zijn!</p>';
+        }
+        else
+        {
+            echo '<p>goed zo</p>';
+        }
+        ?>
+        
+        
+    </body>
+</html>
