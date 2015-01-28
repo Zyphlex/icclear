5e2e8783f6edc5d7b1a563abf56394238fce6c1c
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 14:06:36 +0000

    route opmaak

--- icclear/application/icclear/views/locatie/routes.php
+++ icclear/application/icclear/views/locatie/routes.php
@@ -1,18 +1,3 @@
-<script src="https://maps.googleapis.com/maps/api/js"></script>
-<script>
-  function initialize() {
-    var mapCanvas = document.getElementById('map-canvas');
-    var mapOptions = {
-      center: new google.maps.LatLng(44.5403, -78.5463),
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-  }
-  
-  google.maps.event.addDomListener(window, 'load', initialize);
-</script>
-
 <div class='row'>
     <div class='col-md-12'>
         <h1>Routes</h1>
