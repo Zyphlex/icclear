70137926822d70431241d354bd94455e4a08e9d3
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 07:55:22 +0000

    hotels overzicht

---
+++ icclear/application/icclear/views/admin/hotels/overzicht.php
@@ -0,0 +1,48 @@
+
+
+<div class="col-md-10">
+    
+    <h1>Sessies beheren.</h1>   
+    
+<!--    <div class="panel panel-default" role="tablist">
+
+        <div class="panel-heading" role="tab" id="collapseListGroupHeading1">
+            <h4 class="panel-title">
+                <span href="#collapseListGroup1" aria-expanded="false" aria-controls="collapseListGroup1">
+                    Sessies
+                </span>
+            </h4>
+        </div>
+
+        <div id="collapseListGroup1"  role="tabpanel" aria-labelledby="collapseListGroupHeading1">
+            <div class="panel-body">-->
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Naam</th>
+                            <th>Gemeente</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($sessies as $sessie) { ?>
+                            <tr>
+                                <td><?php echo $hotels->naam ?></td>
+                                <td><?php echo $hotels->gemeente ?></td>
+                                <td>
+                                    <?php echo anchor('#', 'Wijzigen','class="btn btn-default"'); ?>
+                                    <?php echo anchor('#', 'Verwijderen','class="btn btn-default"'); ?>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+<!--            </div>
+        </div>
+
+    </div>-->
+    
+    <?php echo anchor('admin/dashboard/' . $conferentieId, 'Annuleren','class="btn btn-default"'); ?>                           
+    
+    
+</div>
