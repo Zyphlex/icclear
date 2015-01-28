7412f48463e38533a56cc2dd1f9d1cf01e6d759b
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 08:16:33 +0000

    getLand() naar getAll()

--- icclear/application/icclear/controllers/land.php
+++ icclear/application/icclear/controllers/land.php
@@ -4,24 +4,40 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class Land extends CI_Controller {
-
+    
     public function __construct() {
         parent::__construct();
+        $this->load->helper(array('form', 'url'));
+        if (!$this->authex->loggedIn()) {
+            redirect('logon/aanmelden');
+            //voorlopig
+        } else {
+            $user = $this->authex->getUserInfo();
+            if ($user->typeId < 3) {
+                redirect('logon/aanmelden');
+                //voorlopig
+            }
+        }
     }
-
+    
     public function index() {
-        $data['user']  = $this->authex->getUserInfo();        
-        $data['title'] = 'IC Clear - Home'; 
-        $data['active'] = 'admin';
+        $data['user']  = $this->authex->getUserInfo();
         
+        $data['conferentieId'] = $this->session->userdata('conferentieId');
+        $data['title'] = 'IC Clear - Beheer';        
+        $data['active'] = 'admin';        
+                
         $this->load->model('land_model');
         $data['landen'] = $this->land_model->getAll();
 
-        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'admin/land/overzicht', 'footer' => 'main_footer');
-        $this->template->load('main_master', $partials, $data);
-    }       
+        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/labd/overzicht', 'footer' => 'main_footer');
+        $this->template->load('admin_master', $partials, $data);
+    }
+    
 
     
+    
+    // TEST
 }
 
 /* End of file welcome.php */
