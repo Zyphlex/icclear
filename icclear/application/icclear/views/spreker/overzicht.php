ac58e299839f11d0cf8142ec133d9a75f4747b98
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 19:59:06 +0000

    voorstel

--- icclear/application/icclear/views/spreker/overzicht.php
+++ icclear/application/icclear/views/spreker/overzicht.php
@@ -12,7 +12,14 @@
             Nullam vulputate imperdiet sem ornare porta. Nunc eget lectus a metus consectetur suscipit in in urna. 
             Quisque vestibulum erat in convallis faucibus. Aliquam sed placerat dolor.
         </p>
-        <p><?php echo anchor('spreker/voorstel', 'Voorstel indienen', 'class="btn btn-default"'); ?></p>
+        <p><?php
+            if ($user == null) {
+
+                echo '<p>Je moet eerst aangemeld zijn om een voorstel te kunnen indienen</p>';
+            } else {
+                echo anchor('spreker/voorstel', 'Voorstel indienen', 'class="btn btn-default"');
+            }
+            ?></p>
     </div>
 </div>
 
@@ -24,22 +31,22 @@
 </div>
 
 <div class="row"> 
-        <?php foreach ($sprekers as $spreker) { ?>   
+<?php foreach ($sprekers as $spreker) { ?>   
         <div class="col-md-4">
-                <div class="panel panel-default">
-                    <div class="row">
-                        <div class="panel-body">
-                            <div class="col-md-5">
-                                <img src="http://dummyimage.com/110x110/d4c1d4/ffffff&text=PLACEHOLDER" alt="placeholder image" title="placeholder">
-                            </div>
-                            <div class="col-md-7">
-                                <h4><?php echo$spreker->voornaam . ' ' . $spreker->familienaam ?></h4> 
-                                <p class="italic">Sessies:</p>
-                                <p><?php echo $spreker->sessie->onderwerp ?></p>
-                            </div>
-                        </div>      
-                    </div> 
-                </div>  
-            </div>
-        <?php } ?>
+            <div class="panel panel-default">
+                <div class="row">
+                    <div class="panel-body">
+                        <div class="col-md-5">
+                            <img src="http://dummyimage.com/110x110/d4c1d4/ffffff&text=PLACEHOLDER" alt="placeholder image" title="placeholder">
+                        </div>
+                        <div class="col-md-7">
+                            <h4><?php echo$spreker->voornaam . ' ' . $spreker->familienaam ?></h4> 
+                            <p class="italic">Sessies:</p>
+                            <p><?php echo $spreker->sessie->onderwerp ?></p>
+                        </div>
+                    </div>      
+                </div> 
+            </div>  
+        </div>
+<?php } ?>
 </div>
