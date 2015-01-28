bf02c959aff3fa5d648d8ba0ae1b6ee5fb20cbd8
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-28 21:56:47 +0000

    panel

--- icclear/application/icclear/views/admin/gebruiker/wijzigen.php
+++ icclear/application/icclear/views/admin/gebruiker/wijzigen.php
@@ -1,88 +1,82 @@
 
 <div class="col-md-10">
-    <div class="panel panel-default" role="tablist">
 
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Gebruiker wijzigen</h1>
-            </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Gebruiker wijzigen</h1>
         </div>
+    </div>
+    <?php
+    $attributes = array('name' => 'myform', 'id' => 'myform');
+    echo form_open('gebruiker/update', $attributes);
+    echo form_hidden('id', $gebruiker->id);
+    ?>
 
-        <div id="collapseListGroup1"  role="tabpanel" aria-labelledby="collapseListGroupHeading1">
-            <div class="panel-body">
-                <?php
-                $attributes = array('name' => 'myform', 'id' => 'myform');
-                echo form_open('gebruiker/update', $attributes);
-                echo form_hidden('id', $gebruiker->id);
-                ?>
+    <div class="row">
+        <div class="col-md-6">  
 
-                <div class="modal-body">
+            <div class="row">
+                <div class="col-md-4">   
+                    <?php echo form_label('Voornnaam:', 'voornaam'); ?>
+                </div>
 
-                    <div class="row">
-                        <div class="col-md-6">  
-
-                            <div class="row">
-                                <div class="col-md-4">   
-                                    <?php echo form_label('Voornnaam:', 'voornaam'); ?>
-                                </div>
-
-                                <div class="col-md-8">
-                                    <?php echo form_input(array('name' => 'voornaam', 'id' => 'field2', 'value' => $gebruiker->voornaam, 'class' => 'form-control', 'required' => 'required')); ?>
-                                </div>
-                            </div>
+                <div class="col-md-8">
+                    <?php echo form_input(array('name' => 'voornaam', 'id' => 'field2', 'value' => $gebruiker->voornaam, 'class' => 'form-control', 'required' => 'required')); ?>
+                </div>
+            </div>
 
 
 
-                            <div class="row">
-                                <div class="col-md-4">   
-                                    <label for="familienaam">
-                                        Familienaam:
-                                    </label>
-                                </div>
+            <div class="row">
+                <div class="col-md-4">   
+                    <label for="familienaam">
+                        Familienaam:
+                    </label>
+                </div>
 
-                                <div class="col-md-8">   
-                                    <input type="text" name="familienaam" value="<?php echo $gebruiker->familienaam; ?>" id="field1" class="form-control" required="required">
-                                </div>
-                            </div>
+                <div class="col-md-8">   
+                    <input type="text" name="familienaam" value="<?php echo $gebruiker->familienaam; ?>" id="field1" class="form-control" required="required">
+                </div>
+            </div>
 
 
 
-                            <div class="row">
-                                <div class="col-md-4">   
-                                    <label for="emailadres">
-                                        Emailadres: 
-                                    </label>
-                                </div>  
+            <div class="row">
+                <div class="col-md-4">   
+                    <label for="emailadres">
+                        Emailadres: 
+                    </label>
+                </div>  
 
-                                <div class="col-md-8">   
-                                    <input type="text" name="emailadres" value="<?php echo $gebruiker->emailadres; ?>" id="email" class="form-control" required="required">      
-                                </div>
-                            </div>
+                <div class="col-md-8">   
+                    <input type="text" name="emailadres" value="<?php echo $gebruiker->emailadres; ?>" id="email" class="form-control" required="required">      
+                </div>
+            </div>
 
-                            <div class="row">
-                                <div class="col-md-4">   
-                                    <label for="geboortedatum">
-                                        Geboortedatum:
-                                    </label>
-                                </div>
+            <div class="row">
+                <div class="col-md-4">   
+                    <label for="geboortedatum">
+                        Geboortedatum:
+                    </label>
+                </div>
 
-                                <div class="col-md-8">   
-                                    <input type="date" class="form-control" value="<?php echo $gebruiker->geboortedatum; ?>" id="field7" maxlength="524288" name="geboortedatum" required="required" style="width: 158px;" tabindex="0" title="">
-                                </div>
-                            </div>
+                <div class="col-md-8">   
+                    <input type="date" class="form-control" value="<?php echo $gebruiker->geboortedatum; ?>" id="field7" maxlength="524288" name="geboortedatum" required="required" style="width: 158px;" tabindex="0" title="">
+                </div>
+            </div>
 
-                            <div class="row">
-                                <div class="col-md-4">   
-                                    <label for="geslacht">
-                                        Geslacht:
-                                    </label>  
-                                </div>  
+            <div class="row">
+                <div class="col-md-4">   
+                    <label for="geslacht">
+                        Geslacht:
+                    </label>  
+                </div>  
 
-                                <div class="col-md-8">        
-                                    <div class="my-radio">
-                                        <?php
-                                        if (strtolower($gebruiker->geslacht) == "man") {
-                                            echo '<div class="">
+                <div class="col-md-8">        
+                    <div class="my-radio">
+                        <?php
+                        if (strtolower($gebruiker->geslacht) == "man") {
+                            echo '<div class="">
                                             <input type="radio" name="geslacht" id="field8-1"  class="form-horizontal" checked="checked" value="Man">
                                             <span class="option-title">
                                                 Man
@@ -94,8 +88,8 @@
                                                 Vrouw
                                             </span>
                                         </div>';
-                                        } else {
-                                            echo '<div class="">
+                        } else {
+                            echo '<div class="">
                                             <input type="radio" name="geslacht" id="field8-1"  class="form-horizontal" value="Man">
                                             <span class="option-title">
                                                 Man
@@ -107,24 +101,24 @@
                                                 Vrouw
                                             </span>
                                         </div>';
-                                        }
-                                        ?>
-                                    </div>
-                                </div>
-                            </div>
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
 
-                            <div class="row">
-                                <div class="col-md-4">   
-                                    <label for="type">
-                                        Type:
-                                    </label>  
-                                </div>  
+            <div class="row">
+                <div class="col-md-4">   
+                    <label for="type">
+                        Type:
+                    </label>  
+                </div>  
 
-                                <div class="col-md-8">        
-                                    <div class="my-radio">
-                                        <?php
-                                        if ($gebruiker->typeId == 2) {
-                                            echo '<div class="">
+                <div class="col-md-8">        
+                    <div class="my-radio">
+                        <?php
+                        if ($gebruiker->typeId == 2) {
+                            echo '<div class="">
                                             <input type="radio" name="type" id="field9-1"  class="form-horizontal" checked="checked" value="2">
                                             <span class="option-title">
                                                 Spreker
@@ -136,8 +130,8 @@
                                                 Bezoeker
                                             </span>
                                         </div>';
-                                        } elseif ($gebruiker->typeId == 1) {
-                                            echo '<div class="">
+                        } elseif ($gebruiker->typeId == 1) {
+                            echo '<div class="">
                                             <input type="radio" name="type" id="field9-1"  class="form-horizontal"  value="2">
                                             <span class="option-title">
                                                 Spreker
@@ -149,100 +143,94 @@
                                                 Bezoeker
                                             </span>
                                         </div>';
-                                        }
-                                        ?>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-
-
-                        <div class="col-md-6 border-left">      
-                            <div class="row">
-                                <div class="col-md-4">   
-                                    <label for="land">
-                                        Land:
-                                    </label>
-                                </div>
-
-                                <div class="col-md-8">   
-                                    <?php
-                                    foreach ($landen as $land) {
-                                        $options[$land->id] = $land->naam;
-                                    }
-                                    echo form_dropdown('land', $options, $gebruiker->landId, 'class="form-control" id="field9" required="required"');
-                                    ?>
-                                </div>
-
-
-
-                                <div class="row">
-                                    <div class="col-md-4">   
-                                        <label for="gemeente">
-                                            Gemeente:
-                                        </label>
-                                    </div>
-
-                                    <div class="col-md-8">   
-                                        <input type="text" name="gemeente" value="<?php echo $gebruiker->gemeente; ?>" id="field10" class="form-control" required="required">
-                                    </div>
-                                </div>
-
-
-
-                                <div class="row">
-                                    <div class="col-md-4">   
-                                        <label for="postcode">
-                                            Postcode:
-                                        </label>
-                                    </div>
-
-                                    <div class="col-md-8">   
-                                        <input type="text" name="postcode" value="<?php echo $gebruiker->postcode; ?>" id="field10-b" class="form-control" required="required">
-                                    </div>
-                                </div>
-
-
-
-                                <div class="row">
-                                    <div class="col-md-4">   
-                                        <label for="straat">
-                                            Straat:
-                                        </label>
-                                    </div>
-
-                                    <div class="col-md-8">   
-                                        <input type="text" name="straat" value="<?php echo $gebruiker->straat; ?>" id="field11" class="form-control" required="required">
-                                    </div>
-                                </div>
-
-
-
-                                <div class="row">
-                                    <div class="col-md-4">   
-                                        <label for="huisnummer">
-                                            Huisnummer:
-                                        </label>
-                                    </div>
-
-                                    <div class="col-md-8">   
-                                        <input type="number" name="huisnummer" value="<?php echo $gebruiker->nummer; ?>" id="field12" class="form-control" required="required">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>         
-
+                        }
+                        ?>
                     </div>
-
                 </div>
-
-                <?php echo anchor('gebruiker/overzichtGebruikers', 'Annuleer', 'class="btn btn-default"'); ?>
-                <?php echo form_submit('opslaan', 'Opslaan', 'class="btn btn-default"'); ?>
-
-                <?php echo form_close(); ?>
             </div>
 
         </div>
+
+
+        <div class="col-md-6 border-left">      
+            <div class="row">
+                <div class="col-md-4">   
+                    <label for="land">
+                        Land:
+                    </label>
+                </div>
+
+                <div class="col-md-8">   
+                    <?php
+                    foreach ($landen as $land) {
+                        $options[$land->id] = $land->naam;
+                    }
+                    echo form_dropdown('land', $options, $gebruiker->landId, 'class="form-control" id="field9" required="required"');
+                    ?>
+                </div>
+
+
+
+                <div class="row">
+                    <div class="col-md-4">   
+                        <label for="gemeente">
+                            Gemeente:
+                        </label>
+                    </div>
+
+                    <div class="col-md-8">   
+                        <input type="text" name="gemeente" value="<?php echo $gebruiker->gemeente; ?>" id="field10" class="form-control" required="required">
+                    </div>
+                </div>
+
+
+
+                <div class="row">
+                    <div class="col-md-4">   
+                        <label for="postcode">
+                            Postcode:
+                        </label>
+                    </div>
+
+                    <div class="col-md-8">   
+                        <input type="text" name="postcode" value="<?php echo $gebruiker->postcode; ?>" id="field10-b" class="form-control" required="required">
+                    </div>
+                </div>
+
+
+
+                <div class="row">
+                    <div class="col-md-4">   
+                        <label for="straat">
+                            Straat:
+                        </label>
+                    </div>
+
+                    <div class="col-md-8">   
+                        <input type="text" name="straat" value="<?php echo $gebruiker->straat; ?>" id="field11" class="form-control" required="required">
+                    </div>
+                </div>
+
+
+
+                <div class="row">
+                    <div class="col-md-4">   
+                        <label for="huisnummer">
+                            Huisnummer:
+                        </label>
+                    </div>
+
+                    <div class="col-md-8">   
+                        <input type="number" name="huisnummer" value="<?php echo $gebruiker->nummer; ?>" id="field12" class="form-control" required="required">
+                    </div>
+                </div>
+            </div>
+        </div>         
+
     </div>
+
+    <?php echo anchor('gebruiker/overzichtGebruikers', 'Annuleer', 'class="btn btn-default"'); ?>
+    <?php echo form_submit('opslaan', 'Opslaan', 'class="btn btn-default"'); ?>
+
+    <?php echo form_close(); ?>
 </div>
