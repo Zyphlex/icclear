908df3e8a69aca9b4ba7898a876c056826c0a637
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-29 09:18:06 +0000

    namen in footer

--- icclear/application/icclear/views/main_footer.php
+++ icclear/application/icclear/views/main_footer.php
@@ -1,10 +1,10 @@
 <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-4">
         <h4>HELP</h4>
         <p><?php echo anchor('faq', 'F.A.Q.'); ?></p>
         <p>User Guide</p>
     </div>
-    <div class="col-md-6 right">
+    <div class="col-md-4">
         <h4>CONTACT</h4>
         <p><span class="glyphicon glyphicon-envelope"></span> email: <a href="mailto:support@icclear.test">support@icclear.test</a></p>
         <p><span class="glyphicon glyphicon-phone-alt"></span> tel: 017 55 94 781</p>
