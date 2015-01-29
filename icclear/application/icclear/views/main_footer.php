f47dcabbd2b62b851c41ec672ac41b145faf1f7c
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-01-29 09:15:48 +0000

    namen in footer

--- icclear/application/icclear/views/main_footer.php
+++ icclear/application/icclear/views/main_footer.php
@@ -1,15 +1,18 @@
 <div class="row">
-    <div class="col-md-4">
-        <h4>ROUTE</h4>
-    </div>
-    <div class="col-md-4">
+    <div class="col-md-6">
         <h4>HELP</h4>
         <p><?php echo anchor('faq', 'F.A.Q.'); ?></p>
         <p>User Guide</p>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-6">
         <h4>CONTACT</h4>
         <p><span class="glyphicon glyphicon-envelope"></span> email: <a href="mailto:support@icclear.test">support@icclear.test</a></p>
         <p><span class="glyphicon glyphicon-phone-alt"></span> tel: 017 55 94 781</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <p>PHP Project IT Ninjas - Frederik Van Hooghten, Rob Oosthoek, Leslie Milants, Abderrahmane Ikrou</p>
     </div>
 </div>
