4ec82a640c85f6ddab201085260dc0889aaa48cd
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-12 14:10:28 +0000

    textbox

--- icclear/application/icclear/controllers/spreker.php
+++ icclear/application/icclear/controllers/spreker.php
@@ -35,15 +35,7 @@ class Spreker extends CI_Controller {
     }
     
     public function indienen(){
-        $data['user']  = $this->authex->getUserInfo();        
-        $data['title'] = 'IC Clear - Sprekers'; 
-        $data['active'] = 'spreker';
-        
-        $this->load->model('gebruiker_model');
-        $data['sprekers'] = $this->gebruiker_model->getSprekersActieve();
-                
-        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'spreker/overzicht', 'footer' => 'main_footer');
-        $this->template->load('main_master', $partials, $data); 
+       
     }
   
     
