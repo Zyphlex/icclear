2b6b9b36a40e058c579768d6bec46ff1e41bed80
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-12 20:08:41 +0000

    sessies  in planningen

--- icclear/application/icclear/models/planning_model.php
+++ icclear/application/icclear/models/planning_model.php
@@ -32,7 +32,7 @@ class Planning_model extends CI_Model {
     }
     
     function getAllPlanningen() {        
-        $this->db->order_by('conferentiedagId');
+        $this->db->order_by('beginuur');
         $query = $this->db->get('planning');
         $planningen = $query->result();
         
