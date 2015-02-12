035e5c8233fcecad9102c596bd5cd7d3374486b8
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-12 15:12:46 +0000

    confonderdeel nieuw model

---
+++ icclear/application/icclear/models/conferentie_onderdeel_model.php
@@ -0,0 +1,35 @@
+<?php
+
+class Conferentie_onderdeel_model extends CI_Model {
+
+    // +----------------------------------------------------------
+    // | Beershop - product_model
+    // +----------------------------------------------------------
+    // | Thomas More Kempen - 2 TI - 201x-201x
+    // +----------------------------------------------------------
+    // | Product model
+    // |
+    // +----------------------------------------------------------
+    // | K. Vangeel
+    // +----------------------------------------------------------
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function get($id) {
+        $this->db->where('id', $id);
+        $query = $this->db->get('conferentieOnderdeel');
+        return $query->row();
+    }
+
+    function getHuidigeConferentie()
+    {
+        $this->db->where('statusId', '2');
+        $query = $this->db->get('conferentie');
+        return $query->row();
+    }
+
+}
+
+?>
