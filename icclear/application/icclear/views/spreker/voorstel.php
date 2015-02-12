6d1a5e88f80beb0c61d6ade9ab6921a87cf2d057
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-12 14:03:09 +0000

    textbox

--- icclear/application/icclear/views/spreker/voorstel.php
+++ icclear/application/icclear/views/spreker/voorstel.php
@@ -2,56 +2,13 @@
     <div class="col-md-12">
         <h1>Conferentie <?php echo $conferentie->naam ?> - Voorstel voor sessie indienen</h1>
     </div>
-</div>
-
-<div class="row">
-    <div class="col-md-8">
-        <h2>Profielfoto en biografie</h2>
-        <p>
-            Praesent sed est id leo molestie malesuada. Pellentesque interdum elit eu neque facilisis, eget elementum ante tempor. 
-            Nunc dictum venenatis magna non tincidunt. Vestibulum vitae faucibus odio, et posuere est. Sed ac nisi ex. 
-            Donec porttitor vitae sapien nec laoreet. Maecenas dignissim dignissim justo a maximus.
-            Vestibulum vitae faucibus odio, et posuere est. Sed ac nisi ex. 
-            Donec porttitor vitae sapien nec laoreet. Maecenas dignissim dignissim justo a maximus.
-            Pellentesque interdum elit eu neque facilisis, eget elementum ante tempor. 
-        </p> 
-    </div> 
-</div>
-
-
-<form action="spreker/indienen">
-<div class="row">
-    <div class="col-md-2">
-        <label for="foto" class="control-label">Foto: </label>
-    </div>
-    <div class="col-md-2 fileUpload btn btn-default">
-        <span>Bestand selecteren</span>
-        <input id="foto" type="file" name="foto" class="upload"/>
-    </div>
-</div>
-    
-    <br/>
-    
-<div class="row">
-    <div class="col-md-2">
-        <label for="biografie" class="control-label">Biografie: </label>
-    </div>
-    <div class="col-md-8">
-        <textarea id="biografie" name="biografie" rows="10" class="form-control"></textarea>
-    </div>
-</div>
-    
-    
-<br/><br/>
-    
+</div>    
     
 <div class="row">
     <div class="col-md-8">
         <h2>Sessie voorstel</h2>
         <p>
-            Praesent sed est id leo molestie malesuada. Pellentesque interdum elit eu neque facilisis, eget elementum ante tempor. 
-            Nunc dictum venenatis magna non tincidunt. Vestibulum vitae faucibus odio, et posuere est. Sed ac nisi ex. 
-            Donec porttitor vitae sapien nec laoreet. Maecenas dignissim dignissim justo a maximus.
+            Als u bestanden hebt die u later zal gebruiken, dan kan u ze hier uploaden.
         </p>        
     </div> 
 </div>
@@ -62,7 +19,7 @@
     <div class="col-md-2">
         <label for="sessieonderwerp" class="control-label">Onderwerp: </label>
     </div>
-    <div class="col-md-2">
+    <div class="col-md-4">
         <input id="sessieonderwerp" type="text" name="sessieonderwerp" class="form-control"/>
     </div>
 </div>
