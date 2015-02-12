e9ae9ad9bc2a87213802578d0bba71db99be6125
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-12 14:01:19 +0000

    textbox

--- icclear/application/icclear/views/spreker/voorstel.php
+++ icclear/application/icclear/views/spreker/voorstel.php
@@ -2,8 +2,49 @@
     <div class="col-md-12">
         <h1>Conferentie <?php echo $conferentie->naam ?> - Voorstel voor sessie indienen</h1>
     </div>
-</div>  
+</div>
 
+<div class="row">
+    <div class="col-md-8">
+        <h2>Profielfoto en biografie</h2>
+        <p>
+            Praesent sed est id leo molestie malesuada. Pellentesque interdum elit eu neque facilisis, eget elementum ante tempor. 
+            Nunc dictum venenatis magna non tincidunt. Vestibulum vitae faucibus odio, et posuere est. Sed ac nisi ex. 
+            Donec porttitor vitae sapien nec laoreet. Maecenas dignissim dignissim justo a maximus.
+            Vestibulum vitae faucibus odio, et posuere est. Sed ac nisi ex. 
+            Donec porttitor vitae sapien nec laoreet. Maecenas dignissim dignissim justo a maximus.
+            Pellentesque interdum elit eu neque facilisis, eget elementum ante tempor. 
+        </p> 
+    </div> 
+</div>
+
+
+<form action="spreker/indienen">
+<div class="row">
+    <div class="col-md-2">
+        <label for="foto" class="control-label">Foto: </label>
+    </div>
+    <div class="col-md-2 fileUpload btn btn-default">
+        <span>Bestand selecteren</span>
+        <input id="foto" type="file" name="foto" class="upload"/>
+    </div>
+</div>
+    
+    <br/>
+    
+<div class="row">
+    <div class="col-md-2">
+        <label for="biografie" class="control-label">Biografie: </label>
+    </div>
+    <div class="col-md-8">
+        <textarea id="biografie" name="biografie" rows="10" class="form-control"></textarea>
+    </div>
+</div>
+    
+    
+<br/><br/>
+    
+    
 <div class="row">
     <div class="col-md-8">
         <h2>Sessie voorstel</h2>
@@ -21,13 +62,13 @@
     <div class="col-md-2">
         <label for="sessieonderwerp" class="control-label">Onderwerp: </label>
     </div>
-    <div class="col-md-4">
-        <input id="sessieonderwerp" type="text" name="sessieonderwerp" class="form-control" size="50" />
+    <div class="col-md-2">
+        <input id="sessieonderwerp" type="text" name="sessieonderwerp" class="form-control"/>
     </div>
 </div>
-
+    
 <br/>
-
+    
 <div class="row">
     <div class="col-md-2">
         <label for="biografie" class="control-label">Omschrijving: </label>
@@ -36,7 +77,7 @@
         <textarea id="biografie" name="biografie" rows="10" class="form-control"></textarea>
     </div>
 </div>
-
+    
 <br/>
 
 <div class="row">
@@ -48,10 +89,12 @@
         <input id="sessiebijlage" type="file" name="sessiebijlage" class="upload"/>
     </div>
 </div>
-
+        
 <div class="row">
     <div class="col-md-4 italic">
-        Als u enkele bestanden heeft, kan u ze hier uploaden. 
+        Vestibulum in velit at nibh euismod commodo. 
+        Aliquam vitae urna consectetur metus convallis sodales. 
+        Donec suscipit eu velit ac tempor. 
     </div>
 </div>
 
@@ -59,7 +102,9 @@
 
 <div class="row">
     <div class="col-md-12">        
-        <?php echo anchor('spreker', 'Annuleren', 'class="btn btn-default"'); ?>     
-        
+        <?php echo anchor('spreker', 'Annuleren','class="btn btn-default"'); ?>     
+        <input type="submit" class="btn btn-default" value="Voorstel versturen">
     </div>
 </div>
+
+</form>
