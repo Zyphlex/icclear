a92776ec837d01d7dd7a49e14448c764557e30b0
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-12 15:03:28 +0000

    overzicht

---
+++ icclear/application/icclear/views/admin/inschrijving/overzicht.php
@@ -0,0 +1,34 @@
+
+
+    <div class="col-md-10">
+        
+        <h1>Inschijvingen beheren.</h1>  
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Familienaam</th>
+                                <th>Voornaam</th>
+                                <th>ConfOnderdeel</th>
+                                <th>Datum</th>
+                                <th>Betaling</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($inschrijvingen as $inschrijving) {?>
+                            <tr>
+                                <td><?php echo $inschrijving->gebruiker->familienaam ?></td>
+                                <td><?php echo $inschrijving->gebruiker->voornaam ?></td>
+                                <td><?php echo $inschrijving->conferentieOnderdeelId ?></td>
+                                <td><?php echo $inschrijving->datum ?></td>
+                                <td><?php echo $inschrijving->betaling->methode ?></td>
+                                <td><?php echo anchor('gebruiker/wijzig/' . $inschrijving->id, 'Wijzigen','class="btn btn-default"'); ?>
+                                    <?php echo anchor('gebruiker/verwijder/' . $inschrijving->id, 'Verwijderen','class="btn btn-default"'); ?></td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+        
+        <?php echo anchor('admin', 'Annuleren','class="btn btn-default"'); ?> 
+        <?php echo anchor('gebruiker/nieuw', 'Nieuwe gebruiker toevoegen','class="btn btn-default"'); ?> 
+        
+    </div>
