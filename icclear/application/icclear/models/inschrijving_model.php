186c42e6e7cbbec93ed15c8babffaa2fe0e5d5ab
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-12 14:37:37 +0000

    nieuwe model

--- icclear/application/icclear/models/inschrijving_model.php
+++ icclear/application/icclear/models/inschrijving_model.php
@@ -1,53 +1,31 @@
 <?php
 
-class Inschrijving_model extends CI_Model {
+class Land_model extends CI_Model {
 
-    function __construct() {
+    // +----------------------------------------------------------
+    // | Beershop - product_model
+    // +----------------------------------------------------------
+    // | Thomas More Kempen - 2 TI - 201x-201x
+    // +----------------------------------------------------------
+    // | Product model
+    // |
+    // +----------------------------------------------------------
+    // | K. Vangeel
+    // +----------------------------------------------------------
+
+    function __construct()
+    {
         parent::__construct();
     }
 
-    /*function getAllWithGebruiker() {
-        $this->db->order_by('gebruikerId', 'asc');
-        $query = $this->db->get('inschrijving');
-        $inschrijvingen = $query->result();
+       
+    function getAll()
+    {        
+        $query = $this->db->get('land');
+        return $query->result();
+    }    
 
-        $this->load->model('gebruiker_model');
-
-        foreach ($inschrijvingen as $inschrijving) {
-            $inschrijving->gebruiker = $this->gebruiker_model->get($inschrijving->gebruikerId);
-        }
-        
-        return $inschrijvingen;
-    }
-
-    }*/
-
-    function getPerOnderdeel($id) {
-        $this->db->where('id', $id);
-        $query = $this->db->get('inschrijving');
-        $sessie = $query->row();
-        
-        return $sessie;
-    }
-
-
-    function getInschrijvingen($id)    {
-	
-        $this->db->where('conferentieId',$id);
-	$query = $this->db->get('conferentieOnderdeel');
-	$onderdelen = $query->result();
-
-	foreach ($onderdelen as $onderdeel)
-	{	
-		$onderdeel->inschrijving = $this->getPerOnderdeel($onderdeel->id);		
-	}
-        
-	$this->load->model('gebruiker_model');
-        foreach ($onderdelen as $onderdeel) {
-            $onderdeel->gebruiker = $this->gebruiker_model->get($onderdeel->inschrijving->gebruikerId);
-        }
-        
-        return $onderdelen;
-    }
+    
 }
+
 ?>
