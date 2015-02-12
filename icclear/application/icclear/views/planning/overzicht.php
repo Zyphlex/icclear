1be3941e2cb9e0cc6679ac766938a78814821c28
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-12 14:50:34 +0000

    sessies  in planningen

--- icclear/application/icclear/views/planning/overzicht.php
+++ icclear/application/icclear/views/planning/overzicht.php
@@ -62,14 +62,12 @@
         ?>
             </div>
         </div>
-
     </div>    
 </div>
 
 <div class='row'>
     <div class='col-md-12'>
-        <h3>Activiteiten</h3>
-        
+        <h3>Activiteiten</h3>        
         <div class="panel panel-default">
             <div class="panel-body">                
                 <table class = "table">
