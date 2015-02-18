3bf275dd5ed3f0df9ac27e738b522eba101e6f53
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-18 16:13:41 +0000

    admins

---
+++ icclear/application/icclear/models/gebruiker_activiteit_model.php
@@ -0,0 +1,37 @@
+<?php
+
+class Gebruiker_activiteit_model extends CI_Model {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function check_username_availablity() {
+        $username = trim($this->input->post('username'));
+
+        $query = $this->db->query('SELECT * FROM gebruiker where gebruikersnaam="' . $username . '"');
+
+        if ($query->num_rows() > 0)
+            return false;
+        else
+            return true;
+    }
+
+    function getActiviteitPrijs() {
+        $query = $this->db->get('gebruikerActiviteit');
+        $gebActiviteiten = $query->result();
+
+        $this->load->model('gebruiker_model');
+        $this->load->model('activiteit_model');
+
+
+        foreach ($gebActiviteiten as $gebActiviteit) {
+            $gebActiviteit->gebruiker = $this->gebruiker_model->get($gebActiviteit->gebruikerId);
+            $gebActiviteit->activiteit = $this->activiteit_model->get($gebActiviteit->activiteitId);
+        }
+        return $gebActiviteiten;
+    }
+
+}
+
+?>
