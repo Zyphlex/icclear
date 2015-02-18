3dedd0ae2a6e2204e41537ca5c448fa6d30c50fa
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-18 14:18:23 +0000

    admins

--- icclear/application/icclear/models/gebruiker_model.php
+++ icclear/application/icclear/models/gebruiker_model.php
@@ -24,7 +24,7 @@ class Gebruiker_model extends CI_Model {
     }
 
     function getAll() {
-        $this->db->where('typeId', 1);
+        $this->db->where('typeId', !3);
         $this->db->order_by('familienaam', 'asc');
         $query = $this->db->get('gebruiker');
         return $query->result();
