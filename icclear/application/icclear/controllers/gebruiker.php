a05e0b0a0db6dff48aa10d6f7bfcc4a57a8e3866
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-18 14:50:15 +0000

    intellingen

--- icclear/application/icclear/controllers/gebruiker.php
+++ icclear/application/icclear/controllers/gebruiker.php
@@ -187,11 +187,10 @@ class Gebruiker extends CI_Controller {
     
     public function instellingen()
     {
-        $data['user'] = $this->authex->getUserInfo();
-        $data['conferentieId'] = $this->session->userdata('conferentieId');
+        $data['user'] = $this->authex->getUserInfo();        
         
-        $data['title'] = 'IC Clear - Beheer';
-        $data['active'] = 'admin';
+        $data['title'] = 'IC Clear - Profielinstellingen';
+        $data['active'] = '';
         
         $this->load->model('inschrijving_model');
         $data['inschrijvingen'] = $this->inschrijving_model->getAllInschijvingByConferentie($this->session->userdata('conferentieId'));
