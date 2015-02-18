d13edb04542d64a0c4b9933e7074a9b66801748d
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-18 14:05:29 +0000

    admins

---
+++ icclear/application/icclear/views/admin/gebruiker/overzicht_admin.php
@@ -0,0 +1,31 @@
+
+
+    <div class="col-md-10">
+        
+        <h1>Admins beheren.</h1>  
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Familienaam</th>
+                                <th>Voornaam</th>
+                                <th>Email</th>
+                                <th>Beheer</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($admins as $admin) {?>
+                            <tr>
+                                <td><?php echo $admin->familienaam ?></td>
+                                <td><?php echo $admin->voornaam ?></td>
+                                <td><?php echo $admin->emailadres ?></td>
+                                <td><?php echo anchor('gebruiker/wijzig/' . $admin->id, 'Wijzigen','class="btn btn-default"'); ?>
+                                    <?php echo anchor('gebruiker/verwijder/' . $admin->id, 'Verwijderen','class="btn btn-default"'); ?></td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+        
+        <?php echo anchor('admin', 'Annuleren','class="btn btn-default"'); ?> 
+        <?php echo anchor('gebruiker/nieuw', 'Nieuwe admin toevoegen','class="btn btn-default"'); ?> 
+        
+    </div>
