8f22b28c3f28abc9c35ce002f015f9a52c9a64b1
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-18 21:51:24 +0000

    intellingen

---
+++ icclear/application/icclear/controllers/profiel.php
@@ -0,0 +1,78 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Profiel extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper(array('form', 'url'));
+        if (!$this->authex->loggedIn()) {
+            redirect('logon/aanmelden');         
+        }         
+    }
+    
+    public function wijzig($id) {
+
+        $data['user'] = $this->authex->getUserInfo();
+        $data['conferentieId'] = $this->session->userdata('conferentieId');
+
+        $data['title'] = 'IC Clear - Beheer';
+        $data['active'] = 'admin';
+
+        $this->load->model('gebruiker_model');
+        $data['gebruiker'] = $this->gebruiker_model->get($id);
+
+        $this->load->model('land_model');
+        $data['landen'] = $this->land_model->getAll();
+
+        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/gebruiker/wijzigen', 'footer' => 'main_footer');
+        $this->template->load('admin_master', $partials, $data);
+    }
+    
+    public function update() {
+        $gebruiker = new stdClass();
+
+        $gebruiker->id = $this->input->post('id');
+        $gebruiker->voornaam = $this->input->post('voornaam');
+        $gebruiker->familienaam = $this->input->post('familienaam');
+        $gebruiker->geboortedatum = $this->input->post('geboortedatum');
+        $gebruiker->emailadres = $this->input->post('emailadres');
+        $gebruiker->geslacht = $this->input->post('geslacht');
+        $gebruiker->typeId = $this->input->post('type');
+        $gebruiker->landId = $this->input->post('land');
+        $gebruiker->gemeente = $this->input->post('gemeente');
+        $gebruiker->postcode = $this->input->post('postcode');
+        $gebruiker->straat = $this->input->post('straat');
+        $gebruiker->nummer = $this->input->post('huisnummer');
+
+        $this->load->model('gebruiker_model');
+
+        $this->gebruiker_model->update($gebruiker);
+
+        redirect('home');
+    }
+    
+    public function instellingen() {
+        $user = $this->authex->getUserInfo();
+        $data['user'] = $user;
+
+        $data['title'] = 'IC Clear - Profielinstellingen';
+        $data['active'] = '';
+
+        $this->load->model('gebruiker_model');
+        $data['gebruiker'] = $this->gebruiker_model->get($user->id);
+        
+        $this->load->model('land_model');
+        $data['landen'] = $this->land_model->getAll();
+
+        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'gebruiker/wijzigen', 'footer' => 'main_footer');
+        $this->template->load('main_master', $partials, $data);
+    }
+
+    
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
