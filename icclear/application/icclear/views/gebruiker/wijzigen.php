d4193314076038201a4cd5267459ac9b7cc3cce6
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-18 14:54:58 +0000

    intellingen

--- icclear/application/icclear/views/gebruiker/wijzigen.php
+++ icclear/application/icclear/views/gebruiker/wijzigen.php
@@ -21,7 +21,7 @@
                 </div>
 
                 <div class="col-md-8">
-                    <?php echo form_input(array('name' => 'voornaam', 'id' => 'field2', 'value' => $gebruiker->voornaam, 'class' => 'form-control', 'required' => 'required')); ?>
+                    <?php echo form_input(array('name' => 'voornaam', 'id' => 'field2', 'value' => $gebruiker->voornaam, 'class' => 'form-control', 'disabled' => 'disabled')); ?>
                 </div>
             </div>
 
