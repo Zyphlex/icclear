516a41cb202fe873b8f2c1a41986aadacbd45c9c
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-18 14:37:54 +0000

    admins

--- icclear/application/icclear/views/admin/gebruiker/overzicht.php
+++ icclear/application/icclear/views/admin/gebruiker/overzicht.php
@@ -1,31 +1,38 @@
 
 
-    <div class="col-md-10">
-        
-        <h1>Gebruikers beheren(is nog enkel bezoekers)</h1>  
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Familienaam</th>
-                                <th>Voornaam</th>
-                                <th>Email</th>
-                                <th>Beheer</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach($gebruikers as $gebruiker) {?>
-                            <tr>
-                                <td><?php echo $gebruiker->familienaam ?></td>
-                                <td><?php echo $gebruiker->voornaam ?></td>
-                                <td><?php echo $gebruiker->emailadres ?></td>
-                                <td><?php echo anchor('gebruiker/wijzig/' . $gebruiker->id, 'Wijzigen','class="btn btn-default"'); ?>
-                                    <?php echo anchor('gebruiker/verwijder/' . $gebruiker->id, 'Verwijderen','class="btn btn-default"'); ?></td>
-                            </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
-        
-        <?php echo anchor('admin', 'Annuleren','class="btn btn-default"'); ?> 
-        <?php echo anchor('gebruiker/nieuw', 'Nieuwe gebruiker toevoegen','class="btn btn-default"'); ?> 
-        
-    </div>
+<div class="col-md-10">
+
+    <h1>Gebruikers beheren</h1>  
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Familienaam</th>
+                <th>Voornaam</th>
+                <th>Email</th>
+                <th>Type</th>
+                <th>Beheer</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($gebruikers as $gebruiker) { ?>
+                <tr>
+                    <td><?php echo $gebruiker->familienaam ?></td>
+                    <td><?php echo $gebruiker->voornaam ?></td>
+                    <td><?php echo $gebruiker->emailadres ?></td>
+                    <?php if ($gebruiker->typeId == 1) { ?>
+                        <td>Bezoeker</td>
+                    <?php } else { ?>
+                        <td>Spreker</td>
+                    <?php } ?>
+
+                    <td><?php echo anchor('gebruiker/wijzig/' . $gebruiker->id, 'Wijzigen', 'class="btn btn-default"'); ?>
+                        <?php echo anchor('gebruiker/verwijder/' . $gebruiker->id, 'Verwijderen', 'class="btn btn-default"'); ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+
+    <?php echo anchor('admin', 'Annuleren', 'class="btn btn-default"'); ?> 
+    <?php echo anchor('gebruiker/nieuw', 'Nieuwe gebruiker toevoegen', 'class="btn btn-default"'); ?> 
+
+</div>
