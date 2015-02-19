5a3b0e769b4a09a6af7433c0d7d4a7668c15d332
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-19 21:46:36 +0000

    betaling opvolgen per gebruiker bij instellingen

--- icclear/application/icclear/views/gebruiker/wijzigen.php
+++ icclear/application/icclear/views/gebruiker/wijzigen.php
@@ -101,6 +101,17 @@
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-4">   
+                    <label for="betaling">
+                        Betaling:
+                    </label>
+                </div>
+
+                <div class="col-md-8">   
+                    <input type="text" class="form-control" value="<?php echo $gebruiker->betaling; ?>" id="betaling" name="betaling">
+                </div>
+            </div>
             <?php if ($gebruiker->typeId == 2) { ?>
             <div class="row">
                 <div class="col-md-4">   
