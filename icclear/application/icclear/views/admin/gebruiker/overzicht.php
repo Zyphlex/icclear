313804f915334164d2bbbb726f3b09712da63942
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-19 14:25:34 +0000

    verwijderen

--- icclear/application/icclear/views/admin/gebruiker/overzicht.php
+++ icclear/application/icclear/views/admin/gebruiker/overzicht.php
@@ -1,37 +1,10 @@
-<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="myModalLabel">Gebruiker Verwijderen</h4>
-                </div>
-            
-                <div class="modal-body">
-                    <p>Deze gebruiker wordt verwijderd.</p>
-                    <p>Ben je zeker?</p>
-                    <p class="debug-url"></p>
-                </div>
-                
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Annuleer</button>
-                    <a href="https://www.google.be/" class="btn btn-danger danger">Verwijder</a>
-                </div>
-            </div>
-        </div>
-    </div>
+<script>
+    $('#confirm-delete').on('show.bs.modal', function (e) {
+        $(this).find('.danger').attr('href', $(e.relatedTarget).data('href'));
 
-    <a data-href="delete.php?id=23" data-toggle="modal" data-target="#confirm-delete" href="#">Delete record #23</a><br>
-    <a data-href="delete.php?id=54" data-toggle="modal" data-target="#confirm-delete" href="#">Delete record #54</a>
-
-
-    <script>
-        $('#confirm-delete').on('show.bs.modal', function(e) {
-            $(this).find('.danger').attr('href', $(e.relatedTarget).data('href'));
-            
-            $('.debug-url').html('Delete URL: <strong>' + $(this).find('.danger').attr('href') + '</strong>');
-        })
-    </script>
+        $('.debug-url').html('Delete URL: <strong>' + $(this).find('.danger').attr('href') + '</strong>');
+    })
+</script>
 
 <div class="col-md-10">
 
@@ -47,22 +20,45 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($gebruikers as $gebruiker) { ?>
-                <tr>
-                    <td><?php echo $gebruiker->familienaam ?></td>
-                    <td><?php echo $gebruiker->voornaam ?></td>
-                    <td><?php echo $gebruiker->emailadres ?></td>
-                    <?php if ($gebruiker->typeId == 1) { ?>
-                        <td>Bezoeker</td>
-                    <?php } else { ?>
-                        <td>Spreker</td>
-                    <?php } ?>
+        <div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
 
-                    <td><?php echo anchor('gebruiker/wijzig/' . $gebruiker->id, 'Wijzigen', 'class="btn btn-default"'); ?>
-                        <?php echo anchor('gebruiker/verwijder/' . $gebruiker->id, 'Verwijderen', 'class="btn btn-default"', 'data-toggle="modal"', 'data-target="#confirm-delete"'); ?></td>
-                <a data-href="https://subversion.khk.be/projecten/TI1415project23/icclear/icclear.php/gebruiker/verwijder/30" data-toggle="modal" data-target="#confirm-delete" href="#">Verwijder</a>
-                </tr>
-            <?php } ?>
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title" id="myModalLabel">Gebruiker Verwijderen</h4>
+                    </div>
+
+                    <div class="modal-body">
+                        <p>Deze gebruiker wordt verwijderd.</p>
+                        <p>Ben je zeker?</p>
+                        <p class="debug-url"></p>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Annuleer</button>
+                        
+
+                        <?php foreach ($gebruikers as $gebruiker) { ?>
+                            <tr>
+                                <td><?php echo $gebruiker->familienaam ?></td>
+                                <td><?php echo $gebruiker->voornaam ?></td>
+                                <td><?php echo $gebruiker->emailadres ?></td>
+                                <?php if ($gebruiker->typeId == 1) { ?>
+                                    <td>Bezoeker</td>
+                                <?php } else { ?>
+                                    <td>Spreker</td>
+                                <?php } ?>
+
+                                <td><?php echo anchor('gebruiker/wijzig/' . $gebruiker->id, 'Wijzigen', 'class="btn btn-default"'); ?>
+                            <a data-href="" data-toggle="modal" data-target="#confirm-delete" class="btn btn-default" href="#">Verwijderen</a>
+                            <a href="https://subversion.khk.be/projecten/TI1415project23/icclear/icclear.php/gebruiker/verwijder/<?php $gebruiker->id?>" class="btn btn-danger danger">Verwijder</a>
+                            </tr>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
         </tbody>
     </table>
 
