c8e243ac7e6bfa620beb34e649de1b63977d5978
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-25 15:52:43 +0000

    gebouwen

---
+++ icclear/application/icclear/views/admin/gebouw/wijzigen.php
@@ -0,0 +1,49 @@
+<div class="col-md-10">
+    <h1>Gebouw wijzigen</h1>
+
+    <form action="gebouw/opslaan" method="post">
+
+        <div class="row">            
+            <label for="naam" class="control-label col-md-2">Naam:</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" id="naam" name="naam" value="<?php echo $gebouw->naam ?>"/>                
+            </div>
+        </div>
+        <div class="row">            
+            <label for="naam" class="control-label col-md-2">Postcode:</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" id="naam" name="naam" value="<?php echo $gebouw->postcode ?>"/>                
+            </div>
+        </div>
+        <div class="row">            
+            <label for="naam" class="control-label col-md-2">Gemeente:</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" id="naam" name="naam" value="<?php echo $gebouw->gemeente ?>"/>                
+            </div>
+        </div>
+        <div class="row">            
+            <label for="naam" class="control-label col-md-2">Straat:</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" id="naam" name="naam" value="<?php echo $gebouw->straat ?>"/>                
+            </div>
+        </div>
+        <div class="row">            
+            <label for="naam" class="control-label col-md-2">Nummer:</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" id="naam" name="naam" value="<?php echo $gebouw->nummer ?>"/>                
+            </div>
+        </div>        
+
+       
+
+<div class="row">
+    <div class="col-md-12">                
+        <?php echo anchor('admin', 'Annuleren', 'class="btn btn-default"'); ?>         
+        <input type="submit" value="Opslaan" class="btn btn-default"/>             
+    </div>
+</div>
+
+</form>
+
+
+</div>
