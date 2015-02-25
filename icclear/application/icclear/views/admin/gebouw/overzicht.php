d5fda946b43a1785856026781fba703408188d1f
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-25 14:01:47 +0000

    gebouwen

---
+++ icclear/application/icclear/views/admin/gebouw/overzicht.php
@@ -0,0 +1,31 @@
+<div class="col-md-10">               
+        <table class = "table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Titel</th>
+                    <th>Inhoud</th>
+                    <th>Gepost door</th>                    
+                </tr>
+            </thead>  
+            <tbody>                              
+                <?php
+                $teller = 1;
+                foreach ($aankondigingen as $aankondiging) {                    
+                        echo '<tr>' . "\n";
+                        echo '<td>' . $teller . '</td>' . "\n";
+                        echo '<td>' . $aankondiging->titel . '</td>' . "\n";
+                        echo '<td>' . $aankondiging->inhoud . '</td>' . "\n";                                                                      
+                        echo '<td>' . $aankondiging->poster->voornaam . ' ' . $aankondiging->poster->familienaam . '</td>' . "\n";
+                        echo '</tr>' . "\n";
+                        $teller++;                    
+                }
+                ?>
+            </tbody>
+        </table>
+    <p>
+        <?php echo anchor('admin/dashboard/' . $conferentieId, 'Annuleren','class="btn btn-default"'); ?>     
+        <?php echo anchor('aankondiging/toevoegen', 'Nieuwe aankondiging', 'class="btn btn-default"'); ?>
+    </p>
+</div>
+
