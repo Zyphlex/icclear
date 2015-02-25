19c514a388879c21661318112095d6a51cee1d35
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-25 14:21:38 +0000

    routes per venue

--- icclear/application/icclear/views/locatie/overzicht.php
+++ icclear/application/icclear/views/locatie/overzicht.php
@@ -38,34 +38,14 @@
                         <p><?php echo $gebouw->gebouw->gemeente ?> (<?php echo $gebouw->gebouw->postcode ?>)</p>
                         <p><?php echo $gebouw->gebouw->straat ?> <?php echo $gebouw->gebouw->nummer ?></p>
                     
-                        <button id="zoekRoutes" class="btn btn-primary" data-toggle="modal" data-target="#routesModal">Toon routes</button>
+                        <button id="zoekRoutes" class="btn btn-primary">Toon routes</button>
                     </div>
                 </div>  
             </div>
         <?php $teller++; } ?>
     
-                <div id="resultaat"></div>
 </div>
 
-<div class="modal fade" id="routesModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Routes naar <?php echo $gebouw->gebouw->naam ?></h4>
-            </div>
-
-
-            <div class="modal-body"> 
-                <div id="resultaat"></div>
-            </div>
-            
-             <div class="modal-footer">
-                 <input type="hidden" value="<?php $gebouw->gebouw->id ?>" id="gebouw"/>
-                <button type="button" class="btn btn-primary" data-dismiss="modal">Annuleer</button>
-            </div>
-            
-        </div>            
-    </div>
-</div>  
+<div class="row">    
+    <div id="resultaat"></div>
+</div>
