0cdbd2aeb1fb32f0b21cddf04f9ad2a7eb557664
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-25 15:27:28 +0000

    routes per venue

--- icclear/application/icclear/views/locatie/overzichtroutes.php
+++ icclear/application/icclear/views/locatie/overzichtroutes.php
@@ -2,7 +2,7 @@
     <div class = "panel-body">  
         <?php foreach ($routes as $route) { ?>
             <h3><?php echo $route->vertrekPunt ?></h3>
-            <p><?php echo $route->omschrijving ?></p>
+            <p><?php echo $route->beschrijving ?></p>
         <?php } ?>
     </div>
 </div>
