cef73c92b209e11cc6da083fa93bdd678d836aaa
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-26 14:47:10 +0000

    ajax/json bij opvragen van faq beheer

--- icclear/application/icclear/views/admin/faq/lijst.php
+++ icclear/application/icclear/views/admin/faq/lijst.php
@@ -1,12 +1,23 @@
-<?php foreach ($vragen as $vraag) { ?>
-    <tr>
-        <td><?php echo $vraag->vraag ?></td>
-        <td><?php echo $vraag->antwoord ?></td>
-        <td>
-            <p>                                        
-                <?php echo anchor('Wijzig', 'class="wijzigFag btn btn-default"', 'data-toggle="modal"', 'data-target="#"', 'data-id="' . $vraag->id . '"'); ?>
-                <?php echo anchor('faqbeheer/verwijder/' . $vraag->id, 'Verwijder', 'class="btn btn-default"'); ?>   
-            </p>                                 
-        </td>
-    </tr>
-<?php } ?>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Vraag</th>
+            <th>Antwoord</th>
+            <th>Beheer</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($vragen as $vraag) { ?>
+            <tr>
+                <td><?php echo $vraag->vraag ?></td>
+                <td><?php echo $vraag->antwoord ?></td>
+                <td>
+                    <p>                                        
+                        <?php echo anchor('Wijzig', 'class="wijzigFag btn btn-default"', 'data-toggle="modal"', 'data-target="#"', 'data-id="' . $vraag->id . '"'); ?>
+                        <?php echo anchor('faqbeheer/verwijder/' . $vraag->id, 'Verwijder', 'class="btn btn-default"'); ?>   
+                    </p>                                 
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
