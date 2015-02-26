704ac64f61e3c0a7f259baf3deacd6999d3ccaac
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-26 14:25:24 +0000

    activiteit nieuw

--- icclear/application/icclear/models/activiteit_model.php
+++ icclear/application/icclear/models/activiteit_model.php
@@ -2,66 +2,66 @@
 
 class Activiteit_model extends CI_Model {
 
-    function __construct()
-    {
+    function __construct() {
         parent::__construct();
     }
-    
-     function get($id) {
+
+    function get($id) {
         $this->db->where('id', $id);
         $query = $this->db->get('activiteit');
         return $query->row();
     }
-    
-     function getAllActiviteiten()
-    {        
+
+    function getAllActiviteiten() {
+        $this->db->order_by('conferentieId');
         $query = $this->db->get('activiteit');
         $activiteiten = $query->result();
-        
+
         $this->load->model('conferentie_model');
-        
+
         foreach ($activiteiten as $activiteit) {
-           $activiteit->conferentie = $this->conferentie_model->get($activiteit->conferentieId);
+            $activiteit->conferentie = $this->conferentie_model->get($activiteit->conferentieId);
         }
-        
+
         return $activiteiten;
     }
-    
-    
-    function getActiviteitenPerConferentie()
-    {
+
+    function getActiviteitenPerConferentie() {
         $query = $this->db->get('activiteit');
         $activiteiten = $query->result();
-        
+
         $this->load->model('conferentie_model');
-         foreach ($activiteiten as $activiteit){
-             $activiteit->conferentie = $this->conferentie_model->get($activiteit->conferentieId);
-         }
-         
-         return $activiteiten;
+        foreach ($activiteiten as $activiteit) {
+            $activiteit->conferentie = $this->conferentie_model->get($activiteit->conferentieId);
+        }
+
+        return $activiteiten;
     }
-    
+
     function getActiviteitenActieve() {
         $this->load->model('conferentie_model');
         $actieveConferentie = $this->conferentie_model->getActieveConferentie();
-        
-        $this->db->where('conferentieId',$actieveConferentie->id);
+
+        $this->db->where('conferentieId', $actieveConferentie->id);
         $query = $this->db->get('activiteit');
         return $query->result();
     }
-    
-    function update($activiteit)
-    {
+
+    function update($activiteit) {
         $this->db->where('id', $activiteit->id);
         $this->db->update('activiteit', $activiteit);
     }
-    
-    function insert($activiteit)
-    {
+
+    function insert($activiteit) {
         $this->db->insert('activiteit', $activiteit);
         return $this->db->insert_id();
     }
-    
+
+    function delete($id) {
+        $this->db->where('id', $id);
+        $this->db->delete('activiteit');
+    }
+
 }
 
 ?>
