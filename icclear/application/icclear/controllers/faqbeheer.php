f9081577b22bdb763c8f0a745593740a4ee8c53f
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-26 20:56:56 +0000

    faqbeheer delete

--- icclear/application/icclear/controllers/faqbeheer.php
+++ icclear/application/icclear/controllers/faqbeheer.php
@@ -120,7 +120,7 @@ class Faqbeheer extends CI_Controller {
         $this->load->model('faq_model');
         $deleted = $this->faq_model->delete($id);
         
-        echo $deleted;
+        echo "0";
     }
 
 }
