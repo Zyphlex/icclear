b9c19764658f14c09f605af4c7c3d4dd813d51e5
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-26 16:17:37 +0000

    wijzigen naar CI

--- icclear/application/icclear/views/admin/gebouw/toevoegen.php
+++ icclear/application/icclear/views/admin/gebouw/toevoegen.php
@@ -1,50 +1,48 @@
 <div class="col-md-10">
-    <h1>Gebouw toevoegen</h1>
-
-    <form action="<?php echo base_url(); ?>icclear.php/gebouw/insert" method="post">
-
-        <div class="row">            
-            <label for="naam" class="control-label col-md-2">Naam:</label>
-            <div class="col-md-4">
-                <input class="form-control" type="text" id="naam" name="naam"/>                
-            </div>
+    <h1>Gebouw toevoegen</h1>   
+    <?php echo form_open('gebouw/insert'); ?>
+    <div class="row">            
+        <?php
+        $attributesLabel = array('class' => 'control-label col-md-2');        
+        echo form_label('Naam:', 'naam', $attributesLabel);
+        ?>
+        <div class="col-md-4">
+            <?php echo form_input(array('id' => 'naam', 'name' => 'naam', 'class' => 'form-control')) ?>                     
         </div>
-        <div class="row">            
-            <label for="postcode" class="control-label col-md-2">Postcode:</label>
-            <div class="col-md-4">
-                <input class="form-control" type="text" id="postcode" name="postcode"/>                
-            </div>
-        </div>
-        <div class="row">            
-            <label for="gemeente" class="control-label col-md-2">Gemeente:</label>
-            <div class="col-md-4">
-                <input class="form-control" type="text" id="gemeente" name="gemeente"/>                
-            </div>
-        </div>
-        <div class="row">            
-            <label for="straat" class="control-label col-md-2">Straat:</label>
-            <div class="col-md-4">
-                <input class="form-control" type="text" id="straat" name="straat"/>                
-            </div>
-        </div>
-        <div class="row">            
-            <label for="nummer" class="control-label col-md-2">Nummer:</label>
-            <div class="col-md-4">
-                <input class="form-control" type="text" id="nummer" name="nummer"/>                
-            </div>
-        </div>        
-
-       
-
-<div class="row">
-    <div class="col-md-12">                
-        <?php echo anchor('admin', 'Annuleren', 'class="btn btn-default"'); ?>         
-        <input type="submit" value="Toevoegen" class="btn btn-default"/>             
     </div>
+    <div class="row">            
+        <?php echo form_label('Postcode:', 'postcode', $attributesLabel); ?>
+        <div class="col-md-4">            
+            <?php echo form_input(array('id' => 'postcode', 'name' => 'postcode', 'class' => 'form-control')) ?>    
+        </div>
+    </div>
+    <div class="row">            
+        <?php echo form_label('Gemeente:', 'gemeente', $attributesLabel); ?>
+        <div class="col-md-4">                
+            <?php echo form_input(array('id' => 'gemeente', 'name' => 'gemeente', 'class' => 'form-control')) ?> 
+        </div>
+    </div>
+    <div class="row">            
+        <label for="straat" class="control-label col-md-2">Straat:</label>
+        <div class="col-md-4">                 
+            <?php echo form_input(array('id' => 'straat', 'name' => 'straat', 'class' => 'form-control')) ?> 
+        </div>
+    </div>
+    <div class="row">            
+        <?php echo form_label('Nummer:', 'nummer', $attributesLabel); ?>
+        <div class="col-md-4">                           
+            <?php echo form_input(array('id' => 'nummer', 'name' => 'nummer', 'class' => 'form-control')) ?> 
+        </div>
+    </div>        
+
+    <div class="row">
+        <div class="col-md-12">                            
+            <?php echo anchor('admin', 'Annuleren', 'class="btn btn-default"'); ?>         
+            <?php echo form_submit('submit', 'Toevoegen', "class='btn btn-default'");?>                           
+        </div>
+    </div>
+
+    <?php form_close(); ?>
+
+
 </div>
-
-</form>
-
-
-</div>
-    
