ff81e007d821e59cf76cb515304fc28468a03807
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-26 12:25:51 +0000

    routes model gebouw bij route opvragen

--- icclear/application/icclear/views/locatie/overzicht.php
+++ icclear/application/icclear/views/locatie/overzicht.php
@@ -58,7 +58,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             
-<!--            <div class="modal-header">
+            <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                 <h4 class="modal-title">Routes</h4>
             </div>
@@ -69,8 +69,7 @@
             
              <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Sluiten</button>
-            </div>-->
-                <p>Test</p>
+            </div>
             
         </div>            
     </div>
