80096b8870687df26af785d461b2c3718a8c2739
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-26 14:44:08 +0000

    ajax/json bij opvragen van faq beheer

--- icclear/application/icclear/views/admin/faq/overzicht.php
+++ icclear/application/icclear/views/admin/faq/overzicht.php
@@ -1,12 +1,9 @@
-
-    <script src="<?php echo base_url() . APPPATH; ?>js/jquery-1.11.2.min.js"></script>   
+<script src="<?php echo base_url() . APPPATH; ?>js/jquery-1.11.2.min.js"></script>   
 <script type="text/javascript">
     function haaloverzicht () {
-        alert("ajax opgeroepen");
         $.ajax({type : "GET",
                 url : site_url + "/faqbeheer/overzicht",
                 success : function(result){
-                    alert(result);
                     $("#resultaat").html(result);
                 }
         });
@@ -19,7 +16,6 @@
     $(document).ready(function() {
         haaloverzicht();
         $( ".wijzigFaq" ).click(function() {    
-            alert("klik ok");
             haaloverzicht ();
         });
         
@@ -50,7 +46,7 @@
 
 </div>
 
-
+         
 <div class="modal fade" id="faqModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
