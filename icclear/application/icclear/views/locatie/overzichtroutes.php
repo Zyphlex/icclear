5e9d6845746a9bac6145a57dcf1e52c284035ea2
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-26 12:30:10 +0000

    routes model gebouw bij route opvragen

--- icclear/application/icclear/views/locatie/overzichtroutes.php
+++ icclear/application/icclear/views/locatie/overzichtroutes.php
@@ -1,5 +1,18 @@
 <?php foreach ($routes as $route) { ?>
+<div class="row">
+    <div class="col-md-7">
     <h3><?php echo $route->vertrekPunt ?></h3>
     <p><?php echo $route->beschrijving ?></p>
+    </div>
+    <div class="col-md-5">
+            <iframe
+                width="200"
+                height="200"
+                frameborder="0" style="border:0"
+                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAiFkc15nXtE7Tx3TjKmPka06bDVVq1KOU
+                &q=Gare+de+Charleroi-Sud">
+            </iframe>
+    </div>
     <hr/>
+</div>
 <?php } ?>
