0ba03ce361221ab7448fd963396a9e290efae4c3
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-26 14:31:36 +0000

    activiteit nieuw

--- icclear/application/icclear/views/admin/activiteit/overzicht.php
+++ icclear/application/icclear/views/admin/activiteit/overzicht.php
@@ -11,14 +11,14 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($activiteiten as $inschrijving) { ?>
+            <?php foreach ($activiteiten as $activiteit) { ?>
                 <tr>
-                    <td><?php echo $inschrijving->naam ?></td>
-                    <td><?php echo $inschrijving->conferentie->naam ?></td>
-                    <td><?php echo "€ " . $inschrijving->prijs ?></td>
+                    <td><?php echo $activiteit->naam ?></td>
+                    <td><?php echo $activiteit->conferentie->naam ?></td>
+                    <td><?php echo "€ " . $activiteit->prijs ?></td>
 
-                    <td><?php echo anchor('activiteit/wijzig/' . $inschrijving->id, 'Wijzigen', 'class="btn btn-default"'); ?>
-                    <?php echo anchor('activiteit/verwijder/' . $inschrijving->id, 'Verwijderen', 'class="btn btn-default"'); ?>
+                    <td><?php echo anchor('activiteit/wijzig/' . $activiteit->id, 'Wijzigen', 'class="btn btn-default"'); ?>
+                    <?php echo anchor('activiteit/verwijder/' . $activiteit->id, 'Verwijderen', 'class="btn btn-default"'); ?>
                 </tr>
             <?php } ?>
         </tbody>
