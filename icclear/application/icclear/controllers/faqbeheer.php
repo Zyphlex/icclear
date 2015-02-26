ecb0b81dc04d4d7f3031e9ca973658c7b4fe96c3
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-26 21:10:07 +0000

    faqbeheer controllen opschonen

--- icclear/application/icclear/controllers/faqbeheer.php
+++ icclear/application/icclear/controllers/faqbeheer.php
@@ -32,57 +32,7 @@ class Faqbeheer extends CI_Controller {
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/faq/overzicht', 'footer' => 'main_footer');
         $this->template->load('admin_master', $partials, $data);
     }
-  
-    public function wijzig($id) {
-        $data['user'] = $this->authex->getUserInfo();
-        $data['title'] = 'IC Clear - F.A.Q. wijzigen';
-        $data['active'] = 'admin';
-        $data['conferentieId'] = $this->session->userdata('conferentieId');
-        $this->load->model('faq_model');
-        $data['vraag'] = $this->faq_model->get($id);
-        $data['id'] = $id;        
-        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/faq/wijzigen', 'footer' => 'main_footer');
-        $this->template->load('admin_master', $partials, $data);
-    }
-    
-//    public function update() {
-//        $id = $this->input->post('id');
-//        $vraag = $this->input->post('vraag');
-//        $antwoord = $this->input->post('antwoord');
-//        
-//        $this->load->model('faq_model');
-//        $this->faq_model->update($id, $vraag, $antwoord);
-//        
-//        redirect('faqbeheer/index');
-//    }
-//
-//    public function verwijder($id) {       
-//        $this->load->model('faq_model');
-//        $this->faq_model->delete($id); 
-//        redirect('faqbeheer/index');        
-//    }
-//
-//    public function toevoegen() {
-//        $data['user'] = $this->authex->getUserInfo();
-//        $data['title'] = 'IC Clear - F.A.Q. toevoegen';
-//        $data['active'] = 'admin';
-//        $data['conferentieId'] = $this->session->userdata('conferentieId');                
-//        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/faq/toevoegen', 'footer' => 'main_footer');
-//        $this->template->load('admin_master', $partials, $data);
-//    }
-//    
-//    public function insert() {        
-//        $faq->vraag = $this->input->post('vraag');
-//        $faq->antwoord = $this->input->post('antwoord');
-//        
-//        $this->load->model('faq_model');
-//        $id = $this->faq_model->insert($faq);
-//        
-//        if($id != 0){
-//            redirect('faqbeheer/index');
-//        }
-//    }
-    
+      
     public function overzicht() {        
         $this->load->model('faq_model');
         $data['vragen'] = $this->faq_model->getAll();
