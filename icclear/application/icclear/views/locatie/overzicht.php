df374dd7107822a6eb93a6d81e10e43d4d6a75dd
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-26 12:13:22 +0000

    routes per venue

--- icclear/application/icclear/views/locatie/overzicht.php
+++ icclear/application/icclear/views/locatie/overzicht.php
@@ -38,7 +38,7 @@
             <div class="col-md-6">
                 <div class="panel panel-default">
                     <div class = "panel-heading">                        
-                        <h3>Dag <?php echo $teller .  '  ' . $gebouw->gebouw->naam . '     ' . $gebouw->gebouw->id ?></h3>
+                        <h3>Dag <?php echo $teller .  '  ' . $gebouw->gebouw->naam?></h3>
                     </div>
                     <div class = "panel-body">  
                             <div class="thumbnail">
@@ -58,19 +58,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Routes</h4>
-            </div>
-
-
-            <div class="modal-body">                  
-                <div id="resultaat"></div>
-            </div>
-            
-             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Sluiten</button>
-            </div>
+            <div id="resutlaat"></div>
             
         </div>            
     </div>
--- icclear/application/icclear/views/locatie/overzichtroutes.php
+++ icclear/application/icclear/views/locatie/overzichtroutes.php
@@ -1,5 +1,15 @@
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+    <h4 class="modal-title">Routes</h4>
+</div>
 
-        <?php foreach ($routes as $route) { ?>
-            <h3><?php echo $route->vertrekPunt ?></h3>
-            <p><?php echo $route->beschrijving ?></p>
-        <?php } ?>
+<div class="modal-body">    
+    <?php foreach ($routes as $route) { ?>
+        <h3><?php echo $route->vertrekPunt ?></h3>
+        <p><?php echo $route->beschrijving ?></p>
+    <?php } ?>
+</div>
+
+<div class="modal-footer">
+    <button type="button" class="btn btn-default" data-dismiss="modal">Sluiten</button>
+</div>
