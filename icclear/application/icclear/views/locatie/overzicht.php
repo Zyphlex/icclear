a7f9c11878767241edea71a8474d0adafd15a615
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-02-26 12:07:41 +0000

    routes per venue

--- icclear/application/icclear/views/locatie/overzicht.php
+++ icclear/application/icclear/views/locatie/overzicht.php
@@ -1,4 +1,3 @@
-
 <script type="text/javascript">
     function haaloverzicht ( gebouwId ) {
         $.ajax({type : "GET",
@@ -13,16 +12,16 @@
     $(document).ready(function() {
         $( ".zoekRoutes" ).click(function() {
             haaloverzicht( $(this).data("id") );
-            if ( $(this).html() == "Toon routes" )
-            {                
-                $( "#resultaat" ).slideDown(400);
-                $(this).html("Verberg routes");
-            }
-            else
-            {                
-                $( "#resultaat" ).slideUp(400);
-                $(this).html("Toon routes");
-            }
+//            if ( $(this).html() == "Toon routes" )
+//            {                
+//                $( "#resultaat" ).slideDown(400);
+//                $(this).html("Verberg routes");
+//            }
+//            else
+//            {                
+//                $( "#resultaat" ).slideUp(400);
+//                $(this).html("Toon routes");
+//            }
         });
         
     });
@@ -52,12 +51,27 @@
                     </div>
                 </div>  
             </div>
-        <?php $teller++; } ?>
-    
+        <?php $teller++; } ?>    
 </div>
 
-<div class="row">    
-    <div class="col-md-12">
-        <div id="resultaat"></div>
+<div class="modal fade" id="routesModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Routes</h4>
+            </div>
+
+
+            <div class="modal-body">                  
+                <div id="resultaat"></div>
+            </div>
+            
+             <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Sluiten</button>
+            </div>
+            
+        </div>            
     </div>
-</div> 
+</div>  
