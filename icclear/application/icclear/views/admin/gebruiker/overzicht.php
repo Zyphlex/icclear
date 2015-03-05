27cdafb5738a2dfcaf596ca09d1ba9060d779e13
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-05 17:33:10 +0000

    beheer

--- icclear/application/icclear/views/admin/gebruiker/overzicht.php
+++ icclear/application/icclear/views/admin/gebruiker/overzicht.php
@@ -1,71 +1,213 @@
-<!--<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="myModalLabel">Gebruiker Verwijderen</h4>
-                </div>
-            
-                <div class="modal-body">
-                    <p>Deze gebruiker wordt verwijderd.</p>
-                    <p>Ben je zeker?</p>
-                    <p class="debug-url"></p>
-                </div>
-                
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Annuleer</button>
-                    <a href="#" class="btn btn-danger danger">Verwijder</a>
-                </div>
-            </div>
-        </div>
-    </div>
+<script type="text/javascript">
+    //Gegevens opvragen en tonen
+    function haaloverzicht() {
+        $.ajax({type: "GET",
+            url: site_url + "/gebruiker/overzicht",
+            success: function (result) {
+                $("#resultaat").html(result);
+                maakDetailClick();
+                maakDeleteClick();
+            }
+        });
+    }
 
-    <a data-href="gebruiker/verwijder/30" data-toggle="modal" data-target="#confirm-delete" href="#">Delete record #23</a>
+    //Wijzigen refreshen
+    function refreshData() {
+        haaloverzicht();
+    }
 
+    //Klikken op de Verwijderen knop
+    function maakDeleteClick() {
+        $(".verwijderGebruiker").click(function () {
+            deleteid = $(this).data("id");
+            $("#gebruikerDelete").modal('show');
+        });
+    }
 
-    <script>
-        $('#confirm-delete').on('show.bs.modal', function(e) {
-            $(this).find('.danger').attr('href', $(e.relatedTarget).data('href'));
-            
-            $('.debug-url').html('Delete URL: <strong>' + $(this).find('.danger').attr('href') + '</strong>');
-        })
-    </script>-->
+    //Klikken op de Wijzig knop/Toevoeg knop
+    function maakDetailClick() {
+        $(".wijzigGebruiker").click(function () {
+            var iddb = $(this).data("id");
+            $("#id").val(iddb);
+            if (iddb != 0) {
+                // gegevens ophalen via ajax (doorgeven van server met json)
+                $.ajax({type: "GET",
+                    url: site_url + "/gebruiker/detail",
+                    async: false,
+                    data: {id: iddb},
+                    success: function (result) {
+                        var jobject = jQuery.parseJSON(result);
+                        $("#voornaam").val(jobject.voornaam);
+                        $("#familienaam").val(jobject.familienaam);
+                        $("#geboortedatum").val(jobject.geboortedatum);
+                        $("#emailadres").val(jobject.emailadres);
+                        $("#geslacht").val(jobject.geslacht);
+                        $("#typeId").val(jobject.typeId);
+                        $("#landId").val(jobject.landId);
+                        $("#gemeente").val(jobject.gemeente);
+                        $("#postcode").val(jobject.postcode);
+                        $("#straat").val(jobject.straat);
+                        $("#nummer").val(jobject.nummer);
+                    }
+                });
+            } else {
+                // bij toevoegen gewoon vakken leeg maken
+                $("#voornaam").val("");
+                $("#familienaam").val("");
+                $("#geboortedatum").val("");
+                $("#emailadres").val("");
+                $("#geslacht").val("");
+                $("#typeId").val("");
+                $("#landId").val("");
+                $("#gemeente").val("");
+                $("#postcode").val("");
+                $("#straat").val("");
+                $("#nummer").val("");
+            }
+            // dialoogvenster openen
+            $("#gebruikerModal").modal('show');
+        });
+    }
+
+    $(document).ready(function () {
+        //Link leggen met de knoppen die gemaakt worden in lijst.php
+        maakDetailClick();
+        maakDeleteClick();
+        //Lijst eerste maal ophalen en tonen
+        haaloverzicht();
+
+        //Klikken op "OPSLAAN" in de Detail modal
+        $(".opslaanGebruiker").click(function () {
+            var dataString = $("#JqAjaxForm:eq(0)").serialize();
+            $.ajax({
+                type: "POST",
+                url: site_url + "/gebruiker/update",
+                async: false,
+                data: dataString,
+                dataType: "json"
+            });
+            refreshData();
+            $("#gebruikerModal").modal('hide');
+        });
+
+        //Klikken op "BEVESTIG" in de Delete modal
+        $(".deleteGebruiker").click(function () {
+            $.ajax({
+                type: "POST",
+                url: site_url + "/gebruiker/delete",
+                async: false,
+                data: {id: deleteid},
+                success: function (result) {
+                    if (result == '0') {
+                        alert("Er is iets foutgelopen!");
+                    } else {
+                        refreshData();
+                    }
+                    $("#gebruikerDelete").modal('hide');
+                }
+            });
+        });
+
+    });
+</script>
+
 
 <div class="col-md-10">
 
-    <h1>Gebruikers beheren</h1>  
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Familienaam</th>
-                <th>Voornaam</th>
-                <th>Email</th>
-                <th>Type</th>
-                <th>Beheer</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($gebruikers as $gebruiker) { ?>
-                <tr>
-                    <td><?php echo $gebruiker->familienaam ?></td>
-                    <td><?php echo $gebruiker->voornaam ?></td>
-                    <td><?php echo $gebruiker->emailadres ?></td>
-                    <?php if ($gebruiker->typeId == 1) { ?>
-                        <td>Bezoeker</td>
-                    <?php } else { ?>
-                        <td>Spreker</td>
-                    <?php } ?>
+    <h1>Gebruiker beheren</h1>  
 
-                    <td><?php echo anchor('gebruiker/wijzig/' . $gebruiker->id, 'Wijzigen', 'class="btn btn-default"'); ?>
-                    <?php echo anchor('gebruiker/verwijder/' . $gebruiker->id, 'Verwijderen', 'class="btn btn-default"'); ?>
-                    </td>
-                </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+    <div id="resultaat"></div>        
 
-    <?php echo anchor('admin', 'Annuleren', 'class="btn btn-default"'); ?> 
-    <?php echo anchor('gebruiker/nieuw', 'Nieuwe gebruiker toevoegen', 'class="btn btn-default"'); ?> 
+    <?php echo anchor('admin', 'Annuleren', 'class="btn btn-default"'); ?>
+
+    <button class="wijzigGebruiker btn btn-primary" data-id="0">Nieuwe gebruiker Toevoegen</button>
 
 </div>
+
+
+<!-- MODAL VOOR DETAILS -->         
+<div class="modal fade" id="gebruikerModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title"></h4>
+            </div>
+
+            <div class="modal-body">                  
+
+                <form id="JqAjaxForm">
+                    <input type="hidden" name="id" id="id" />
+                    <p><?php echo form_label('Voornaam:', 'voornaam'); ?></p>
+                    <p><?php echo form_input(array('name' => 'voornaam', 'id' => 'voornaam', 'class' => 'form-control')); ?></p>
+
+                    <p><?php echo form_label('Familienaam:', 'familienaam'); ?></td>
+                    <p><?php echo form_input(array('name' => 'familienaam', 'id' => 'familienaam', 'class' => 'form-control')); ?></p>
+
+                    <p><?php echo form_label('Emailadres:', 'emailadres'); ?></td>
+                    <p><?php echo form_input(array('name' => 'emailadres', 'id' => 'emailadres', 'class' => 'form-control')); ?></p>
+
+                    <p><?php echo form_label('Geboortedatum:', 'geboortedatum'); ?></td>
+                    <p><?php echo form_input(array('name' => 'geboortedatum', 'id' => 'geboortedatum', 'class' => 'form-control')); ?></p>
+
+                    <p><?php echo form_label('Geslacht:', 'geslacht'); ?></td>
+                    <p><?php echo form_radio(array('name' => 'geslacht', 'id' => 'geslacht', 'class' => 'form-control')); ?>
+                        <?php echo form_radio(array('name' => 'geslacht', 'id' => 'geslacht', 'class' => 'form-control')); ?></p>
+                    
+                    <p><?php echo form_label('Type:', 'typeId'); ?></td>
+                    <p><?php echo form_radio(array('name' => 'typeId', 'id' => 'typeId', 'class' => 'form-control')); ?>
+                        <?php echo form_radio(array('name' => 'typeId', 'id' => 'typeId', 'class' => 'form-control')); ?></p>
+                    
+                    <p><?php echo form_label('Land:', 'landId'); ?></td>
+                    <p><?php echo form_dropdown(array('name' => 'landId', 'id' => 'landId', 'class' => 'form-control')); ?></p>
+                    
+                    <p><?php echo form_label('Gemeente:', 'gemeente'); ?></td>
+                    <p><?php echo form_input(array('name' => 'gemeente', 'id' => 'gemeente', 'class' => 'form-control')); ?></p>
+
+                    <p><?php echo form_label('Postcode:', 'postcode'); ?></td>
+                    <p><?php echo form_input(array('name' => 'postcode', 'id' => 'postcode', 'class' => 'form-control')); ?></p>
+
+                    <p><?php echo form_label('Straat:', 'straat'); ?></td>
+                    <p><?php echo form_input(array('name' => 'straat', 'id' => 'straat', 'class' => 'form-control')); ?></p>
+
+                    <p><?php echo form_label('Huisnummer:', 'nummer'); ?></td>
+                    <p><?php echo form_input(array('name' => 'nummer', 'id' => 'nummer', 'class' => 'form-control')); ?></p>
+
+                </form>
+
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="opslaanGebruiker btn btn-primary">Opslaan</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Annuleren</button>
+            </div>
+
+        </div>            
+    </div>
+</div>  
+
+
+<!-- MODAL VOOR VERWIJDEREN -->  
+<div class="modal fade" id="gebruikerDelete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">OPGELET!</h4>
+            </div>
+
+            <div class="modal-body">                  
+                <p>Bent u zeker dat u deze gebruiker wilt verwijderen?</p>  
+                <p class="italic">Dit kan niet ongedaan gemaakt worden!</p>                  
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="deleteGebruiker btn btn-primary">Bevestig</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Annuleren</button>
+            </div>
+
+        </div>            
+    </div>
+</div>  
