8aeaa4128bc0633f56fad8621e3941cd6ff84167
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-05 17:33:02 +0000

    beheer

--- icclear/application/icclear/controllers/gebruiker.php
+++ icclear/application/icclear/controllers/gebruiker.php
@@ -56,8 +56,8 @@ class Gebruiker extends CI_Controller {
         $gebruiker->geboortedatum = $this->input->post('geboortedatum');
         $gebruiker->emailadres = $this->input->post('emailadres');
         $gebruiker->geslacht = $this->input->post('geslacht');
-        $gebruiker->typeId = $this->input->post('type');
-        $gebruiker->landId = $this->input->post('land');
+        $gebruiker->typeId = $this->input->post('typeId');
+        $gebruiker->landId = $this->input->post('landId');
         $gebruiker->gemeente = $this->input->post('gemeente');
         $gebruiker->postcode = $this->input->post('postcode');
         $gebruiker->straat = $this->input->post('straat');
