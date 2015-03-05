878650192835c9f47864c8897f5dfdf84ea600c9
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-05 20:11:02 +0000

    admin/faq/toevoegen naar CI code

---
+++ icclear/application/icclear/views/admin/faq/toevoegen.php
@@ -0,0 +1,45 @@
+<div class="col-md-10">  
+
+    <div class="row">
+        <div class="col-md-12">
+            <h1>FAQ toevoegen</h1>
+        </div>
+    </div>
+    <?php
+    $attributes = array('name' => 'myform', 'id' => 'myform');
+    echo form_open('faqbeheer/insert', $attributes);
+    ?>           
+    <div class="row">
+        <div class="col-md-6">  
+
+            <div class="row">
+                <div class="col-md-4">   
+                    <?php echo form_label('Vraag:', 'vraag'); ?>
+                </div>
+
+                <div class="col-md-8">
+                    <?php echo form_input(array('name' => 'vraag', 'id' => 'vraag', 'class' => 'form-control', 'required' => 'required')); ?>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4">   
+                    <?php echo form_label('Antwoord:', 'antwoord'); ?>                    
+                </div>
+
+                <div class="col-md-8">  
+                    <?php echo form_textarea(array('name' => 'antwoord', 'id' => 'antwoord', 'class' => 'form-control', 'required' => 'required', 'rows' => '5', 'cols' => '10')); ?>
+                    //<textarea name="antwoord" id="antwoord" class="form-control" required="required" rows="5" cols="10"></textarea>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <?php echo anchor('faqbeheer', 'Annuleer', 'class="btn btn-default"'); ?>
+    <?php echo form_submit('faqbeheer/opslaan', 'Opslaan', 'class="btn btn-default"'); ?>
+
+    <?php echo form_close(); ?>
+
+</div>
