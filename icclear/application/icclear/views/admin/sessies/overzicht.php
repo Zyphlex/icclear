fcc807ce39645a281c780ccd34a313ac87f9c5c6
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-11 13:40:18 +0000

    sessie per conf beheren

--- icclear/application/icclear/views/admin/sessies/overzicht.php
+++ icclear/application/icclear/views/admin/sessies/overzicht.php
@@ -32,14 +32,18 @@
                         <?php foreach ($sessies as $sessie) { ?>
                             <tr>
                                 <td><?php echo $sessie->onderwerp ?></td>
-                                <td><?php echo toDDMMYYYY($sessie->conferentiedag->datum) ?></td>
-                                <td><?php echo $sessie->planning->beginUur ?></td>
-                                <td><?php echo $sessie->planning->eindUur ?></td>
-                                <td><?php echo $sessie->zaal->naam ?></td>
-                                <td>
-                                    <?php echo anchor('sessies/wijzigen/' . $sessie->id, 'Wijzigen','class="btn btn-default"'); ?>
-                                    <?php echo anchor('sessies/verwijderen' . $sessie->id, 'Verwijderen','class="btn btn-default"'); ?>
-                                </td>
+                                <?php if($sessie->planning != null) { ?>
+                                    <td><?php echo toDDMMYYYY($sessie->conferentiedag->datum) ?></td>
+                                    <td><?php echo $sessie->planning->beginUur ?></td>
+                                    <td><?php echo $sessie->planning->eindUur ?></td>
+                                    <td><?php echo $sessie->zaal->naam ?></td>
+                                    <td>
+                                        <?php echo anchor('sessies/wijzigen/' . $sessie->id, 'Wijzigen','class="btn btn-default"'); ?>
+                                        <?php echo anchor('sessies/verwijderen' . $sessie->id, 'Verwijderen','class="btn btn-default"'); ?>
+                                    </td>
+                                <?php } else { ?>
+                                    <td colspan="5">Nog niet ingepland</td>
+                                <?php } ?>
                             </tr>
                         <?php } ?>
                     </tbody>
