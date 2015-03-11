2d56d1247838efe1426c6cb637a738b4c1325924
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-11 15:38:23 +0000

    beheer admin

--- icclear/application/icclear/views/admin/gebruiker/overzicht_admin.php
+++ icclear/application/icclear/views/admin/gebruiker/overzicht_admin.php
@@ -1,31 +1,176 @@
+<script type="text/javascript">
+    //Gegevens opvragen en tonen
+    function haaloverzicht() {
+        $.ajax({type: "GET",
+            url: site_url + "/adminbeheer/overzicht",
+            success: function (result) {
+                $("#resultaat").html(result);
+                maakDetailClick();
+                maakDeleteClick();
+            }
+        });
+    }
+
+    //Wijzigen refreshen
+    function refreshData() {
+        haaloverzicht();
+    }
+
+    //Klikken op de Verwijderen knop
+    function maakDeleteClick() {
+        $(".verwijderAdmin").click(function () {
+            deleteid = $(this).data("id");
+            $("#adminDelete").modal('show');
+        });
+    }
+
+    //Klikken op de Wijzig knop/Toevoeg knop
+    function maakDetailClick() {
+        $(".wijzigAdmin").click(function () {
+            var iddb = $(this).data("id");
+            $("#id").val(iddb);
+            if (iddb != 0) {
+                // gegevens ophalen via ajax (doorgeven van server met json)
+                $.ajax({type: "GET",
+                    url: site_url + "/adminbeheer/detail",
+                    async: false,
+                    data: {id: iddb},
+                    success: function (result) {
+                        var jobject = jQuery.parseJSON(result);
+                        $("#gebruikersnaam").val(jobject.gebruikersnaam);
+                        $("#voornaam").val(jobject.voornaam);
+                        $("#familienaam").val(jobject.familienaam);
+                        $("#emailadres").val(jobject.emailadres);
+                    }
+                });
+            } else {
+                // bij toevoegen gewoon vakken leeg maken
+                $("#gebruikersnaam").val("");
+                $("#voornaam").val("";
+                        $("#familienaam").val("");
+                $("#emailadres").val("");
+            }
+            // dialoogvenster openen
+            $("#adminModal").modal('show');
+        });
+    }
+
+    $(document).ready(function () {
+        //Link leggen met de knoppen die gemaakt worden in lijst.php
+        maakDetailClick();
+        maakDeleteClick();
+        //Lijst eerste maal ophalen en tonen
+        haaloverzicht();
+
+        //Klikken op "OPSLAAN" in de Detail modal
+        $(".opslaanAdmin").click(function () {
+            var dataString = $("#JqAjaxForm:eq(0)").serialize();
+            $.ajax({
+                type: "POST",
+                url: site_url + "/adminbeheer/update",
+                async: false,
+                data: dataString,
+                dataType: "json"
+            });
+            refreshData();
+            $("#adminModal").modal('hide');
+        });
+
+        //Klikken op "BEVESTIG" in de Delete modal
+        $(".deleteAdmin").click(function () {
+            $.ajax({
+                type: "POST",
+                url: site_url + "/adminbeheer/delete",
+                async: false,
+                data: {id: deleteid},
+                success: function (result) {
+                    if (result == '0') {
+                        alert("Er is iets foutgelopen!");
+                    } else {
+                        refreshData();
+                    }
+                    $("#adminDelete").modal('hide');
+                }
+            });
+        });
+
+    });
+</script>
 
 
-    <div class="col-md-10">
-        
-        <h1>Admins beheren</h1>  
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Familienaam</th>
-                                <th>Voornaam</th>
-                                <th>Email</th>
-                                <th>Beheer</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach($admins as $admin) {?>
-                            <tr>
-                                <td><?php echo $admin->familienaam ?></td>
-                                <td><?php echo $admin->voornaam ?></td>
-                                <td><?php echo $admin->emailadres ?></td>
-                                <td><?php echo anchor('gebruiker/wijzigAdmin/' . $admin->id, 'Wijzigen','class="btn btn-default"'); ?>
-                                    <?php echo anchor('gebruiker/verwijderAdmin/' . $admin->id, 'Verwijderen','class="btn btn-default"'); ?></td>
-                            </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
-        
-        <?php echo anchor('admin', 'Annuleren','class="btn btn-default"'); ?> 
-        <?php echo anchor('gebruiker/nieuwAdmin', 'Nieuwe admin toevoegen','class="btn btn-default"'); ?> 
-        
+<div class="col-md-10">
+
+    <h1>Admin beheren</h1>  
+
+    <div id="resultaat"></div>        
+
+    <?php echo anchor('admin', 'Annuleren', 'class="btn btn-default"'); ?>
+
+    <button class="wijzigAdmin btn btn-primary" data-id="0">Nieuwe Admin Toevoegen</button>
+
+</div>
+
+
+<!-- MODAL VOOR DETAILS -->         
+<div class="modal fade" id="adminModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title"></h4>
+            </div>
+
+            <div class="modal-body">                  
+
+                <form id="JqAjaxForm">
+                    <input type="hidden" name="id" id="id" />
+                    <p><?php echo form_label('Gebruikersnaam:', 'gebruikersnaam'); ?></p>
+                    <p><?php echo form_input(array('name' => 'gebruikersnaam', 'id' => 'gebruikersnaam', 'class' => 'form-control')); ?></p>
+
+                    <p><?php echo form_label('Voornaam:', 'voornaam'); ?></p>
+                    <p><?php echo form_input(array('name' => 'voornaam', 'id' => 'voornaam', 'class' => 'form-control')); ?></p>
+                    
+                    <p><?php echo form_label('Familienaam:', 'familienaam'); ?></p>
+                    <p><?php echo form_input(array('name' => 'familienaam', 'id' => 'familienaam', 'class' => 'form-control')); ?></p>
+                    
+                    <p><?php echo form_label('Emailadres:', 'emailadres'); ?></p>
+                    <p><?php echo form_input(array('name' => 'emailadres', 'id' => 'emailadres', 'class' => 'form-control')); ?></p>
+
+                </form>
+
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="opslaanAdmin btn btn-primary">Opslaan</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Annuleren</button>
+            </div>
+
+        </div>            
     </div>
+</div>  
+
+
+<!-- MODAL VOOR VERWIJDEREN -->  
+<div class="modal fade" id="faqDelete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">OPGELET!</h4>
+            </div>
+
+            <div class="modal-body">                  
+                <p>Bent u zeker dat u deze vraag wilt verwijderen?</p>  
+                <p class="italic">Dit kan niet ongedaan gemaakt worden!</p>                  
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="deleteFaq btn btn-primary">Bevestig</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Annuleren</button>
+            </div>
+
+        </div>            
+    </div>
+</div>  
