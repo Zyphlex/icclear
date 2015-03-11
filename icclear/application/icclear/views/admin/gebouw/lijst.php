a88d1055c6ef99fe4768a49152f1cc0a4865facc
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-11 14:15:23 +0000

    lijst gebouw

---
+++ icclear/application/icclear/views/admin/gebouw/lijst.php
@@ -0,0 +1,28 @@
+<table class="table table-beheer">
+    <thead>
+        <tr>
+            <th>Naam</th>
+            <th>Gemeente</th>
+            <th>Postcode</th>
+            <th>Straat</th>
+            <th>Nummer</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($gebouwen as $gebouw) { ?>
+            <tr>
+                <td><?php echo $gebouw->naam ?></td>
+                <td><?php echo $gebouw->gemeente ?></td>
+                <td><?php echo $gebouw->postcode ?></td>
+                <td><?php echo $gebouw->straat ?></td>
+                <td><?php echo $gebouw->nummer ?></td>
+                <td>
+                    <p>                                        
+                        <button class="wijzigGebouw btn btn-primary" data-id="<?php echo $gebouw->id ?>">Wijzigen</button>
+                        <button class="verwijderGebwouw btn btn-danger" data-id="<?php echo $gebouw->id ?>">Verwijderen</button> 
+                    </p>                                 
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>      
