c1417c4b5ddb8d67e1ed99572e903a0b818eb938
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-11 14:47:04 +0000

    sessie per conf beheren

--- icclear/application/icclear/views/admin/sessies/lijst.php
+++ icclear/application/icclear/views/admin/sessies/lijst.php
@@ -18,17 +18,13 @@
                     <td><?php echo $sessie->planning->beginUur ?></td>
                     <td><?php echo $sessie->planning->eindUur ?></td>
                     <td><?php echo $sessie->planning->zaal->naam ?></td>
-                    <td>
-                        <?php echo anchor('sessies/wijzigen/' . $sessie->id, 'Wijzigen', 'class="btn btn-default"'); ?>
-                        <?php echo anchor('sessies/verwijderen' . $sessie->id, 'Verwijderen', 'class="btn btn-default"'); ?>
-                    </td>
                 <?php } else { ?>
                     <td colspan="5">Nog niet ingepland</td>
                 <?php } ?>
                 <td>
                     <p>                                        
-                        <button class="wijzigSessie btn btn-primary" data-id="<?php echo $vraag->id ?>">Wijzigen</button>
-                        <button class="verwijderSessie btn btn-danger" data-id="<?php echo $vraag->id ?>">Verwijderen</button> 
+                        <button class="wijzigSessie btn btn-primary" data-id="<?php echo $sessie->id ?>">Wijzigen</button>
+                        <button class="verwijderSessie btn btn-danger" data-id="<?php echo $sessie->id ?>">Verwijderen</button> 
                     </p>                                 
                 </td>
             </tr>
