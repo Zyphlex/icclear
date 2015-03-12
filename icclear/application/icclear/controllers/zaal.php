421099e88e99e0664885416890c68f6ff7a52117
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-12 14:37:01 +0000

    zaal controller

---
+++ icclear/application/icclear/controllers/zaal.php
@@ -0,0 +1,81 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Zaal extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper(array('form', 'url'));
+        if (!$this->authex->loggedIn()) {
+            redirect('logon/aanmelden');
+            //voorlopig
+        } else {
+            $user = $this->authex->getUserInfo();
+            if ($user->typeId < 3) {
+                redirect('logon/aanmelden');
+                //voorlopig
+            }
+        }
+    }
+
+    public function index() {
+        $data['user'] = $this->authex->getUserInfo();
+        $data['title'] = 'IC Clear - Zaal';
+        $data['active'] = 'admin';
+
+        $data['conferentieId'] = $this->session->userdata('conferentieId');
+        $this->load->model('zaal_model');
+
+        $data['zalen'] = $this->zaal_model->getAll();
+
+        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/zaal/overzicht', 'footer' => 'main_footer');
+        $this->template->load('admin_master', $partials, $data);
+    }
+
+    public function overzicht() {
+        $this->load->model('zaal_model');
+        $data['zalen'] = $this->zaal_model->getAll();
+
+        $this->load->view('admin/zaal/lijst', $data);
+    }
+
+    public function detail() {
+        $id = $this->input->get('id');
+
+        $this->load->model('zaal_model');
+        $zaal = $this->zaal_model->get($id);
+
+        echo json_encode($zaal);
+    }
+
+    public function update() {
+        $zaal->id = $this->input->post('id');
+        $zaal->naam = $this->input->post('naam');
+        $zaal->gebouwId = $this->input->post('postcode');
+        $zaal->maximumAantalPersonen = $this->input->post('maximumAantalPersonen');
+
+        $this->load->model('zaal_model');
+        if ($zaal->id == 0) {
+            $id = $this->zaal_model->insert($zaal);
+        } else {
+            $this->zaal_model->update($zaal);
+        }
+
+        echo $id;
+    }
+
+    public function delete() {
+        $id = $this->input->post('id');
+
+        $this->load->model('zaal_model');
+        $deleted = $this->zaal_model->delete($id);
+
+        echo $deleted;
+    }
+
+}
+
+/* End of file gebouw.php */
+/* Location: ./application/controllers/gebouw.php */
