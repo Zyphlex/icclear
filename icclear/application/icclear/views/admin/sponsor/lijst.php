3f64c7782862551d3affd7589c30dfef5606288e
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-12 14:40:03 +0000

    Admin_sidenav

--- icclear/application/icclear/views/admin/sponsor/lijst.php
+++ icclear/application/icclear/views/admin/sponsor/lijst.php
@@ -1,7 +1,7 @@
+<h2>Sponsors</h2>
 <table class = "table table-beheer">
         <thead>
             <tr>
-                <th>#</th>
                 <th>Naam</th>
                 <th>Land</th>
                 <th>Postcode</th>
@@ -15,6 +15,43 @@
             <?php
             foreach ($sponsors as $sponsor) {
                 if ($sponsor->type == 'Sponsor') {
+                    echo '<tr>' . "\n";
+                    echo '<td>' . $sponsor->naam . '</td>' . "\n";
+                    echo '<td>' . $sponsor->land->naam . '</td>' . "\n";
+                    echo '<td>' . $sponsor->postcode . '</td>' . "\n";
+                    echo '<td>' . $sponsor->gemeente . '</td>' . "\n";
+                    echo '<td>' . $sponsor->straat . '</td>' . "\n";
+                    echo '<td>' . $sponsor->nummer . '</td>' . "\n";?>
+        <td>
+            <p>
+                <button class="wijzigSponsor btn btn-primary" data-id="<?php echo $sponsor->id ?>">Wijzigen</button>
+                <button class="verwijderSponsor btn btn-danger" data-id="<?php echo $sponsor->id ?>">Verwijderen</button>
+            </p>
+        </td>
+                    <?php echo '</tr>' . "\n";
+                }
+            }
+            ?>
+        </tbody>
+    </table>
+
+<h2>Partners</h2>
+<table class = "table table-beheer">
+        <thead>
+            <tr>
+                <th>Naam</th>
+                <th>Land</th>
+                <th>Postcode</th>
+                <th>Gemeente</th>                    
+                <th>Straat</th>                    
+                <th>Nummer</th>       
+                <th>Beheer</th>
+            </tr>
+        </thead>  
+        <tbody>                              
+            <?php
+            foreach ($sponsors as $sponsor) {
+                if ($sponsor->type == 'Partner') {
                     echo '<tr>' . "\n";
                     echo '<td>' . $sponsor->naam . '</td>' . "\n";
                     echo '<td>' . $sponsor->land->naam . '</td>' . "\n";
