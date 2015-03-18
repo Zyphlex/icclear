4e9a11f2af3a8e53497f77bc37817562a391a1b4
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-18 15:46:40 +0000

    extra knop

--- icclear/application/icclear/views/admin/dashboard.php
+++ icclear/application/icclear/views/admin/dashboard.php
@@ -8,5 +8,5 @@
     <?php echo anchor('email/', 'Emails', 'class="btn btn-default"'); ?>
     <?php echo anchor('aankondiging/', 'Aankondigingen', 'class="btn btn-default"'); ?>
     <?php echo anchor('inschrijven/opvolgen', 'Betalingen Opvolgen', 'class="btn btn-default"'); ?>
-    <?php echo anchor('statistieken', 'Statistieken opvragen', 'class="btn btn-default"'); ?>
+    <?php echo anchor('statistiek/', 'Statistieken opvragen', 'class="btn btn-default"'); ?>
 </div>
