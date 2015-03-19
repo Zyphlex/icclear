ff904f994e92d6b599811d7894cab4b15455a8f9
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-19 14:07:40 +0000

    registreer

--- icclear/application/icclear/views/main_header.php
+++ icclear/application/icclear/views/main_header.php
@@ -44,3 +44,7 @@
 <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog"><div class="modal-content"></div></div>
 </div>
+
+<div class="modal fade" id="myModal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg"><div class="modal-content"></div></div>
+</div>
