27e898032ce4763d780f3565e459d43dd10a114e
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-19 15:10:05 +0000

    Sponsor view, td tags buiten echo

--- icclear/application/icclear/views/admin/sponsor/lijst.php
+++ icclear/application/icclear/views/admin/sponsor/lijst.php
@@ -1,73 +1,77 @@
 <h2>Sponsors</h2>
 <table class = "table table-beheer">
-        <thead>
-            <tr>
-                <th>Naam</th>
-                <th>Land</th>
-                <th>Postcode</th>
-                <th>Gemeente</th>                    
-                <th>Straat</th>                    
-                <th>Nummer</th>       
-                <th>Beheer</th>
-            </tr>
-        </thead>  
-        <tbody>                              
-            <?php
-            foreach ($sponsors as $sponsor) {
-                if ($sponsor->type == 'Sponsor') {
-                    echo '<tr>' . "\n";
-                    echo '<td>' . $sponsor->naam . '</td>' . "\n";
-                    echo '<td>' . $sponsor->land->naam . '</td>' . "\n";
-                    echo '<td>' . $sponsor->postcode . '</td>' . "\n";
-                    echo '<td>' . $sponsor->gemeente . '</td>' . "\n";
-                    echo '<td>' . $sponsor->straat . '</td>' . "\n";
-                    echo '<td>' . $sponsor->nummer . '</td>' . "\n";?>
-        <td>
-            <p>
-                <button class="wijzigSponsor btn btn-primary" data-id="<?php echo $sponsor->id ?>">Wijzigen</button>
-                <button class="verwijderSponsor btn btn-danger" data-id="<?php echo $sponsor->id ?>">Verwijderen</button>
-            </p>
-        </td>
-                    <?php echo '</tr>' . "\n";
-                }
+    <thead>
+        <tr>
+            <th>Naam</th>
+            <th>Land</th>
+            <th>Postcode</th>
+            <th>Gemeente</th>                    
+            <th>Straat</th>                    
+            <th>Nummer</th>       
+            <th>Beheer</th>
+        </tr>
+    </thead>  
+    <tbody>                              
+        <?php
+        foreach ($sponsors as $sponsor) {
+            if ($sponsor->type == 'Sponsor') {
+                ?>
+                <tr>
+                    <td> <?php echo $sponsor->naam; ?></td>
+                    <td> <?php echo $sponsor->land->naam; ?> </td>
+                    <td> <?php echo $sponsor->postcode; ?> </td>
+                    <td> <?php echo $sponsor->gemeente; ?> </td>
+                    <td> <?php echo $sponsor->straat; ?> </td>
+                    <td> <?php echo $sponsor->nummer; ?> </td>
+                    <td>
+                        <p>
+                            <button class="wijzigSponsor btn btn-primary" data-id="<?php echo $sponsor->id ?>">Wijzigen</button>
+                            <button class="verwijderSponsor btn btn-danger" data-id="<?php echo $sponsor->id ?>">Verwijderen</button>
+                        </p>
+                    </td>
+                </tr>
+                <?php
             }
-            ?>
-        </tbody>
-    </table>
+        }
+        ?>
+    </tbody>
+</table>
 
 <h2>Partners</h2>
 <table class = "table table-beheer">
-        <thead>
-            <tr>
-                <th>Naam</th>
-                <th>Land</th>
-                <th>Postcode</th>
-                <th>Gemeente</th>                    
-                <th>Straat</th>                    
-                <th>Nummer</th>       
-                <th>Beheer</th>
-            </tr>
-        </thead>  
-        <tbody>                              
-            <?php
-            foreach ($sponsors as $sponsor) {
-                if ($sponsor->type == 'Partner') {
-                    echo '<tr>' . "\n";
-                    echo '<td>' . $sponsor->naam . '</td>' . "\n";
-                    echo '<td>' . $sponsor->land->naam . '</td>' . "\n";
-                    echo '<td>' . $sponsor->postcode . '</td>' . "\n";
-                    echo '<td>' . $sponsor->gemeente . '</td>' . "\n";
-                    echo '<td>' . $sponsor->straat . '</td>' . "\n";
-                    echo '<td>' . $sponsor->nummer . '</td>' . "\n";?>
-        <td>
-            <p>
-                <button class="wijzigSponsor btn btn-primary" data-id="<?php echo $sponsor->id ?>">Wijzigen</button>
-                <button class="verwijderSponsor btn btn-danger" data-id="<?php echo $sponsor->id ?>">Verwijderen</button>
-            </p>
-        </td>
-                    <?php echo '</tr>' . "\n";
-                }
+    <thead>
+        <tr>
+            <th>Naam</th>
+            <th>Land</th>
+            <th>Postcode</th>
+            <th>Gemeente</th>                    
+            <th>Straat</th>                    
+            <th>Nummer</th>       
+            <th>Beheer</th>
+        </tr>
+    </thead>  
+    <tbody>                              
+        <?php
+        foreach ($sponsors as $sponsor) {
+            if ($sponsor->type == 'Partner') {
+                ?>
+                <tr>
+                    <td> <?php echo $sponsor->naam; ?></td>
+                    <td> <?php echo $sponsor->land->naam; ?> </td>
+                    <td> <?php echo $sponsor->postcode; ?> </td>
+                    <td> <?php echo $sponsor->gemeente; ?> </td>
+                    <td> <?php echo $sponsor->straat; ?> </td>
+                    <td> <?php echo $sponsor->nummer; ?> </td>
+                    <td>
+                        <p>
+                            <button class="wijzigSponsor btn btn-primary" data-id="<?php echo $sponsor->id ?>">Wijzigen</button>
+                            <button class="verwijderSponsor btn btn-danger" data-id="<?php echo $sponsor->id ?>">Verwijderen</button>
+                        </p>
+                    </td>
+                </tr>
+                <?php
             }
-            ?>
-        </tbody>
-    </table>
+    }
+    ?>
+</tbody>
+</table>
