dd469dbca7bbb64be7b0f682c03261fe8870bfb5
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-25 17:40:40 +0000

    countgekeurden

--- icclear/application/icclear/views/admin/dashboard.php
+++ icclear/application/icclear/views/admin/dashboard.php
@@ -2,7 +2,7 @@
     <h1>Conferentie <?php echo $conferentie ?> beheren</h1>     
 
     <h4>DASHBOARD</h4>    
-    <h5>NAAM: <?php echo $conferentie ?></h5>                 
+    <h5>Naam: <?php echo $conferentie ?></h5>                 
     <?php echo anchor('email/', 'Emails', 'class="btn btn-default"'); ?>
     <?php echo anchor('aankondiging/', 'Aankondigingen', 'class="btn btn-default"'); ?>
     <?php echo anchor('inschrijven/opvolgen', 'Betalingen Opvolgen', 'class="btn btn-default"'); ?>            
