75d95c202d340c0fd67e922ed7960144afcb3a65
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-25 18:06:28 +0000

    statistieken

--- icclear/application/icclear/views/admin/dashboard.php
+++ icclear/application/icclear/views/admin/dashboard.php
@@ -1,14 +1,13 @@
 <div class="col-md-10">       
     <h1>Conferentie <?php echo $conferentie ?> beheren</h1>     
-
-    <h4>DASHBOARD<span class="glyphicon glyphicon-th-large"></span></h4>    
-    <h5>Naam: <?php echo $conferentie ?></h5>                 
+    <h4>DASHBOARD <span class="glyphicon glyphicon-th-large"></span></h4>                         
     <?php echo anchor('email/', 'Emails', 'class="btn btn-default"'); ?>
     <?php echo anchor('aankondiging/', 'Aankondigingen', 'class="btn btn-default"'); ?>
     <?php echo anchor('inschrijven/opvolgen', 'Betalingen Opvolgen', 'class="btn btn-default"'); ?>            
 </div>
 <div class="col-md-10">    
     <h4>Conferentie <span class="glyphicon glyphicon-pushpin"></span></h4>
+    <h5>Naam: <?php echo $conferentie ?></h5>
     <p>Periode: <?php echo $dataConferentie->beginDatum; ?> tot <?php echo $dataConferentie->eindDatum; ?></p>    
     <p>Stad: <?php echo $dataConferentie->stad; ?></p>
     <p>Maximum aantal inschrijvingen: <?php echo $dataConferentie->maxInschrijvingen; ?></p>
