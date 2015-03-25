707759011f29388a36c5623d268606655310506c
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-25 18:01:53 +0000

    statistieken

--- icclear/application/icclear/views/admin/dashboard.php
+++ icclear/application/icclear/views/admin/dashboard.php
@@ -1,7 +1,7 @@
 <div class="col-md-10">       
     <h1>Conferentie <?php echo $conferentie ?> beheren</h1>     
 
-    <h4>DASHBOARD</h4>    
+    <h4><span class="glyphicon glyphicon-th-large"></span>DASHBOARD</h4>    
     <h5>Naam: <?php echo $conferentie ?></h5>                 
     <?php echo anchor('email/', 'Emails', 'class="btn btn-default"'); ?>
     <?php echo anchor('aankondiging/', 'Aankondigingen', 'class="btn btn-default"'); ?>
