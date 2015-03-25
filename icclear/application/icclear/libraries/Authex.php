da996afbc358b5cf19a7229b02a14eb10549f13b
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-25 18:21:16 +0000

    authex

--- icclear/application/icclear/libraries/Authex.php
+++ icclear/application/icclear/libraries/Authex.php
@@ -69,12 +69,12 @@ class Authex {
         $CI->session->unset_userdata('user_id');
     }        
 
-    function register($gebruikersnaam, $familienaam, $voornaam, $email, $wachtwoord, $geboortedatum, $geslacht, $land, $gemeente, $postcode, $straat, $huisnummer, $generatedKey)
+    function register($gebruikersnaam, $familienaam, $voornaam, $email, $wachtwoord, $generatedKey)
     {
         // nieuwe gebruiker registreren als email nog niet bestaat
         $CI = & get_instance();
         if ($CI->logon_model->emailVrij($email)) {
-            $id = $CI->logon_model->insert($gebruikersnaam, $familienaam, $voornaam, $email, $wachtwoord, $geboortedatum, $geslacht, $land, $gemeente, $postcode, $straat, $huisnummer, $generatedKey);
+            $id = $CI->logon_model->insert($gebruikersnaam, $familienaam, $voornaam, $email, $wachtwoord, $generatedKey);
             return $id;
         } else {
             return 0;
