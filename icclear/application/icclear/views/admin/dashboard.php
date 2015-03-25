db5bc88c34566046447c286797f15b60ca752d84
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-25 17:59:50 +0000

    statistieken

--- icclear/application/icclear/views/admin/dashboard.php
+++ icclear/application/icclear/views/admin/dashboard.php
@@ -12,11 +12,11 @@
     <p>Periode: <?php echo $dataConferentie->beginDatum; ?> tot <?php echo $dataConferentie->eindDatum; ?></p>    
     <p>Stad: <?php echo $dataConferentie->stad; ?></p>
     <p>Maximum aantal inschrijvingen: <?php echo $dataConferentie->maxInschrijvingen; ?></p>
-    <p>Status: </p>
+    <p>Status: <?php echo $status->status; ?></p>
     <br>
     <h4>Statistieken <span class="glyphicon glyphicon-stats"></span></h4>
     <p>Aantal inschrijvingen: <?php echo $aantalInschrijvingen; ?></p>
     <p>Aantal gekeurde sessies: <?php echo $gekeurdeSessies; ?></p> 
     <p>Aantal ongekeurde sessies: <?php echo $ongekeurdeSessies; ?></p>     
-    <p>Aantal activiteiten: <?php echo $status->status; ?></p>
+    <p>Aantal activiteiten: <?php echo $activiteiten; ?></p>
 </div>
