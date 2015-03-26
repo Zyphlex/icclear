fcb76a9eb9efc9c4c7c4ec269d7bb973a7c69de8
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-26 15:36:43 +0000

    planning beheer

--- icclear/application/icclear/views/admin/planning/lijst.php
+++ icclear/application/icclear/views/admin/planning/lijst.php
@@ -1,4 +1,6 @@
 <?php foreach ($dagen as $dag) { ?>
-<h2><?php echo toDDMMYYYY($dag->datum) ?></h2>
-<p><?php echo $dag->planning->beginUur . " " . $dag->planning->eindUur ?></p>
+    <h2><?php echo toDDMMYYYY($dag->datum) ?></h2>
+    <?php foreach ($dag->planning as $dag2) { ?>
+        <p><?php echo $dag2->beginUur . " " . $dag2->eindUur ?></p>
+    <?php } ?> 
 <?php } ?> 
