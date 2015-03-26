5d4091479400e4fe79bd47a466a2e23d5843b3f2
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-26 15:12:49 +0000

    conferentie onderdelen wijzigen

--- icclear/application/icclear/views/admin/conferentie/lijst.php
+++ icclear/application/icclear/views/admin/conferentie/lijst.php
@@ -20,7 +20,10 @@
                             <td><input type="text" class="form-control" name="formule"></td>
                             <td><input type="text" class="form-control" name="prijs"></td>
                             <td><input type="text" class="form-control" name="korting"></td>
-                            <td><a href="" class="glyphicon glyphicon-plus btn btn-default"></a></td>
+                            <td>
+                                <a href="" class="glyphicon glyphicon-ok btn btn-default"></a>
+                                <a href="" class="glyphicon glyphicon-remove btn btn-default"></a>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
