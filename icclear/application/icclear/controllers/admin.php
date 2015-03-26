0a5b3969d6d523f309bf0e0e72c424f58e0cff01
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-26 13:40:39 +0000

    admin.php gekozen confId op 0

--- icclear/application/icclear/controllers/admin.php
+++ icclear/application/icclear/controllers/admin.php
@@ -29,6 +29,7 @@ class Admin extends CI_Controller {
                 
         $this->load->model('conferentie_model');
         $data['conferenties'] = $this->conferentie_model->getAll();
+        $data['conferentieId'] = 0;
 
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/beheer', 'footer' => 'main_footer');
         $this->template->load('admin_master', $partials, $data);
