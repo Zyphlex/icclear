a5f0aca68e8137d72328ab3ce7c0fb5e9dcb0348
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-26 14:36:53 +0000

    planning beheer

--- icclear/application/icclear/views/admin/planning/lijst.php
+++ icclear/application/icclear/views/admin/planning/lijst.php
@@ -2,4 +2,35 @@
 <h2><?php echo toDDMMYYYY($dag->datum) ?></h2>
 <p><?php echo $dag->id ?></p> 
 <?php } ?>
+<script type="text/javascript">
+	//------------------
+	// jQuery UI bepalen
+	//------------------
+    
+    $(function() {
+        $( "#accordion" ).accordion({
+                autoHeight: false,
+                navigation: true
+        });
+    });
+        
+</script>
    
+<?php 
+
+echo '<div id="accordion">';
+
+foreach ($conferentiedagen as $dag) {
+    echo '<h3><a href="#">' . $dag->datum . "</a></h3>\n"; 
+    echo "<div><p>\n";
+    
+    echo "<p>Hallo boer het werkt</p>";
+//    foreach ($soort->producten as $product) {
+//        echo divAnchor('product/detail/' . $product->id, $product->naam) . "\n";
+//    }  
+    echo "</p></div>\n";
+}
+
+echo '</div>';
+    
+?>
