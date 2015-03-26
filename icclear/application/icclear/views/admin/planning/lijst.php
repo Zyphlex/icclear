27778900141c94467081e07c03b59826c2e5c00e
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-03-26 14:17:53 +0000

    planning beheer

--- icclear/application/icclear/views/admin/planning/lijst.php
+++ icclear/application/icclear/views/admin/planning/lijst.php
@@ -1,4 +1,5 @@
 <?php foreach ($conferentiedagen as $dag) { ?>
 <h2><?php echo toDDMMYYYY($dag->datum) ?></h2>
+<p><?php echo $dag->id ?></p> 
 <?php } ?>
-<p><?php echo $conferentieId ?></p>    
+   
