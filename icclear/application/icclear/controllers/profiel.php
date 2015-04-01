6be6f7a52c9b9dc5e73ee7ee793390f01dab4337
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-01 18:03:59 +0000

    conferenties in inschrijving_model

--- icclear/application/icclear/controllers/profiel.php
+++ icclear/application/icclear/controllers/profiel.php
@@ -68,7 +68,7 @@ class Profiel extends CI_Controller {
         $data['landen'] = $this->land_model->getAll();
         
         $this->load->model('inschrijving_model');
-        $data['inschrijving'] = $this->inschrijving_model->getAllInschijvingByConferentie($user->id);       
+        $data['inschrijving'] = $this->inschrijving_model->getInschijvingByConferentie($user->id);       
         
 
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'gebruiker/wijzigen', 'footer' => 'main_footer');
