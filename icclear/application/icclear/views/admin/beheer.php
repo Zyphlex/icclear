322b2b6d5b2a16edbfa1fe70735e175487341c13
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-01 13:40:45 +0000

    conferenties onderverdeeld

--- icclear/application/icclear/views/admin/beheer.php
+++ icclear/application/icclear/views/admin/beheer.php
@@ -1,33 +1,78 @@
 
 
-    <div class="col-md-10">        
-        <h1>Admin dashboard</h1>  
-        
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Naam</th>
-                                <th>Stad</th>
-                                <th>Begin datum</th>
-                                <th>Beheer</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php $teller = 1; foreach($conferenties as $gebruiker) {?>
-                            <tr>
-                                <td><?php echo $teller ?></td>
-                                <td><?php echo $gebruiker->naam ?></td>
-                                <td><?php echo $gebruiker->stad ?></td>
-                                <td><?php echo toDDMMYYYY($gebruiker->beginDatum) ?></td>
-                                <td><?php echo anchor('admin/dashboard/' . $gebruiker->id, 'Beheren', 'class="btn btn-default"'); ?></td>
-                            </tr>
-                            <?php $teller++; } ?>
-                        </tbody>
-                    </table>
-        
-        
-        <?php echo anchor('conferentie/toevoegen', 'Nieuwe conferentie toevoegen','class="btn btn-default"'); ?>  
-        
-        
-    </div>
+<div class="col-md-10">        
+    <h1>Admin dashboard</h1>  
+    <h3>Verleden</h3>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Naam</th>
+                <th>Stad</th>
+                <th>Begin datum</th>
+                <th>Beheer</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+            foreach ($verleden as $ver) { ?>
+                <tr>
+                    <td><?php echo $ver->naam ?></td>
+                    <td><?php echo $ver->stad ?></td>
+                    <td><?php echo toDDMMYYYY($ver->beginDatum) ?></td>
+                    <td><?php echo anchor('admin/dashboard/' . $ver->id, 'Beheren', 'class="btn btn-default"'); ?></td>
+                </tr>
+    <?php $teller++;
+} ?>
+        </tbody>
+    </table>
+    
+    <h3>Huidige</h3>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Naam</th>
+                <th>Stad</th>
+                <th>Begin datum</th>
+                <th>Beheer</th>
+            </tr>
+        </thead>
+        <tbody>
+                <tr>
+                    <td><?php echo $actieve->naam ?></td>
+                    <td><?php echo $actieve->stad ?></td>
+                    <td><?php echo toDDMMYYYY($actieve->beginDatum) ?></td>
+                    <td><?php echo anchor('admin/dashboard/' . $actieve->id, 'Beheren', 'class="btn btn-default"'); ?></td>
+                </tr>
+    
+        </tbody>
+    </table>
+    
+    <h3>Aankomende</h3>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Naam</th>
+                <th>Stad</th>
+                <th>Begin datum</th>
+                <th>Beheer</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+            foreach ($toekomenden as $toe) { ?>
+                <tr>
+                    <td><?php echo $toe->naam ?></td>
+                    <td><?php echo $toe->stad ?></td>
+                    <td><?php echo toDDMMYYYY($toe->beginDatum) ?></td>
+                    <td><?php echo anchor('admin/dashboard/' . $toe->id, 'Beheren', 'class="btn btn-default"'); ?></td>
+                </tr>
+    <?php $teller++;
+} ?>
+        </tbody>
+    </table>
+
+
+<?php echo anchor('conferentie/toevoegen', 'Nieuwe conferentie toevoegen', 'class="btn btn-default"'); ?>  
+
+
+</div>
