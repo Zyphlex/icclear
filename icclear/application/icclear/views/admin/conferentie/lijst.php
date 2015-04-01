a50d06af7d9af420c106e563798b21c246e44771
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-01 12:52:10 +0000

    conferentie onderdelen wijzigen

--- icclear/application/icclear/views/admin/conferentie/lijst.php
+++ icclear/application/icclear/views/admin/conferentie/lijst.php
@@ -31,4 +31,4 @@
 </table>
 
 
-<button class="wijzigItem glyphicon glyphicon-plus btn btn-primary" data-id="0"></button>
+<button class="wijzigItem glyphicon glyphicon-plus btn btn-primary" data-id="0">Nieuw onderdeel toevoegen</button>
