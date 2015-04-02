f800dd726853cd406b3b5243b53bdd309be2b40d
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-02 13:47:08 +0000

    inschrijven conferentie

--- icclear/application/icclear/views/inschrijving/inschrijving.php
+++ icclear/application/icclear/views/inschrijving/inschrijving.php
@@ -18,73 +18,78 @@ echo form_open('inschrijven/inschrijven', $attributes);
 
 <div class="row">
     <div class="col-md-12">
-        <form action="inschrijven/inschrijven">
-        <h3>Selecteer formule</h3>
+        <form action="inschrijven/inschrijven" method="get">
+            <input type="hidden" name="conferentieId" value="<?php $conferentie->id ?>" />
 
-        <table class="table">
-            <tr>
-                <th>Formule</th>
-                <th>Prijs</th>
-                <th>Early-bird korting</th>
-                <td></td>
-            </tr>
-            <?php
-            foreach ($conferentieOnderdelen as $conferentieOnderdeel) {
-                if ($conferentieOnderdeel->conferentie->statusId == '2') {
-                    echo '<tr><td>' . $conferentieOnderdeel->omschrijving . '</td>';
-                    echo '<td>' . $conferentieOnderdeel->prijs . '</td>';
-                    echo '<td>' . $conferentieOnderdeel->korting . ' %</td>';
-                    echo '<td><input type="radio" name="formule" value="' . $conferentieOnderdeel->id . '"/></td></tr>';
+            <h3>Selecteer formule</h3>
+
+            <table class="table">
+                <tr>
+                    <th>Formule</th>
+                    <th>Prijs</th>
+                    <th>Early-bird korting</th>
+                    <td></td>
+                </tr>
+                <?php
+                foreach ($conferentieOnderdelen as $conferentieOnderdeel) {
+                    if ($conferentieOnderdeel->conferentie->statusId == '2') {
+                        echo "<input type='hidden' name='conferentieOnderdeelId' value='$conferentieOnderdeel->id' />";
+                        echo '<tr><td>' . $conferentieOnderdeel->omschrijving . '</td>';
+                        echo '<td>' . $conferentieOnderdeel->prijs . '</td>';
+                        echo '<td>' . $conferentieOnderdeel->korting . ' %</td>';
+                        echo '<td><input type="radio" name="formule" value="' . $conferentieOnderdeel->id . '"/></td></tr>';
+                    }
                 }
-            }
-            ?>
+                ?>
 
-        </table>
+            </table>
 
-        <h3>Extra activiteiten</h3>
+            <h3>Extra activiteiten</h3>
 
-        <table class="table">
-            <tr>
-                <th>Activiteit</th>
-                <th>Prijs per persoon</th>
-                <th></th>
-                <th>Aantal Personen (max. 10)</th>
-            </tr>
-            <?php
-            foreach ($activiteiten as $activiteit) {
-                if ($activiteit->conferentie->statusId == '2') {
-                    echo '<tr><td>' . $activiteit->naam . '</td>';
-                    echo '<td>' . $activiteit->prijs . '</td>';
-                    ?>
-                    <td><input type="checkbox" name="aanwezig<?php echo $activiteit->id; ?>" id="aanwezig<?php echo $activiteit->id; ?>" value ="<?php echo $activiteit->id; ?>"/></td>
-                    <td><input type="number" name="aantalPersonen" id="aantalPersonen" max="10"/></td>
-                    <?php
+            <table class="table">
+                <tr>
+                    <th>Activiteit</th>
+                    <th>Prijs per persoon</th>
+                    <th></th>
+                    <th>Aantal Personen (max. 10)</th>
+                </tr>
+                <?php
+                foreach ($activiteiten as $activiteit) {
+                    if ($activiteit->conferentie->statusId == '2') {
+                        echo '<tr><td>' . $activiteit->naam . '</td>';
+                        echo '<td>' . $activiteit->prijs . '</td>';
+                        ?>
+                        <td><input type="checkbox" name="aanwezig<?php echo $activiteit->id; ?>" id="aanwezig<?php echo $activiteit->id; ?>" value ="<?php echo $activiteit->id; ?>"/></td>
+                        <td><input type="number" name="aantalPersonen" id="aantalPersonen" max="10"/></td>
+                        <?php
+                    }
                 }
-            }
-            ?>
+                ?>
 
-        </table>
+            </table>
 
-        <h3>Betaling</h3>
+            <h3>Betaling</h3>
 
-        <input type="checkbox" name="factuur" id="factuur"/>
-        <label>Ja, ik wil een factuur ontvangen.</label>
+            <input type="checkbox" name="factuur" id="factuur"/>
+            <label> Ja, ik wil een factuur ontvangen.</label>
 
-        <br/>
-        <br/>
+            <br/>
+            <br/>
 
-        <input type="radio" name="methode" value="VISA"/><label>VISA</label><br/>
-        <input type="radio" name="methode" value="Mastercard"/><label>Mastercard</label><br/>
-        <input type="radio" name="methode" value="PayPal"/><label>PayPal</label><br/>
-        <input type="radio" name="methode" value="Overschrijving"/><label>Overschrijving</label><br/>
+            <input type="radio" name="methode" value="2"/><label> VISA</label><br/>
+            <input type="radio" name="methode" value="3"/><label> Mastercard</label><br/>
+            <input type="radio" name="methode" value="1"/><label> PayPal</label><br/>
+            <input type="radio" name="methode" value="4"/><label> Overschrijving</label><br/>
 
-        <a href="<?php echo base_url(); ?>icclear.php/home" class="btn btn-default">Annuleren</a>
+            <input type="hidden" name="datum" value="<?php date("Y-m-d") ?>" />
 
-        <?php if ($user == null) { ?>
-            <a href="<?php echo base_url(); ?>icclear.php/logon/login" data-toggle="modal" data-target="#myModal">Aanmelden</a>
-        <?php } else { ?>
-            <input type="submit" value="Bevestigen en betalen" class="btn btn-default"/>
-        <?php } ?>
+            <a href="<?php echo base_url(); ?>icclear.php/home" class="btn btn-default">Annuleren</a>
+
+            <?php if ($user == null) { ?>
+                <a href="<?php echo base_url(); ?>icclear.php/logon/login" data-toggle="modal" data-target="#myModal">Aanmelden</a>
+            <?php } else { ?>
+                <input type="submit" value="Bevestigen en betalen" class="btn btn-default"/>
+            <?php } ?>
         </form>
     </div>
 </div>
