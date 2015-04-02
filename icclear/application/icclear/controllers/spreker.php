d939d08f370e1d7dde853d1810a557fed9320138
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-02 14:32:52 +0000

    Spreker controller upload

--- icclear/application/icclear/controllers/spreker.php
+++ icclear/application/icclear/controllers/spreker.php
@@ -107,14 +107,10 @@ class Spreker extends CI_Controller {
         if ( ! $this->upload->do_upload())
 		{
 			$error = array('error' => $this->upload->display_errors());
-
-			$this->load->view('upload_form', $error);
 		}
 		else
 		{
 			$data = array('upload_data' => $this->upload->data());
-
-			$this->load->view('upload_success', $data);
 		}
                 
         $spreker->biografie = $biografie;
