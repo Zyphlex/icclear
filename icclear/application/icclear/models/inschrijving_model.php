89619cb2efa050f9a3a3faed9d8422c160fb0642
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-19 14:19:16 +0000

    username eruit gooien

--- icclear/application/icclear/models/inschrijving_model.php
+++ icclear/application/icclear/models/inschrijving_model.php
@@ -33,45 +33,39 @@ class Inschrijving_model extends CI_Model {
             $inschrijving->betaling = $this->betaling_model->get($inschrijving->betalingId);
             $inschrijving->confonderdeel = $this->conferentie_onderdeel_model->get($inschrijving->conferentieOnderdeelId);
             $inschrijving->conferentie = $this->conferentie_model->get($inschrijving->conferentieId);
-            
         }
         return $inschrijvingen;
     }
-    
+
     function getInschijvingByConferentie($id) {
-        $this->db->where('gebruikerId',$id);
+        $this->db->where('gebruikerId', $id);
         $query = $this->db->get('inschrijving');
-        if ($query->num_rows() == 1){
+        if ($query->num_rows() == 1) {
             $inschrijving = $query->row();
-        }            
-        else{
+            $this->load->model('betaling_model');
+            $this->load->model('conferentie_onderdeel_model');
+            $this->load->model('conferentie_model');
+
+            $inschrijving->betaling = $this->betaling_model->get($inschrijving->betalingId);
+            $inschrijving->confonderdeel = $this->conferentie_onderdeel_model->get($inschrijving->conferentieOnderdeelId);
+            $inschrijving->conferentie = $this->conferentie_model->get($inschrijving->conferentieId);
+        } else {
             $inschrijving = "";
         }
-                
-        $this->load->model('betaling_model');
-        $this->load->model('conferentie_onderdeel_model');
-        $this->load->model('conferentie_model');
-                
-        $inschrijving->betaling = $this->betaling_model->get($inschrijving->betalingId);
-        $inschrijving->confonderdeel = $this->conferentie_onderdeel_model->get($inschrijving->conferentieOnderdeelId);
-        $inschrijving->conferentie = $this->conferentie_model->get($inschrijving->conferentieId);            
-        
         return $inschrijving;
     }
-    
+
     function getCountByConferentie($id) {
-        $this->db->where('conferentieId',$id);
+        $this->db->where('conferentieId', $id);
         $query = $this->db->get('inschrijving');
-        $rowcount = $query->num_rows();        
+        $rowcount = $query->num_rows();
         return $rowcount;
     }
-    
-    function insert($inschrijving)
-    {
+
+    function insert($inschrijving) {
         $this->db->insert('inschrijving', $inschrijving);
         return $this->db->insert_id();
     }
-    
 
 }
 
