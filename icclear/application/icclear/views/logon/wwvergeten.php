9dec0d24bba4d60c18ff7c5438890e6463844b45
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-19 14:00:14 +0000

    username eruit gooien

--- icclear/application/icclear/views/logon/wwvergeten.php
+++ icclear/application/icclear/views/logon/wwvergeten.php
@@ -30,7 +30,7 @@
 
 
     <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Sluit</button>
         <input type="submit" name="mysubmit" value="Verstuur Email" class="btn btn-primary"  />
     </div>
     
