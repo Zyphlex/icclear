e6604e453c68a33980d31127af4d377c2fda866e
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-19 17:39:51 +0000

    nietactiefpagina

---
+++ icclear/application/icclear/views/logon/nietActief.php
@@ -0,0 +1,6 @@
+<div class="bs-example">
+    <div class="alert alert-danger">        
+        Uw account is nog niet geactiveerd.          
+        <div>Kijk in uw mailbox of u een email hebt gekregen met een activatiecode in en klik dan op die link of maak een account aan.</div>                        
+    </div>
+</div>
