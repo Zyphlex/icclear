7d81ad0af26deaf7b4e28488bafb7f8e92110523
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-22 13:41:24 +0000

    geld

--- icclear/application/icclear/models/betaling_model.php
+++ icclear/application/icclear/models/betaling_model.php
@@ -20,6 +20,16 @@ class Betaling_model extends CI_Model {
         return $this->db->insert_id();
     }
     
+    function getPrijsByGebruiker($id){
+        $this->db->where('id', $id);
+        $query = $this->db->get('betaling');
+        $betaling = $query->row();
+        
+        $this->load->model('gebruiker_activiteit_model');
+        $geld = $this->gebruiker_activiteit_model->getPrijsByGebruiker($betaling->gebruikerId);
+        
+        return $geld;
+    }
     
 }
 
