bdd8097b903260ca67c8b58c158347050eb16c15
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-22 13:33:53 +0000

    actieve conferentie meegeven

--- icclear/application/icclear/controllers/planningbeheer.php
+++ icclear/application/icclear/controllers/planningbeheer.php
@@ -32,6 +32,9 @@ class Planningbeheer extends CI_Controller {
         
         $this->load->model('zaal_model'); 
         $data['zalen'] = $this->zaal_model->getAll();
+        
+        $this->load->model('conferentie_model');
+        $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
                 
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/planning/overzicht', 'footer' => 'main_footer');
         $this->template->load('admin_master', $partials, $data);
