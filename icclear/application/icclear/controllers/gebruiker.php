bf9e337f91e28f11c30370125ae4e4661cda5582
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-22 13:29:45 +0000

    engels naar nederlands

--- icclear/application/icclear/controllers/gebruiker.php
+++ icclear/application/icclear/controllers/gebruiker.php
@@ -32,6 +32,9 @@ class Gebruiker extends CI_Controller {
         $this->load->model('land_model');        
         $data['landen'] = $this->land_model->getAll();
         
+        $this->load->model('conferentie_model');
+        $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
+        
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/gebruiker/overzicht', 'footer' => 'main_footer');
         $this->template->load('admin_master', $partials, $data);
     }
