eb0517e16e40c512272c1e66466852cb69102596
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-22 13:36:08 +0000

    actieve conferentie meegeven

--- icclear/application/icclear/controllers/land.php
+++ icclear/application/icclear/controllers/land.php
@@ -29,6 +29,9 @@ class Land extends CI_Controller {
                 
         $this->load->model('land_model');
         $data['landen'] = $this->land_model->getAll();
+        
+        $this->load->model('conferentie_model');
+        $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
 
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/land/overzicht', 'footer' => 'main_footer');
         $this->template->load('admin_master', $partials, $data);
