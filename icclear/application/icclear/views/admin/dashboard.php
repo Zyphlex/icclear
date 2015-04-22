8cc8572cb830ba6b8e9315b05ed07f5308ae9659
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-22 17:51:27 +0000

    aankondingen knop weg

--- icclear/application/icclear/views/admin/dashboard.php
+++ icclear/application/icclear/views/admin/dashboard.php
@@ -1,7 +1,6 @@
 <div class="col-md-10">       
     <h1>Conferentie <?php echo $conferentie->naam ?> beheren</h1>         
-    <?php echo anchor('email/', 'Emails', 'class="btn btn-default"'); ?>
-    <?php echo anchor('aankondiging/', 'Aankondigingen', 'class="btn btn-default"'); ?>           
+    <?php echo anchor('email/', 'Emails', 'class="btn btn-default"'); ?>          
 </div>
 <div class="col-md-10">    
     <h4>Conferentie <span class="glyphicon glyphicon-pushpin"></span></h4>
