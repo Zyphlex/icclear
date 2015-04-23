8f6ccde1664ba70d4a75f87c493c72c7dec6af9f
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-23 12:53:28 +0000

    betalingtype model

---
+++ icclear/application/icclear/models/betalingtype_model.php
@@ -0,0 +1,25 @@
+<?php
+
+class Betalingtype_model extends CI_Model {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    function get($id) {
+        $this->db->where('id', $id);
+        $query = $this->db->get('betaling');
+        return $query->row();
+    }
+            
+    function getAll()
+    {        
+        $this->db->order_by('omschrijving');
+        $query = $this->db->get('betalingtype');
+        return $query->result();
+    }    
+    
+}
+
+?>
