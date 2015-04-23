4d15b05781cac91588bf193851b28174d8cf7763
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-23 18:35:48 +0000

    login oplossing

--- icclear/application/icclear/views/logon/fout.php
+++ icclear/application/icclear/views/logon/fout.php
@@ -1,11 +1,6 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>        
-        <?php
-        
-        ?>
-    </body>
-</html>
+<div class="bs-example">
+    <div class="alert alert-danger">        
+        Er is een fout opgetreden.
+        <div>U heeft een fout e-mailadres of wachtwoord ingegeven.</div>                        
+    </div>
+</div>
