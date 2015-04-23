8504bf77b223b867134c967e9eaf2e3cc420c32a
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-23 12:45:55 +0000

    Spreker upload foto

--- icclear/application/icclear/controllers/spreker.php
+++ icclear/application/icclear/controllers/spreker.php
@@ -7,97 +7,92 @@ class Spreker extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        
+
         $this->load->helper('form');
     }
 
     public function index() {
-        $data['user']  = $this->authex->getUserInfo();        
-        $data['title'] = 'IC Clear - Sprekers'; 
+        $data['user'] = $this->authex->getUserInfo();
+        $data['title'] = 'IC Clear - Sprekers';
         $data['active'] = 'spreker';
-        
+
         $this->load->model('conferentie_model');
         $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
-        
+
         $this->load->model('gebruiker_model');
         $data['sprekers'] = $this->gebruiker_model->getSprekersActieve();
-                
+
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'spreker/overzicht', 'footer' => 'main_footer');
-        $this->template->load('main_master', $partials, $data); 
+        $this->template->load('main_master', $partials, $data);
     }
+
     public function voorstel() {
-        
-        $data['user']  = $this->authex->getUserInfo();        
-        $data['title'] = 'IC Clear - Sprekers'; 
-        $data['active'] = 'programma';       
-        
+
+        $data['user'] = $this->authex->getUserInfo();
+        $data['title'] = 'IC Clear - Sprekers';
+        $data['active'] = 'programma';
+
         $this->load->model('conferentie_model');
         $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
-        
+
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'spreker/voorstel', 'footer' => 'main_footer');
-        $this->template->load('main_master', $partials, $data); 
-        
+        $this->template->load('main_master', $partials, $data);
     }
-    
-    public function indienen()
-    {
+
+    public function indienen() {
         $this->load->model('conferentie_model');
         $gebruiker = $this->authex->getUserInfo();
         $conferentie = $this->conferentie_model->getActieveConferentie();
-        
+
         $sessie = new stdClass();
         $sessie->onderwerp = $this->input->post('sessieonderwerp');
         $sessie->omschrijving = $this->input->post('sessieomschrijving');
         $sessie->datumIngediend = date('Y-m-d');
         $sessie->isGoedgekeurd = '0';
         $sessie->gebruikerIdSpreker = $gebruiker->id;
-        $sessie->conferentieId = $conferentie->id;    
+        $sessie->conferentieId = $conferentie->id;
 
         $this->load->model('sessies_model');
         $this->sessies_model->insert($sessie);
-        
+
         redirect('spreker');
     }
-        
-    public function biografie($id, $key)
-    {
-        $data['user'] = $this->authex->getUserInfo();        
-        
+
+    public function biografie($id, $key) {
+        $data['user'] = $this->authex->getUserInfo();
+
         $data['title'] = 'IC Clear - Biografie';
         $data['active'] = 'spreker';
-         
+
         $this->load->model('conferentie_model');
         $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
-        
+
         $this->load->model('gebruiker_model');
         $spreker = $this->gebruiker_model->get($id);
         $data['spreker'] = $spreker;
-        
+
         if ($id == $spreker->id && $key == $spreker->generatedKey) {
             $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'spreker/biografie', 'footer' => 'main_footer');
-        }
-        else
-        {
+        } else {
             
         }
-        $this->template->load('main_master', $partials, $data);    
+        $this->template->load('main_master', $partials, $data);
     }
-    
-    public function updateBiografie()
-    {
-        $data['user'] = $this->authex->getUserInfo();        
-        
+
+    public function updateBiografie() {
+        $data['user'] = $this->authex->getUserInfo();
+
         $data['title'] = 'IC Clear - Biografie';
         $data['active'] = 'spreker';
-        
+
         $id = $this->session->userdata('user_id');
-        
+
         $this->load->model('gebruiker_model');
         $spreker = $this->gebruiker_model->get($id);
-        
+
         // biografie
         $biografie = $this->input->post("biografie");
-        
+
         // foto
         $config['upload_path'] = base_url() . 'application/upload/';
         $config['allowed_types'] = 'jpg';
@@ -105,30 +100,27 @@ class Spreker extends CI_Controller {
         $config['max_size'] = 200;
         $config['max_height'] = 500;
         $config['max_width'] = 500;
-        
+
+        if (!is_dir($config['upload_path'])) {
+            mkdir($config['upload_path'], 0777, TRUE);
+        }
+
         $this->load->library('upload', $config);
-         if(!is_dir($config['upload_path'])){
-                    mkdir($config['upload_path'],0777,TRUE);
-                }
         $this->upload->initialize($config);
-        if ( ! $this->upload->do_upload())
-		{
-			$error = array('error' => $this->upload->display_errors());
-		}
-		else
-		{
-			$data = array('upload_data' => $this->upload->data());
-		}
-                
+        if (!$this->upload->do_upload()) {
+            $error = array('error' => $this->upload->display_errors());
+        } else {
+            $data = array('upload_data' => $this->upload->data());
+        }
+
         $spreker->biografie = $biografie;
         $spreker->foto = $config['file_name'];
-        
+
         $this->gebruiker_model->update($spreker);
-        
+
         redirect('profiel/instellingen');
     }
-  
-    
+
 }
 
 ?>
