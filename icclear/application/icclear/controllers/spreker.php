ab28a10cb047781dbc38680b4fad92d40592636c
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-23 12:55:13 +0000

    Spreker upload foto

--- icclear/application/icclear/controllers/spreker.php
+++ icclear/application/icclear/controllers/spreker.php
@@ -110,6 +110,7 @@ class Spreker extends CI_Controller {
         
         if (!$this->upload->do_upload()) {
             $error = array('error' => $this->upload->display_errors());
+            echo 'lol fail';
         } else {
             $data = array('upload_data' => $this->upload->data());
             echo 'gelukt';
@@ -120,7 +121,7 @@ class Spreker extends CI_Controller {
 
         $this->gebruiker_model->update($spreker);
 
-        redirect('profiel/instellingen');
+        /*redirect('profiel/instellingen');*/
     }
 
 }
