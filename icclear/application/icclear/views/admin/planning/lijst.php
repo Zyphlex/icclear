fc7ea15212387c59645192c4555a2412e4f010dd
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-23 22:12:43 +0000

    gebouw per zaal

--- icclear/application/icclear/views/admin/planning/lijst.php
+++ icclear/application/icclear/views/admin/planning/lijst.php
@@ -22,7 +22,7 @@
                 <tr>
                     <td><?php echo $planning->beginUur . " tot " . $planning->eindUur ?></td>
                     <td><?php echo $planning->sessie->onderwerp ?></td>
-                    <td><?php echo $planning->zaal->naam . "(" . $planning->gebouw->naam . ")"?></td>
+                    <td><?php echo $planning->zaal->naam . " (" . $planning->gebouw->naam . ")"?></td>
                     <?php
                     if ($planning->plenair == 1) {
                         echo '<td>Ja</td>';
