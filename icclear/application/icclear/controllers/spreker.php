4f6beb8faf06314c65653e27274eff408e0c538f
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-23 13:44:09 +0000

    foto uploaden

--- icclear/application/icclear/controllers/spreker.php
+++ icclear/application/icclear/controllers/spreker.php
@@ -95,7 +95,7 @@ class Spreker extends CI_Controller {
         // foto
         $config['upload_path'] = './application/upload/';
         $config['allowed_types'] = 'jpg';
-        //$config['file_name'] = 'spreker' . $spreker->id . '.jpg';
+        $config['file_name'] = 'spreker' . $spreker->id . '.jpg';
         $config['max_size'] = 200;
         $config['max_height'] = 250;
         $config['max_width'] = 250;
@@ -121,7 +121,7 @@ class Spreker extends CI_Controller {
         }
 
         $spreker->biografie = $biografie;
-        //$spreker->foto = $config['file_name'];
+        $spreker->foto = $config['file_name'];
 
         $this->gebruiker_model->update($spreker);
 
