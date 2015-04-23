2fd6eb7187ef6417ce93bb965707f6b82f0891a9
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-23 15:05:35 +0000

    boolean

--- icclear/application/icclear/controllers/logon.php
+++ icclear/application/icclear/controllers/logon.php
@@ -14,29 +14,27 @@ class Logon extends CI_Controller {
 
     public function login() {
         $data['user'] = $this->authex->getUserInfo();
-        
+
         $this->load->view('logon/login', $data);
     }
 
     public function aanmelden() {
         $email = $this->input->post('email');
         $password = $this->input->post('password');
-        
+
         //is geactiveerd
         $this->load->model('logon_model');
         $actCheck = $this->logon_model->isGeactiveerd($email);
-        if($actCheck == true){
+        if ($actCheck == true) {
             //login
-        if ($this->authex->login($email, sha1($password))) {
-            redirect('home');
+            if ($this->authex->login($email, sha1($password))) {
+                redirect('home');
+            } else {
+                redirect('faq');
+            }
         } else {
-            redirect('faq');
+            redirect('logon/nietGeactiveerd');
         }
-        }
-        else{
-            redirect('logon/nietGeactiveerd');            
-        }
-        
     }
 
     public function vergeten() {
@@ -59,19 +57,19 @@ class Logon extends CI_Controller {
         $this->load->view('logon/registreer', $data);
     }
 
-    public function add() {         
+    public function add() {
         $email = $this->input->post('emailadres');
-        $genkey = sha1(mt_rand(10000, 99999) . time() . $email);        
+        $genkey = sha1(mt_rand(10000, 99999) . time() . $email);
         $user = new stdClass();
-        
+
         $user->familienaam = $this->input->post('familienaam');
         $user->voornaam = $this->input->post('voornaam');
         $user->email = $email;
-        $user->wachtwoord = $this->input->post('wachtwoord1');                
+        $user->wachtwoord = $this->input->post('wachtwoord1');
         $user->geslacht = $this->input->post('geslacht');
         $user->land = $this->input->post('land');
         $user->generatedKey = $genkey;
-                
+
         $id = $this->authex->register($user);
         if ($id != 0) {
             $this->sendmail($user->email, $user->generatedKey);
@@ -82,7 +80,7 @@ class Logon extends CI_Controller {
     }
 
     public function nieuwPass() {
-        $data['user'] = ''; 
+        $data['user'] = '';
         $data['active'] = '';
         $data['title'] = 'IC Clear - Wachtwoord aangepast';
 
@@ -90,26 +88,25 @@ class Logon extends CI_Controller {
         $generatedKey = $this->input->post('key');
 
         $this->authex->nPass($wachtwoord, $generatedKey);
-        
+
         $newKey = sha1(mt_rand(10000, 99999) . time() . $wachtwoord);
         $this->authex->updateKey($generatedKey, $newKey);
-        
+
         $this->load->model('conferentie_model');
         $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
-        
+
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'logon/ww_succes', 'footer' => 'main_footer');
         $this->template->load('main_master', $partials, $data);
-        
     }
 
     public function passReset($generatedKey) {
-         $this->load->model('logon_model');
-        
+        $this->load->model('logon_model');
+
         $data['user'] = '';
         $data['generatedKey'] = $generatedKey;
-        $data['title'] = 'IC Clear - Register'; 
+        $data['title'] = 'IC Clear - Register';
         $data['active'] = '';
-        
+
         $this->load->model('conferentie_model');
         $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
 
@@ -120,24 +117,24 @@ class Logon extends CI_Controller {
         }
         $this->template->load('main_master', $partials, $data);
     }
-    
-    public function nietGeactiveerd(){
+
+    public function nietGeactiveerd() {
         $data['user'] = '';
-        $data['title'] = 'IC Clear - Account niet geactiveerd'; 
+        $data['title'] = 'IC Clear - Account niet geactiveerd';
         $data['active'] = '';
-        
+
         $this->load->model('conferentie_model');
         $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
-        
+
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'logon/nietActief', 'footer' => 'main_footer');
         $this->template->load('main_master', $partials, $data);
     }
 
     public function bestaat() {
         $data['title'] = 'IC Clear - Register';
-        $data['user'] = ''; 
+        $data['user'] = '';
         $data['active'] = '';
-        
+
         $this->load->model('conferentie_model');
         $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
 
@@ -147,9 +144,9 @@ class Logon extends CI_Controller {
 
     public function klaar() {
         $data['title'] = 'IC Clear - Register';
-        $data['user'] = $this->authex->getUserInfo(); 
+        $data['user'] = $this->authex->getUserInfo();
         $data['active'] = '';
-        
+
         $this->load->model('conferentie_model');
         $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
 
@@ -168,12 +165,12 @@ class Logon extends CI_Controller {
 
         $this->authex->activate($generatedKey);
 
-        $data['user'] = null; 
+        $data['user'] = null;
         $data['active'] = '';
-        
+
         $this->load->model('conferentie_model');
         $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
-        
+
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'logon/user_geactiveerd', 'footer' => 'main_footer');
         $this->template->load('main_master', $partials, $data);
     }
@@ -183,11 +180,11 @@ class Logon extends CI_Controller {
         $this->email->to($to);
         $this->email->subject('Geregistreerd bij IC Clear');
         $this->email->message('U bent geregistreerd! Klik op onderstaande link om uw registratie te activeren ' . "\n" . "\n " . site_url("logon/activeer/$generatedKey"));
-        $this->email->send();         
+        $this->email->send();
     }
 
     public function resetPass() {
- 
+
         $data['active'] = '';
         $data['title'] = 'IC Clear - Email verzonden';
 
@@ -203,7 +200,7 @@ class Logon extends CI_Controller {
         $this->email->send();
 
         $data['user'] = '';
-        
+
         $this->load->model('conferentie_model');
         $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
 
@@ -214,13 +211,17 @@ class Logon extends CI_Controller {
     function check_email_availablity() {
         $this->load->model('email_model');
         $result = $this->email_model->check_email_availablity();
+        $boolean = false;
 
-        if (!$result)
+        if (!$result) {
             echo '<p class="form-note form-note-used" id="feedbackemail">Niet beschikbaar</p>';
-        else
+        } else {
             echo '<p class="form-note form-note-ok" id="feedbackemail">Beschikbaar</p>';
+            $boolean = true;
+        }
+        return $boolean;
     }
-   
+
 }
 
 /* End of file welcome.php */
