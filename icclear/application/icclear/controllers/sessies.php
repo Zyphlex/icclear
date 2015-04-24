337cfc3fb3ac42c6a01f35e1a48b59b09d4ca26f
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-24 21:01:08 +0000

    sessie voorkeuren

--- icclear/application/icclear/controllers/sessies.php
+++ icclear/application/icclear/controllers/sessies.php
@@ -22,9 +22,12 @@ class Sessies extends CI_Controller {
     }
     
     public function index() {
-        $data['user']  = $this->authex->getUserInfo();
+         $user  = $this->authex->getUserInfo();
+        $data['user'] = $user;
+        $data['conferentieId'] = $this->session->userdata('conferentieId');        
+        $this->load->model('inschrijving_model');
+        $data['inschrijving'] = $this->inschrijving_model->getInschijvingByGebruiker($user->id);
         
-        $data['conferentieId'] = $this->session->userdata('conferentieId');
         $data['title'] = 'IC Clear - Beheer';        
         $data['active'] = 'admin';        
                 
