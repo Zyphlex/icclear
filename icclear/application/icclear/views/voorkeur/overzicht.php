980d42a1e919bc166b7c9163043318bf77943b13
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-24 20:50:10 +0000

    sessie voorkeuren

--- icclear/application/icclear/views/voorkeur/overzicht.php
+++ icclear/application/icclear/views/voorkeur/overzicht.php
@@ -4,44 +4,17 @@
     </div>
 </div>
     
-<div class='row'>
-        <?php $teller = 1; foreach ($gebouwen as $gebouw) { ?>
+<div class='row'>        
             <div class="col-md-6">
                 <div class="panel panel-default">
-                    <div class = "panel-heading">                        
-                        <h3>Dag <?php echo $teller .  '  ' . $gebouw->gebouw->naam?></h3>
+                    <div class = "panel-heading">                                                
                     </div>
                     <div class = "panel-body">  
                             <div class="thumbnail">
                                 <img src="http://placehold.it/350x150" alt="placeholder image" title="placeholder">
-                            </div>
-                        <p><?php echo $gebouw->gebouw->gemeente ?> (<?php echo $gebouw->gebouw->postcode ?>)</p>
-                        <p><?php echo $gebouw->gebouw->straat ?> <?php echo $gebouw->gebouw->nummer ?></p>
-                    
-                        <button data-id="<?php echo $gebouw->gebouw->id ?>" class="zoekRoutes btn btn-primary" data-toggle="modal" data-target="#routesModal">Toon routes</button>
+                            </div>                                                                    
                     </div>
                 </div>  
-            </div>
-        <?php $teller++; } ?>    
+            </div>          
 </div>
 
-<div class="modal fade" id="routesModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Routes</h4>
-            </div>
-
-            <div class="modal-body">                  
-                <div id="resultaat"></div>
-            </div>
-            
-             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Sluiten</button>
-            </div>
-            
-        </div>            
-    </div>
-</div>  
