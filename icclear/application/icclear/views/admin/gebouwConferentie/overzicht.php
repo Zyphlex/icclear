faa99bd9a93d7ebce07be5af2b9380f09a0054d5
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-24 21:31:25 +0000

    gebouw opmaak fix

--- icclear/application/icclear/views/admin/gebouwConferentie/overzicht.php
+++ icclear/application/icclear/views/admin/gebouwConferentie/overzicht.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
-        <title></title>
 
-        <script type="text/javascript">
-            $(document).ready(function () {
-                $('.table').DataTable();
-            });
-        </script>
-    </head>
-    <body>
         <div class="col-md-10"> 
             <?php foreach ($dagen as $dag) { ?>
                 <h3><?php echo toDDMMYYYY($dag->datum) ?></h3>
@@ -45,5 +28,3 @@ and open the template in the editor.
                 </table>  
             </div>
         <?php } ?> 
-    </body>
-</html>
