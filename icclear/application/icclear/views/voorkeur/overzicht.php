9df61e93d98238085597a82a35e36eb30e26859d
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-24 21:11:05 +0000

    sessie voorkeuren

--- icclear/application/icclear/views/voorkeur/overzicht.php
+++ icclear/application/icclear/views/voorkeur/overzicht.php
@@ -1,10 +1,10 @@
-<div class='row'>
-    <div class='col-md-12'>
+<div class="row">
+    <div class="col-md-12">
         <h1>Sessie voorkeuren</h1>
     </div>
 </div>
     
-<div class='row'>        
+<div class="row">        
             <div class="col-md-6">
                 <div class="panel panel-default">
                     <div class = "panel-heading">                                                
