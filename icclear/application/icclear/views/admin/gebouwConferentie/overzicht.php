979afbbb227a5c09919f46ff1c068db5634eb181
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-24 20:30:47 +0000

    gebouw per zaal

--- icclear/application/icclear/views/admin/gebouwConferentie/overzicht.php
+++ icclear/application/icclear/views/admin/gebouwConferentie/overzicht.php
@@ -1,28 +1,9 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
-        <title>Deel x oefening y</title>
-    </head>
-    <body>
-        <?php
-        //--------------------------------------------------------------------------------
-        // Deel x oefening y 
-        //--------------------------------------------------------------------------------
-        // Thomas More Kempen 2TI
-        //--------------------------------------------------------------------------------
-        // php pagina: oefeningyy.php
-        //
-        // Doel van de oefening:
-        //
-        //--------------------------------------------------------------------------------
-        // Auteur: Leslie Milants
-        //--------------------------------------------------------------------------------
-        ?>
-    </body>
-</html>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.table').DataTable();
+    });
+</script>
+    
+    <?php foreach ($conferentiedagen as $dag) { ?>
+<p><?php $dag->datum ?></p>
+<?php } ?> 
