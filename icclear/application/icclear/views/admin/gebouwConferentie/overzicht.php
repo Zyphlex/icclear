150df1c399fca39ef7b65e6feb131bc32ea28320
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-24 21:19:19 +0000

    gebouwen per dag

--- icclear/application/icclear/views/admin/gebouwConferentie/overzicht.php
+++ icclear/application/icclear/views/admin/gebouwConferentie/overzicht.php
@@ -8,7 +8,7 @@ and open the template in the editor.
     <head>
         <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
         <title></title>
-        
+
         <script type="text/javascript">
             $(document).ready(function () {
                 $('.table').DataTable();
@@ -16,31 +16,35 @@ and open the template in the editor.
         </script>
     </head>
     <body>
+        <div class="col-md-10"> 
+            <?php foreach ($dagen as $dag) { ?>
+                <h3><?php echo toDDMMYYYY($dag->datum) ?></h3>
+            </div>
 
-        <?php foreach ($dagen as $dag) { ?>
-            <h3><?php echo toDDMMYYYY($dag->datum) ?></h3>
-            <table class="table table-beheer">
-                <thead>
-                    <tr>
-
-                        <th>Naam</th>
-                        <th>Gemeente</th>                
-                        <th>Postcode</th>
-                        <th>Land</th>
-                        <th>Gebouw</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($dag->planning as $planning) { ?>
+            <div class="col-md-10"> 
+                <table class="table table-beheer">
+                    <thead>
                         <tr>
-                            <td><?php echo $planning->gebouw->naam ?></td>
-                            <td><?php echo $planning->gebouw->gemeente ?></td>
-                            <td><?php echo $planning->gebouw->postcode ?></td>
-                            <td><?php echo $planning->land->naam ?></td>
+
+                            <th>Naam</th>
+                            <th>Gemeente</th>                
+                            <th>Postcode</th>
+                            <th>Land</th>
+                            <th>Gebouw</th>
                         </tr>
-                    <?php } ?>
-                </tbody>
-            </table>  
+                    </thead>
+                    <tbody>
+                        <?php foreach ($dag->planning as $planning) { ?>
+                            <tr>
+                                <td><?php echo $planning->gebouw->naam ?></td>
+                                <td><?php echo $planning->gebouw->gemeente ?></td>
+                                <td><?php echo $planning->gebouw->postcode ?></td>
+                                <td><?php echo $planning->land->naam ?></td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>  
+            </div>
         <?php } ?> 
     </body>
 </html>
