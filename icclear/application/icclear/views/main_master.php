e16168216b8074dafe44a1ebd228ea60a6455a90
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-27 12:39:35 +0000

    test

--- icclear/application/icclear/views/main_master.php
+++ icclear/application/icclear/views/main_master.php
@@ -15,6 +15,7 @@
 
     ?>
 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title; ?></title>
     <meta charset="utf-8"> 
     <link rel="stylesheet" type="text/css" 
