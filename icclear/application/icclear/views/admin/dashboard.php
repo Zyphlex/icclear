c2ad3ee3c9862fc672097819c9eb4dd01203bb18
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-27 13:20:12 +0000

    object id #fix op dashboard

--- icclear/application/icclear/views/admin/dashboard.php
+++ icclear/application/icclear/views/admin/dashboard.php
@@ -4,7 +4,7 @@
 </div>
 <div class="col-md-10">    
     <h4>Conferentie <span class="glyphicon glyphicon-pushpin"></span></h4>
-    <h5>Naam: <?php echo $conferentie ?></h5>
+    <h5>Naam: <?php echo $conferentie->naam ?></h5>
     <p>Periode: <?php echo $dataConferentie->beginDatum; ?> tot <?php echo $dataConferentie->eindDatum; ?></p>    
     <p>Stad: <?php echo $dataConferentie->stad; ?></p>
     <p>Maximum aantal inschrijvingen: <?php echo $dataConferentie->maxInschrijvingen; ?></p>
