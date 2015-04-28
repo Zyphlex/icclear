89a511be32e19ee2bd5a08ca19c84c0dc84cc14e
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-28 09:30:04 +0000

    opdrachtgever

--- icclear/application/icclear/views/main_footer.php
+++ icclear/application/icclear/views/main_footer.php
@@ -15,6 +15,7 @@
 
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
-        <p>PHP Project - Opdrachtgever: Karine Nickolay - IT Ninjas - Groep 23: Frederik Van Hooghten, Rob Oosthoek, Leslie Milants & Abderrahmane Ikrou</p>
+        <p>Opdrachtgever: Karine Nickolay</p>
+        <p>PHP Project - IT Ninjas - Groep 23: Frederik Van Hooghten, Rob Oosthoek, Leslie Milants & Abderrahmane Ikrou</p>        
     </div>
 </div>
