c63fbd41fac6147a8d7ac87c701b71d8d206d2cb
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-29 18:37:41 +0000

    modal programma overzicht fixers

--- icclear/application/icclear/views/planning/overzicht.php
+++ icclear/application/icclear/views/planning/overzicht.php
@@ -76,7 +76,7 @@
                                         ?>
                                         <tr>
                                             <td><?php echo $planning->beginUur . ' - ' . $planning->eindUur ?></td> 
-                                            <td><a href="" data-toggle="modal" data-target="#itemModal" class="toonItem" data-id="<?php echo $planning->sessie->id ?>"><?php echo $planning->sessie->onderwerp ?></a></td>                                    
+                                            <td><a href="" data-toggle="modal" class="toonItem" data-id="<?php echo $planning->sessie->id ?>"><?php echo $planning->sessie->onderwerp ?></a></td>                                    
                                             <td><?php echo $planning->spreker->voornaam . ' ' . $planning->spreker->familienaam ?></td>
                                         </tr>
                                         <?php
