675e2e861699796c2589d48de7037cd8da333c8e
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-29 14:18:47 +0000

    htmlentities

--- icclear/application/icclear/views/admin/gebruiker/lijst.php
+++ icclear/application/icclear/views/admin/gebruiker/lijst.php
@@ -1,39 +1,41 @@
 <script type="text/javascript">
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('.table').DataTable();
     });
 </script>
 
 <div class="table-responsive">
-<table class="table table-beheer">
-    <thead>
-        <tr>
-            <th>Familienaam</th>
-            <th>Voornaam</th>
-            <th>Email</th>
-            <th>Type</th>
-            <th>Beheer</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($gebruikers as $gebruiker) { ?>
+    <table class="table table-beheer">
+        <thead>
             <tr>
-                <td><?php echo $gebruiker->familienaam ?></td>
-                <td><?php echo $gebruiker->voornaam ?></td>
-                <td><?php echo $gebruiker->emailadres ?></td>
-                <?php if ($gebruiker->typeId == 1) { ?>
-                    <td>Bezoeker</td>
-                <?php } else { ?>
-                    <td>Spreker</td>
-                <?php } ?>
-                <td>
-                    <p>                                        
-                        <button class="wijzigGebruiker glyphicon glyphicon-pencil btn btn-primary" data-id="<?php echo $gebruiker->id ?>"></button>
-                        <button class="verwijderGebruiker glyphicon glyphicon-trash btn btn-danger" data-id="<?php echo $gebruiker->id ?>"></button> 
-                    </p>                                 
-                </td>
+                <th>Familienaam</th>
+                <th>Voornaam</th>
+                <th>Email</th>
+                <th>Type</th>
+                <th>Beheer</th>
             </tr>
-        <?php } ?>
-    </tbody>
-</table>     
+        </thead>
+        <tbody>
+            <?php foreach ($gebruikers as $gebruiker) { ?>
+                <tr>
+                    <td><?php echo $gebruiker->familienaam ?></td>
+                    <td><?php echo $gebruiker->voornaam ?></td>
+                    <td>
+                        <button class="emailGebruiker glyphicon glyphicon-envelope btn btn-primary groen" data-id="<?php echo $gebruiker->id ?>"></button>
+                        <?php echo $gebruiker->emailadres ?></td>
+                    <?php if ($gebruiker->typeId == 1) { ?>
+                        <td>Bezoeker</td>
+                    <?php } else { ?>
+                        <td>Spreker</td>
+                    <?php } ?>
+                    <td>
+                        <p>                                        
+                            <button class="wijzigGebruiker glyphicon glyphicon-pencil btn btn-primary" data-id="<?php echo $gebruiker->id ?>"></button>
+                            <button class="verwijderGebruiker glyphicon glyphicon-trash btn btn-danger" data-id="<?php echo $gebruiker->id ?>"></button> 
+                        </p>                                 
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>     
 </div>
