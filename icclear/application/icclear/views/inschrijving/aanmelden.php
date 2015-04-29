7073f95e71ad9d54371e74a586c38ba4c3885ecf
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-29 19:28:56 +0000

    inschrijven login/register opmaak

--- icclear/application/icclear/views/inschrijving/aanmelden.php
+++ icclear/application/icclear/views/inschrijving/aanmelden.php
@@ -1,5 +1,5 @@
-<div class="row space-bottom">
-    <div class="col-sm-12">
+<div class="row">
+    <div class="col-sm-12 space-bottom">
         <h1>Gelieve eerst in te loggen of registreren</h1>
         <p>Vooraleer wij uw inschrijving kunnen verwerken, moet u eerst inloggen of registreren.</p>
         <p>De inschrijving zal dan automatisch verwerkt worden. U hoeft dus <strong>niet</strong> nog eens in te schrijven.</p>
