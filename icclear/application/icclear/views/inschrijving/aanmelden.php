7a4dab9538b90da63fd37b4802899421b66220fc
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-29 13:53:47 +0000

    inschrijving aanmelden/registreren

--- icclear/application/icclear/views/inschrijving/aanmelden.php
+++ icclear/application/icclear/views/inschrijving/aanmelden.php
@@ -311,9 +311,11 @@ echo form_open('logon/add', $attributes);
     </div>         
     
     <div class="row margin-top">
-        <div class="btn-group col-md-12 col-sd-12">
+        <div class=" col-md-12 col-sd-12">
+        <div class="btn-group">
         <button type="button" class="btn btn-default" data-dismiss="modal">Annuleer</button>        
         <button name="mysubmit" id="mySubmit" class="btn btn-primary">Registreer</button>
+        </div>
         </div>
     </div>
     
