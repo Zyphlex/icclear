abe23d6b1f24560863043054da84e9c73f883d92
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-29 15:14:19 +0000

    sessie uitgebreid tonen

--- icclear/application/icclear/views/main_master.php
+++ icclear/application/icclear/views/main_master.php
@@ -18,17 +18,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title; ?></title>
     <meta charset="utf-8"> 
-    <link rel="stylesheet" type="text/css" 
-          href="<?php echo base_url() . APPPATH; ?>css/bootstrap.css" />
-    <link rel="stylesheet" type="text/css" 
-          href="<?php echo base_url() . APPPATH; ?>css/style.css" />
-    <link rel="stylesheet" type="text/css" 
-          href="<?php echo base_url() . APPPATH; ?>css/jquery.dataTables.min.css" />
     
-    <link rel="icon" type="image/png" 
-          href="<?php echo base_url() . APPPATH; ?>img/default/favicon.png"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() . APPPATH; ?>css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() . APPPATH; ?>css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() . APPPATH; ?>css/style.css" />
     
-    <script src="<?php echo base_url() . APPPATH; ?>js/jquery-1.11.2.min.js"></script>    
+    <link rel="icon" type="image/png" href="<?php echo base_url() . APPPATH; ?>img/default/favicon.png"/>
+    
+    <script src="<?php echo base_url() . APPPATH; ?>js/jquery-1.11.2.min.js"></script>  
+    <script src="<?php echo base_url() . APPPATH; ?>js/bootstrap.js"></script>
+    <script src="<?php echo base_url() . APPPATH; ?>js/jquery.dataTables.js"></script>  
+    
     
     <script type="text/javascript">
         var site_url = '<?php echo site_url();?>';
@@ -43,10 +43,7 @@
         <div id="navigatie"><?php echo $nav; ?></div>
         <div class="content" id="inhoud"><?php echo $content; ?></div>
         <div class="footer" id="footer"><?php echo $footer; ?></div>
-    </div>
-    
-  <script src="<?php echo base_url() . APPPATH; ?>js/bootstrap.js"></script> 
-  <script src="<?php echo base_url() . APPPATH; ?>js/jquery.dataTables.js"></script>    
+    </div>   
   
 </body>
 </html>
