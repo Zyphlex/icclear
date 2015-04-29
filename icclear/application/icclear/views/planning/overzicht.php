7d091c736d5c4cb5311c654677ecc66dafd95a2a
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-29 18:35:20 +0000

    modal programma overzicht fixers

--- icclear/application/icclear/views/planning/overzicht.php
+++ icclear/application/icclear/views/planning/overzicht.php
@@ -42,13 +42,88 @@
             Etiam vestibulum, velit nec consequat faucibus, nisi lectus tincidunt mi, in mattis eros mauris lobortis felis. 
             Sed nec tincidunt tortor, ac fermentum sem. 
             Aliquam dignissim, tellus id tincidunt facilisis, massa lectus tincidunt lacus, in pellentesque nulla magna vel neque.
-        </p>
-        <a href="" data-toggle="modal" data-target="#itemModal" class="toonItem" data-id="1">Heya</a>   
-        <button type="button" href="" class="toonItem" data-id="2">Button</button>                                   
-                                            
+        </p>                               
     </div>
 </div>
 
+
+<div class='row'>
+    <div class='col-md-12'>
+        <h3>Sessies</h3>        
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <?php
+                $id = 0;
+                $counter = 1;
+                foreach ($planningen as $dag) {
+                    if ($dag->conferentiedag->id != $id && $dag->conferentiedag->conferentieId == $actieveId->id) {
+                        echo "\n" . '<h4>Dag ' . $counter . '</h4>' . "\n";
+                        $counter++;
+                        ?>
+                        <table class = "table">
+                            <thead>
+                                <tr>
+                                    <th><span class="glyphicon glyphicon-time"></span> Tijdstip</th>
+                                    <th><span class="glyphicon glyphicon-paperclip"></span> Onderwerp</th>                                                                                                                
+                                    <th><span class="glyphicon glyphicon-bullhorn"></span> Spreker</th>
+                                </tr>
+                            </thead>  
+                            <tbody>            
+                                <?php $id = $dag->conferentiedag->id; ?>                
+                                <?php
+                                foreach ($planningen as $planning) {
+                                    if ($dag->conferentiedag->id == $planning->conferentiedag->id && $planning->conferentiedag->conferentieId == $actieveId->id) {
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $planning->beginUur . ' - ' . $planning->eindUur ?></td> 
+                                            <td><a href="" data-toggle="modal" data-target="#itemModal" class="toonItem" data-id="<?php echo $planning->sessie->id ?>"><?php echo $planning->sessie->onderwerp ?></a></td>                                    
+                                            <td><?php echo $planning->spreker->voornaam . ' ' . $planning->spreker->familienaam ?></td>
+                                        </tr>
+                                        <?php
+                                    }
+                                }
+                                ?>
+                            </tbody>
+                        </table>   
+                        <br/>
+                        <?php
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    </div>    
+</div>
+
+<div class='row'>
+    <div class='col-md-12'>
+        <h3>Activiteiten</h3>        
+        <div class="panel panel-default">
+            <div class="panel-body">                
+                <table class = "table">
+                    <thead>
+                        <tr>
+                            <th>Naam</th>
+                            <th>Omschrijving</th>
+                            <th>Prijs</th>
+                        </tr>
+                    </thead>  
+                    <tbody>            
+                        <?php foreach ($activiteiten as $activiteit) { ?>
+                            <tr>
+                                <td><?php echo $activiteit->naam ?></td>
+                                <td><?php echo $activiteit->omschrijving ?></td>
+                                <td><?php echo toKomma($activiteit->prijs) ?> EUR</td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>         
+
+            </div>
+        </div>
+
+    </div>    
+</div>
 
 
 <!-- MODAL VOOR DETAILS -->         
