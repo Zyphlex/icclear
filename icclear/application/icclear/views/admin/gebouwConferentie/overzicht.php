2a1f9231ddd3b3a065dc756f875ba21f3cf91a8f
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-29 14:49:34 +0000

    Pagina: gebouwen per dag

--- icclear/application/icclear/views/admin/gebouwConferentie/overzicht.php
+++ icclear/application/icclear/views/admin/gebouwConferentie/overzicht.php
@@ -1,20 +1,44 @@
 
 <div class="row">
-        <div class="col-md-10"> 
-            
-            <h2><?php echo $conferentie->naam ?></h2>
-            <h4><?php echo $conferentie->land->naam ?></h4>
+    <div class="col-md-10"> 
 
-                <table class="table table-responsive table-beheer">
-                    <thead>
-                        <tr>
-                            <th>Datum</th>
-                            <th>Gebouw</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        
-                    </tbody>
-                </table>  
-            </div>
+        <h2><?php echo $conferentie->naam ?></h2>
+        <h4><?php echo $conferentie->land->naam ?></h4>
+
+        <?php
+        $attributes = array('name' => 'myform', 'id' => 'myform');
+        echo form_open('gebouw/gebouwPerDagOpslaan', $attributes);
+        ?>
+
+        <table class="table table-responsive table-beheer">
+            <thead>
+                <tr>
+                    <th>Datum</th>
+                    <th>Gebouw</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $teller = 0;
+                foreach ($conferentiadagen as $conferentiedag) {
+                    $teller++;
+                    ?>                
+                    <tr>
+                        <td><?php echo $conferentiedag->datum ?></td>
+                        <td>
+                            <?php
+                            $options[0] = '-- Kies een gebouw --';
+                            foreach ($gebouwen as $gebouw) {
+                                $options[$gebouw->id] = $gebouw->naam;
+                            }
+                            echo form_dropdown('gebouw', $options, 0, 'class="form-control"');
+                            ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>  
+        <?php echo form_submit('gebouw/gebouwPerDagOpslaan', 'Opslaan', 'class="btn btn-default"'); ?>
+        <?php echo form_close(); ?>
+    </div>
 </div>
