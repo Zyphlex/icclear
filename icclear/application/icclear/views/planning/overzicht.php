f2322c59c33b50adaf805501edbc5566b696b213
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-29 20:14:54 +0000

    sessie uitgebreid tonen

--- icclear/application/icclear/views/planning/overzicht.php
+++ icclear/application/icclear/views/planning/overzicht.php
@@ -133,7 +133,7 @@
 
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title"></h4>
+                <h4 class="modal-title">Sessieonderwerp</h4>
             </div>
 
             <div class="modal-body">                  
