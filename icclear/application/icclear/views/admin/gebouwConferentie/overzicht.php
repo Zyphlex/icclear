e9377bad5981d2c4aeb556a71cdbf9c26b47efd8
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-29 14:18:01 +0000

    basis opmaak

--- icclear/application/icclear/views/admin/gebouwConferentie/overzicht.php
+++ icclear/application/icclear/views/admin/gebouwConferentie/overzicht.php
@@ -1,30 +1,20 @@
 
 <div class="row">
         <div class="col-md-10"> 
-            <?php foreach ($dagen as $dag) { ?>
-                <h3><?php echo toDDMMYYYY($dag->datum) ?></h3>
+            
+            <h2><?php echo $conferentie->naam ?></h2>
+            <h4><?php echo $conferentie->land->naam ?></h4>
 
                 <table class="table table-responsive table-beheer">
                     <thead>
                         <tr>
-
-                            <th>Naam</th>
-                            <th>Gemeente</th>                
-                            <th>Postcode</th>
-                            <th>Land</th>
+                            <th>Datum</th>
+                            <th>Gebouw</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <?php foreach ($dag->planning as $planning) { ?>
-                            <tr>
-                                <td><?php echo $planning->gebouw->naam ?></td>
-                                <td><?php echo $planning->gebouw->gemeente ?></td>
-                                <td><?php echo $planning->gebouw->postcode ?></td>
-                                <td><?php echo $planning->land->naam ?></td>
-                            </tr>
-                        <?php } ?>
+                        
                     </tbody>
                 </table>  
-        <?php } ?> 
             </div>
 </div>
