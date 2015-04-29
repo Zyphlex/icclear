4781dca2045e82efd28d2a2ae76a8d8b307ca532
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-29 14:21:05 +0000

    getPerLand

--- icclear/application/icclear/models/gebouw_model.php
+++ icclear/application/icclear/models/gebouw_model.php
@@ -2,67 +2,65 @@
 
 class Gebouw_model extends CI_Model {
 
-    function __construct()
-    {
+    function __construct() {
         parent::__construct();
     }
 
-       
-    function getAll()
-    {        
+    function getAll() {
         $query = $this->db->get('gebouw');
         return $query->result();
     }
-    
-    function get($id)
-    {        
+
+    function get($id) {
         $this->db->where('id', $id);
         $query = $this->db->get('gebouw');
         return $query->row();
     }
-        
-    function getGebouwenConferentie()
-    {
-        $this->load->model('conferentie_model');        
+
+    function getGebouwenConferentie() {
+        $this->load->model('conferentie_model');
         $conferentie = $this->conferentie_model->getActieveConferentie();
-        
+
         $this->db->where('conferentieId', $conferentie->id);
         $this->db->order_by('gebouwId', 'asc');
-        $query = $this->db->get('conferentiedag');   
+        $query = $this->db->get('conferentiedag');
         $dagen = $query->result();
-                  
+
         foreach ($dagen as $dag) {
             $dag->gebouw = $this->get($dag->gebouwId);
         }
-        
+
         return $dagen;
     }
-            
-    function update($gebouw)
-    {
+
+    function update($gebouw) {
         //Html entities en extra spaties verwijderen
         $gebouw = escape_html($gebouw);
         
         $this->db->where('id', $gebouw->id);
         $this->db->update('gebouw', $gebouw);
     }
-    
-    function insert($gebouw)
-    {
+
+    function insert($gebouw) {
         //Html entities en extra spaties verwijderen
         $gebouw = escape_html($gebouw);
         
         $this->db->insert('gebouw', $gebouw);
         return $this->db->insert_id();
     }
-    
-     function delete($id)
-    {
+
+    function delete($id) {
         $this->db->where('id', $id);
         $this->db->delete('gebouw');
     }
+
+    function getPerLand($landId) {
+        $this->db->where('landId', $landId);
+        $query = $this->db->get('gebouw');
+        return $query->result();
     
-    
+    }
+
 }
 
 ?>
