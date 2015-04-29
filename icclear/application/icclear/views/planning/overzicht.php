9d7d4f275edf63d93f81a685c6d28bd2ebde4def
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-29 15:00:12 +0000

    sessie uitgebreid tonen

--- icclear/application/icclear/views/planning/overzicht.php
+++ icclear/application/icclear/views/planning/overzicht.php
@@ -25,7 +25,7 @@
                 $("#onderwerp").val("");
                 $("#omschrijving").val("");
             } */
-            alert("test3");
+            alert("test4");
             // dialoogvenster openen
             $("#test").modal('show');
         });
@@ -130,10 +130,6 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
 
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title"></h4>
-            </div>
 
             <div class="modal-body">                  
 
