e8a271b4d0baeb602089bb4797620abd5a6f1801
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-30 18:51:27 +0000

    test aankondiging

--- icclear/application/icclear/views/admin/aankondiging/overzicht.php
+++ icclear/application/icclear/views/admin/aankondiging/overzicht.php
@@ -39,7 +39,7 @@
                         alert(result);
                         var jobject = jQuery.parseJSON(result);
                         $("#titel").val(jobject.titel);
-                        $("#inhoud").val(jobject.inhoud);
+                        $("#inhoud").html(jobject.inhoud);
                         $("#gepostDoor").val(jobject.gepostDoor);
                     }
                 });
