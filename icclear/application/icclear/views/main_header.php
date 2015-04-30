7c27cab4746a4c6d9de9c8786b112d31ff92dacc
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-30 19:36:15 +0000

    inlog knop rechts boven

--- icclear/application/icclear/views/main_header.php
+++ icclear/application/icclear/views/main_header.php
@@ -119,21 +119,20 @@
             <div class="row">
                 <div class="text-center underline">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h3>Aanmelden</h3>
+                    <h3>Wachtwoord vergeten</h3>
                 </div>   
 
                 <?php echo form_label('Emailadres:', 'email', array('class' => 'col-sm-4 control-label')); ?> 
                 <div class="col-sm-8">
                     <?php echo form_input(array('type' => 'email', 'name' => 'email', 'id' => 'email', 'class' => 'form-control', 'size' => '30')); ?>        
                 </div>
+
+                <div class="col-xs-12 margin-top space-bottom20">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Sluit</button>
+                    <input type="submit" name="mysubmit" value="Verstuur Email" class="btn btn-primary"  />
+                </div>
+
             </div>
-
-
-            <div class="col-xs-12 margin-top space-bottom20">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Sluit</button>
-                <input type="submit" name="mysubmit" value="Verstuur Email" class="btn btn-primary"  />
-            </div>
-
             <?php echo form_close(); ?>
             
         </div>            
