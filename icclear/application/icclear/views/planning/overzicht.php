6f9a4a5cf547a9f44a42f90699e9b3c904629b03
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-30 14:07:26 +0000

    programma spreker img

--- icclear/application/icclear/views/planning/overzicht.php
+++ icclear/application/icclear/views/planning/overzicht.php
@@ -202,8 +202,7 @@
 
                 <form id="JqAjaxForm">                     
                     <?php echo form_input(array('name' => 'id', 'type' => 'hidden', 'id' => 'id')); ?>
-                    <p><b>Foto:</b></p>
-                    <p><img id="foto" src="" width="100px" height="85px;"></p>
+                    <img class="left" id="foto" src="" width="100px" height="85px;">
 
                     <p><b>Naam:</b></p> 
                     <p><span id="voornaam"></span> <span id="familienaam"></span></p> 
