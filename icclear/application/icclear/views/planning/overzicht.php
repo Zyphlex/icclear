be5d1e7162894a6b7e288efdbc8be3d0ca7ab947
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-30 15:06:09 +0000

    programma spreker details

--- icclear/application/icclear/views/planning/overzicht.php
+++ icclear/application/icclear/views/planning/overzicht.php
@@ -109,7 +109,7 @@
                                 
                                         <tr class="under-link">
                                             <td>
-                                                <span class="label label-warning">
+                                                <span class="label label-danger">
                                                 <span aria-hidden="true" class="glyphicon glyphicon-time"></span> <?php echo $planning->beginUur . ' - ' . $planning->eindUur ?>
                                                 </span>
                                             </td> 
