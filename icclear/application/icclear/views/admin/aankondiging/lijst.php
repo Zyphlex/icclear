a70549046568b916e28fe5bd2c9879ea1504619e
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-30 15:12:03 +0000

    aankondigingen lijst

---
+++ icclear/application/icclear/views/admin/aankondiging/lijst.php
@@ -0,0 +1,29 @@
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.table').DataTable();
+    });
+</script>
+    
+<table class="table table-responsive table-beheer">
+    <thead>
+        <tr>                                
+            <th>Titel</th>   
+            <th>Inhoud</th>
+            <th>Gepost door</th>
+            <th>Beheer</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($aankondigingen as $aankondiging) { ?>
+            <tr>
+                <td><?php echo $aankondiging->titel ?></td>
+                <td><?php echo $aankondiging->inhoud ?></td>
+                <td><?php echo $aankondiging->admin->voornaam ?></td>
+                <td>
+                    <button class="wijzigItem glyphicon glyphicon-pencil btn btn-primary" data-id="<?php echo $aankondiging->id ?>"></button>
+                    <button class="verwijderItem glyphicon glyphicon-trash btn btn-danger" data-id="<?php echo $aankondiging->id ?>"></button>                                 
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
