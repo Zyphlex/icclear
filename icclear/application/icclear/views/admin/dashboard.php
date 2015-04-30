4ec149e97accfbf2c1578471f633ba7bcddf18ca
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-30 13:05:58 +0000

    Dashboard

--- icclear/application/icclear/views/admin/dashboard.php
+++ icclear/application/icclear/views/admin/dashboard.php
@@ -5,9 +5,9 @@
 <div class="col-md-10">    
     <h4>Conferentie <span class="glyphicon glyphicon-pushpin"></span></h4>
     <h5>Naam: <?php echo $conferentie->naam ?></h5>
-    <p>Periode: <?php echo $dataConferentie->beginDatum; ?> tot <?php echo $dataConferentie->eindDatum; ?></p>    
-    <p>Stad: <?php echo $dataConferentie->stad; ?></p>
-    <p>Maximum aantal inschrijvingen: <?php echo $dataConferentie->maxInschrijvingen; ?></p>
+    <p>Periode: <?php echo $conferentie->beginDatum; ?> tot <?php echo $conferentie->eindDatum; ?></p>    
+    <p>Stad: <?php echo $conferentie->stad; ?></p>
+    <p>Maximum aantal inschrijvingen: <?php echo $conferentie->maxInschrijvingen; ?></p>
     <p>Status: <?php echo $status->status; ?></p>
     <br>
     <h4>Statistieken <span class="glyphicon glyphicon-stats"></span></h4>
