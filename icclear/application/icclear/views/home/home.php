be01048ba9bef0dbac81a2236e602bbe28f11fb0
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-30 22:10:15 +0000

    home

--- icclear/application/icclear/views/home/home.php
+++ icclear/application/icclear/views/home/home.php
@@ -5,12 +5,12 @@
 </div>
 
 <div class="row">
-    <div class="col-md-8">
+    <div class="col-xs-8">
         <h2>Wat is IC Clear?</h2>
         <p><?php echo $algemeneinfo->omschrijving ?></p>
     </div> 
     
-    <div class="col-md-4">
+    <div class="col-sm-4">
         <div class="thumbnail">
             <img src="<?php echo base_url() . APPPATH; ?>img/default/home.jpg" alt="ICclear clarity" title="Home Afbeelding">
         </div>
@@ -21,10 +21,9 @@
 
 <div class="row">
     <?php foreach ($aankondigingen as $aankondiging){ ?>
-        <div class="col-md-4">
-                <h1><?php echo $aankondiging->titel; ?></h1>
-
-                <p><?php echo $aankondiging->inhoud; ?></p>
+        <div class="col-xs-4 panel panel-default">
+            <h1 class="underline"><?php echo $aankondiging->titel; ?></h1>
+            <p><?php echo $aankondiging->inhoud; ?></p>
         </div>
     <?php } ?>
 </div>
