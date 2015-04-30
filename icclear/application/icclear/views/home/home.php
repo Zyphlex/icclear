16c6367fd3a5c8d58058894d104cbfd19ce625c0
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-30 22:43:54 +0000

    home

--- icclear/application/icclear/views/home/home.php
+++ icclear/application/icclear/views/home/home.php
@@ -34,7 +34,7 @@ $( document ).ready(function() {
 
 <div class="row">
     <?php foreach ($aankondigingen as $aankondiging){ ?>  
-        <div class="col-sm-4">    
+        <div class="col-sm-4 col-xs-12">    
             <div class="panel panel-default">    
             <div class="nieuws-item">
                 <h1><?php echo $aankondiging->titel; ?></h1>
