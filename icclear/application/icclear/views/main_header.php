faab282c340fd164a1dc055ca1c8a4bd67594ecb
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-30 19:21:01 +0000

    inlog knop rechts boven

--- icclear/application/icclear/views/main_header.php
+++ icclear/application/icclear/views/main_header.php
@@ -8,7 +8,7 @@
     <div class="col-md-4 logon">
         <p>
             <span class="">
-                <a href="<?php echo base_url(); ?>icclear.php/logon/login" data-toggle="modal" data-target="#myModal">Aanmelden</a> 
+                <a href="#" data-toggle="modal" data-target="#myModal">Aanmelden</a> 
                 / 
                 <a href="<?php echo base_url(); ?>icclear.php/logon/register" data-toggle="modal" data-target="#myModal1">Registreer</a>
             </span>
@@ -51,7 +51,66 @@
 
 <!-- Modal -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog"><div class="modal-content"></div></div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            
+            <?php 
+    $attributes = array('name' => 'myform');
+    echo form_open('logon/aanmelden', $attributes);
+?>
+
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+         <h4 class="modal-title">Aanmelden</h4>
+    </div>
+
+
+    <div class="modal-body">        
+        
+        <div class="row">
+            <div class="col-md-4">      
+                <label for="email">E-mailadres:</label>    
+            </div>
+
+            <div class="col-md-8">
+                <input type="text" name="email" value="" id="email" size="30" class="form-control"  />    
+            </div>
+        </div>
+      
+        <div class="row">     
+            <div class="col-md-4">
+                <label for="password">Wachtwoord:</label>    
+            </div>
+
+            <div class="col-md-8">
+                <input type="password" name="password" value="" id="password" size="30" class="form-control"  />    
+            </div>
+        </div>
+        
+        <div class="row">     
+            <div class="col-md-4"></div>
+
+            <div class="col-md-8">  
+                <a href="<?php echo base_url(); ?>icclear.php/logon/vergeten" data-dismiss="modal" data-toggle="modal" data-target="#myModal2">Wachtwoord vergeten?</a>
+            </div>
+        </div>
+        
+
+    </div>
+
+
+    <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Annuleer</button>
+        <input type="submit" name="mysubmit" value="Aanmelden" class="btn btn-primary"  />
+    </div>
+    
+</form>
+
+
+
+            
+        </div>            
+    </div>
 </div>
 
 <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
