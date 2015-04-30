67522db3df4f298f296310ca92e385eed17597d8
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-04-30 13:27:11 +0000

    kleur hover icons sessie uitgebreid tonen

--- icclear/application/icclear/views/planning/overzicht.php
+++ icclear/application/icclear/views/planning/overzicht.php
@@ -202,7 +202,7 @@
                 <form id="JqAjaxForm">                     
                     <?php echo form_input(array('name' => 'id', 'type' => 'hidden', 'id' => 'id')); ?>
                     <p><b>Foto:</b></p>
-                    <p><img src="https://subversion.khk.be/projecten/TI1415project23/icclear/application/upload/default.jpg" width="100px" height="85px;"></p>
+                    <p><img id="foto" src="" width="100px" height="85px;"></p>
 
                     <p><b>Naam:</b></p> 
                     <p><span id="voornaam"></span> <span id="familienaam"></span></p> 
