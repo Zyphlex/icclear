8639456ef8344d910a9883b0dc5a2dad567cc94e
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-02 18:59:44 +0000

    sprekers overzicht

--- icclear/application/icclear/views/spreker/overzicht.php
+++ icclear/application/icclear/views/spreker/overzicht.php
@@ -9,19 +9,17 @@
         <div class="col-sm-3">
             <div class="equalizer panel panel-default">
                     <div class="panel-body">      
-                        <div class="center-block">
-                            <h4><?php echo$p->spreker->voornaam . ' ' . $p->spreker->familienaam ?></h4> 
+                            <h4 class="center-block"><?php echo$p->spreker->voornaam . ' ' . $p->spreker->familienaam ?></h4> 
                             <?php if ($p->spreker->foto == 'spreker' . $p->spreker->id . '.jpg') { ?>
-                                <img src="<?php echo base_url() . 'application/upload/fotos/sprekers/' . $p->spreker->foto; ?>" 
+                                <img class="center-block" src="<?php echo base_url() . 'application/upload/fotos/sprekers/' . $p->spreker->foto; ?>" 
                                  alt="Foto <?php echo$p->spreker->familienaam . ' ' . $p->spreker->voornaam; ?>" 
                                  title="Foto <?php echo$p->spreker->familienaam . ' ' . $p->spreker->voornaam; ?>"
                                  height="110" data-toggle="bottom">
                             <?php } else { ?>
-                                <img src="<?php echo base_url() . 'application/upload/fotos/sprekers/default.jpg'; ?>" 
+                                <img class="center-block" src="<?php echo base_url() . 'application/upload/fotos/sprekers/default.jpg'; ?>" 
                                  alt="Foto niet beschikbaar" title="Foto niet beschikbaar" height="110" data-toggle="bottom">
                             <?php } ?>
                         </div> 
-                    </div>
             </div>  
         </div>
     <?php } ?>
