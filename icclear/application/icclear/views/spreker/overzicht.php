2103643e0d6b2ee9ab97dcb051e94893505a2bf1
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-02 18:41:22 +0000

    sprekers overzicht

--- icclear/application/icclear/views/spreker/overzicht.php
+++ icclear/application/icclear/views/spreker/overzicht.php
@@ -5,32 +5,30 @@
 </div>
 
 <div class="row"> 
-    <?php foreach ($sprekers as $sp) { ?>   
-    <?php foreach ($sp->programma->sessie as $spreker) { ?>   
+    <?php foreach ($sprekers as $p) { ?>    
         <div class="col-md-4">
             <div class="panel panel-default">
                 <div class="row">
                     <div class="panel-body">
-                        <div class="col-md-5">
-                            <?php if ($spreker->foto == 'spreker' . $spreker->id . '.jpg') { ?>
-                            <img src="<?php echo base_url() . 'application/upload/fotos/sprekers/' . $spreker->foto; ?>" 
-                                 alt="<?php echo$spreker->familienaam . ' ' . $spreker->voornaam; ?>" 
-                                 title="<?php echo$spreker->familienaam . ' ' . $spreker->voornaam; ?>"
+                        <div class="col-sm-5 col-xs-12">
+                            <?php if ($p->spreker->foto == 'spreker' . $p->spreker->id . '.jpg') { ?>
+                            <img src="<?php echo base_url() . 'application/upload/fotos/sprekers/' . $p->spreker->foto; ?>" 
+                                 alt="<?php echo$p->spreker->familienaam . ' ' . $spreker->voornaam; ?>" 
+                                 title="<?php echo$p->spreker->familienaam . ' ' . $spreker->voornaam; ?>"
                                  height="110">
                             <?php } else { ?>
                             <img src="<?php echo base_url() . 'application/upload/fotos/sprekers/default.jpg'; ?>" 
                                  alt="Foto niet beschikbaar" title="Foto niet beschikbaar" height="110">
                             <?php } ?>
                         </div>
-                        <div class="col-md-7">
-                            <h4><?php echo$spreker->voornaam . ' ' . $spreker->familienaam ?></h4> 
+                        <div class="col-sm-7 col-xs-12">
+                            <h4><?php echo$p->spreker->voornaam . ' ' . $p->spreker->familienaam ?></h4> 
                             <p class="italic">Sessies:</p>
-                            <p><?php echo $spreker->sessie->onderwerp ?></p>
+                            <p><?php echo $p->spreker->sessie->onderwerp ?></p>
                         </div>
                     </div>      
                 </div> 
             </div>  
         </div>
-    <?php } ?>
     <?php } ?>
 </div>
