7948f45df83b992802aa8ccba0fe8fc25ef38573
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-02 21:38:38 +0000

    hmmm

--- icclear/application/icclear/views/admin_sidenav.php
+++ icclear/application/icclear/views/admin_sidenav.php
@@ -4,7 +4,7 @@
         </div>
     </div>-->
     
-    <div class="col-md-2">  
+    <div class="col-xs-2">  
         <h4><span class="glyphicon glyphicon-th-large"></span> DASHBOARD</h4>                         
         <div class="panel panel-default">
             
