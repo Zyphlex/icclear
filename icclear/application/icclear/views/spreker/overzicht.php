4951952fa137443dc6d7a40a9517a8a70c333b48
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-02 18:52:56 +0000

    sprekers overzicht

--- icclear/application/icclear/views/spreker/overzicht.php
+++ icclear/application/icclear/views/spreker/overzicht.php
@@ -10,7 +10,8 @@
             <div class="panel panel-default">
                 <div class="row">
                     <div class="panel-body">
-                        <div class="col-sm-5 col-xs-12">
+                        <div class="col-sm-5 col-xs-12">                            
+                            <h4><?php echo$p->spreker->voornaam . ' ' . $p->spreker->familienaam ?></h4> 
                             <?php if ($p->spreker->foto == 'spreker' . $p->spreker->id . '.jpg') { ?>
                             <img src="<?php echo base_url() . 'application/upload/fotos/sprekers/' . $p->spreker->foto; ?>" 
                                  alt="<?php echo$p->spreker->familienaam . ' ' . $spreker->voornaam; ?>" 
@@ -20,11 +21,6 @@
                             <img src="<?php echo base_url() . 'application/upload/fotos/sprekers/default.jpg'; ?>" 
                                  alt="Foto niet beschikbaar" title="Foto niet beschikbaar" height="110">
                             <?php } ?>
-                        </div>
-                        <div class="col-sm-7 col-xs-12">
-                            <h4><?php echo$p->spreker->voornaam . ' ' . $p->spreker->familienaam ?></h4> 
-                            <p class="italic">Sessies:</p>
-                            <p><?php echo $p->spreker->sessie->onderwerp ?></p>
                         </div>
                     </div>      
                 </div> 
