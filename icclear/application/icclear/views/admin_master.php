54e0e30b517897026ad0a4f05d923469a67d0bc0
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-02 20:46:15 +0000

    datatable style

--- icclear/application/icclear/views/admin_master.php
+++ icclear/application/icclear/views/admin_master.php
@@ -22,12 +22,15 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() . APPPATH; ?>css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() . APPPATH; ?>css/jquery.dataTables.min.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() . APPPATH; ?>css/style.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() . APPPATH; ?>css/dataTables.bootstrap.css" />
     
     <link rel="icon" type="image/png" href="<?php echo base_url() . APPPATH; ?>img/default/favicon.png"/>
     
     <script src="<?php echo base_url() . APPPATH; ?>js/jquery-1.11.2.min.js"></script>  
     <script src="<?php echo base_url() . APPPATH; ?>js/bootstrap.js"></script>
     <script src="<?php echo base_url() . APPPATH; ?>js/jquery.dataTables.js"></script>    
+    <script src="<?php echo base_url() . APPPATH; ?>js/dataTables.bootstrap.js"></script>   
+    <script src="<?php echo base_url() . APPPATH; ?>js/script.js"></script>
     
     <script type="text/javascript">
         var site_url = '<?php echo site_url();?>';
