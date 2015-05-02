17bc0fdcc64d95d96b7ae67acb959b09329d1c49
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-02 18:56:17 +0000

    sprekers overzicht

--- icclear/application/icclear/views/spreker/overzicht.php
+++ icclear/application/icclear/views/spreker/overzicht.php
@@ -6,18 +6,18 @@
 
 <div class="row"> 
     <?php foreach ($sprekers as $p) { ?>    
-        <div class="col-sm-4">
+        <div class="col-sm-3">
             <div class="equalizer panel panel-default">
-                    <div class="panel-body">                        
+                    <div class="panel-body center-block">                        
                             <h4><?php echo$p->spreker->voornaam . ' ' . $p->spreker->familienaam ?></h4> 
                             <?php if ($p->spreker->foto == 'spreker' . $p->spreker->id . '.jpg') { ?>
                                 <img src="<?php echo base_url() . 'application/upload/fotos/sprekers/' . $p->spreker->foto; ?>" 
                                  alt="Foto <?php echo$p->spreker->familienaam . ' ' . $p->spreker->voornaam; ?>" 
                                  title="Foto <?php echo$p->spreker->familienaam . ' ' . $p->spreker->voornaam; ?>"
-                                 height="110">
+                                 height="110" data-toggle="bottom">
                             <?php } else { ?>
                                 <img src="<?php echo base_url() . 'application/upload/fotos/sprekers/default.jpg'; ?>" 
-                                 alt="Foto niet beschikbaar" title="Foto niet beschikbaar" height="110">
+                                 alt="Foto niet beschikbaar" title="Foto niet beschikbaar" height="110" data-toggle="bottom">
                             <?php } ?>
                         </div> 
             </div>  
