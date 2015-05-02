a9d0240667a50d13aead8d299a7b18fbc0ac34d5
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-02 21:25:43 +0000

    collapse try

--- icclear/application/icclear/views/admin_sidenav.php
+++ icclear/application/icclear/views/admin_sidenav.php
@@ -4,16 +4,12 @@
         </div>
     </div>-->
 <div class="col-sm-2">    
-        
-        
-        
-    </div>
 
 
-<nav id="myNavmenu" class="navmenu navmenu-default navmenu-fixed-left offcanvas-sm" role="navigation">
-    <h4><span class="glyphicon glyphicon-th-large"></span> DASHBOARD</h4>                         
+    <nav id="myNavmenu" class="navmenu navmenu-default navmenu-fixed-left offcanvas-sm" role="navigation">
+        <h4><span class="glyphicon glyphicon-th-large"></span> DASHBOARD</h4>                         
         <div class="panel panel-default">
-            
+
             <div class="panel-heading" role="tab" id="collapseListGroupHeading1">
                 <h4 class="panel-title">
                     Beheren
@@ -23,41 +19,41 @@
             <div class="panel-body">
                 <h4>Algemeen</h4>
                 <ul class="nav nav-pills nav-stacked">
-                    <li><?php echo anchor('adminbeheer' , 'Admins'); ?></li>
-                    <li><?php echo anchor('gebruiker' , 'Gebruikers'); ?></li>
-                    <li><?php echo anchor('activiteit' , 'Activiteiten'); ?></li>
-                    <li><?php echo anchor('land' , 'Landen'); ?></li>
-                    <li><?php echo anchor('zaal' , 'Zalen'); ?></li>
-                    <li><?php echo anchor('gebouw' , 'Gebouwen'); ?></li>
-                    <li><?php echo anchor('routesbeheer' , 'Routes'); ?></li>
-                    <li><?php echo anchor('hotels' , 'Hotels'); ?></li>
+                    <li><?php echo anchor('adminbeheer', 'Admins'); ?></li>
+                    <li><?php echo anchor('gebruiker', 'Gebruikers'); ?></li>
+                    <li><?php echo anchor('activiteit', 'Activiteiten'); ?></li>
+                    <li><?php echo anchor('land', 'Landen'); ?></li>
+                    <li><?php echo anchor('zaal', 'Zalen'); ?></li>
+                    <li><?php echo anchor('gebouw', 'Gebouwen'); ?></li>
+                    <li><?php echo anchor('routesbeheer', 'Routes'); ?></li>
+                    <li><?php echo anchor('hotels', 'Hotels'); ?></li>
                     <li><?php echo anchor('sponsorbeheer', 'Sponsors'); ?></li>
-                    <li><?php echo anchor('faqbeheer' , 'F.A.Q.'); ?></li>
-                    
-            
-            
-            <?php if($conferentieId != null) { ?>
-                    
-                <h4>Conferentie</h4>
-                    <li><?php echo anchor('conferentie', 'Conferentie'); ?></li>
-                    <li><?php echo anchor('inschrijven/opvolgen', 'Inschrijvingen'); ?></li>
-                    <li><?php echo anchor('sessies', 'Sessies'); ?></li>
-                    <li><?php echo anchor('aankondiging/', 'Aankondigingen'); ?></li>
-                    <li><?php echo anchor('planningbeheer' , 'Planningen'); ?></li>
-                    <li><?php echo anchor('gebouw/gebouwPerDag/' . $conferentieId, 'Gebouwen') ?></li>
-                
-            <?php } ?>            
-            
+                    <li><?php echo anchor('faqbeheer', 'F.A.Q.'); ?></li>
+
+
+
+                    <?php if ($conferentieId != null) { ?>
+
+                        <h4>Conferentie</h4>
+                        <li><?php echo anchor('conferentie', 'Conferentie'); ?></li>
+                        <li><?php echo anchor('inschrijven/opvolgen', 'Inschrijvingen'); ?></li>
+                        <li><?php echo anchor('sessies', 'Sessies'); ?></li>
+                        <li><?php echo anchor('aankondiging/', 'Aankondigingen'); ?></li>
+                        <li><?php echo anchor('planningbeheer', 'Planningen'); ?></li>
+                        <li><?php echo anchor('gebouw/gebouwPerDag/' . $conferentieId, 'Gebouwen') ?></li>
+
+                    <?php } ?>            
+
                 </ul>
             </div>
         </div>
-</nav>
-<div class="navbar navbar-default navbar-fixed-top">
-    <button type="button" class="navbar-toggle" data-toggle="offcanvas" data-target="#myNavmenu" data-canvas="body">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-    </button>
-</div>
+    </nav>
+    <div class="navbar navbar-default navbar-fixed-top">
+        <button type="button" class="navbar-toggle" data-toggle="offcanvas" data-target="#myNavmenu" data-canvas="body">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+    </div>
 
 </div>
