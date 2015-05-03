c579f7e95ae9079d0d0c37e185eff9b59d7950c7
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-03 14:04:11 +0000

    required test

--- icclear/application/icclear/views/inschrijving/inschrijving.php
+++ icclear/application/icclear/views/inschrijving/inschrijving.php
@@ -11,10 +11,8 @@ $(document).ready(function() {
     $("#aanwezig").click(function() {
         if ($(this).checked = false) {
             $("input[id='aantalPersonen'][name='" + $(this).attr('value') + "']").prop('required',false);
-            alert('false');
         } else if ($(this).checked) {
             $("input[id='aantalPersonen'][name='" + $(this).attr('value') + "']").prop('required',true);
-            alert("true");
         }
     });
 });
