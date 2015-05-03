2d75d98d62c679e20ea4438dbca393b8c640166b
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-03 12:15:38 +0000

    inschrijven dubbele code in aparte function test

--- icclear/application/icclear/views/inschrijving/inschrijving.php
+++ icclear/application/icclear/views/inschrijving/inschrijving.php
@@ -1,8 +1,8 @@
 <script type='text/javascript'>       
 $(document).ready(function() {
     $("#aantalPersonen").onchange(function() {
-        alert("#aanwezig" + $(this).attr(name));
-        $("#aanwezig" + $(this).attr(name)).prop('checked');
+        alert("#aanwezig" + $(this).attr('name'));
+        $("#aanwezig" + $(this).attr('name')).prop('checked');
     });
 });
 </script>
