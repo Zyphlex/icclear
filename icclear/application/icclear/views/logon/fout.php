09287ffb4b1103888e86415988f97f652ecd86a7
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-04 21:33:28 +0000

    voorstel indienen inschrijven naderhand en inschrijven conferentie

--- icclear/application/icclear/views/logon/fout.php
+++ icclear/application/icclear/views/logon/fout.php
@@ -1,6 +1,6 @@
 <div class="bs-example">
-    <div class="alert alert-danger">        
-        <h1 class="text-center">Er is een fout opgetreden.</h1>
+    <div class="space-bottom alert alert-danger">        
+        <h1 class="space-bottom15 text-center">Er is een fout opgetreden.</h1>
         <p class="space-bottom danger italic text-center">U heeft een fout e-mailadres of wachtwoord ingegeven.</p>      
         
         <a href="javascript:history.go(-1)" class="col-xs-offset-3 col-xs-6 btn btn-default">Terug</a>
