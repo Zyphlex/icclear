4f81c8614101de9188a3677b53a11889b80df155
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-04 19:07:17 +0000

    land tonen bij conferentie

--- icclear/application/icclear/views/admin/beheer.php
+++ icclear/application/icclear/views/admin/beheer.php
@@ -16,27 +16,27 @@
             <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
                 <div class="panel-body">
                     <div class="table-responsive">
-                    <table class="table table-beheer" style="margin-left: 50px">
-                        <thead>
-                            <tr>
-                                <th>Naam</th>                                
-                                <th>Periode</th>
-                                <th>Stad</th>
-                                <th>Land</th>
-                                <th>Beheer</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td style="width:200px"><?php echo $conferentie->naam ?></td>                                
-                                <td style="width:250px"><?php echo toDDMMYYYY($conferentie->beginDatum) . " - " . toDDMMYYYY($conferentie->eindDatum) ?></td>
-                                <td style="width:150px"><?php echo $conferentie->stad ?></td>
-                                <td style="width:150px"><?php echo $conferentie->land->naam ?></td>
-                                <td style="width:150px"><?php echo anchor('admin/dashboard/' . $conferentie->id, 'Beheren', 'class="btn btn-default"'); ?></td>
-                            </tr>
+                        <table class="table table-beheer" style="margin-left: 50px">
+                            <thead>
+                                <tr>
+                                    <th>Naam</th>                                
+                                    <th>Periode</th>
+                                    <th>Stad</th>
+                                    <th>Land</th>
+                                    <th>Beheer</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td style="width:200px"><?php echo $conferentie->naam ?></td>                                
+                                    <td style="width:250px"><?php echo toDDMMYYYY($conferentie->beginDatum) . " - " . toDDMMYYYY($conferentie->eindDatum) ?></td>
+                                    <td style="width:150px"><?php echo $conferentie->stad ?></td>
+                                    <td style="width:150px"><?php echo $conferentie->land->naam ?></td>
+                                    <td style="width:150px"><?php echo anchor('admin/dashboard/' . $conferentie->id, 'Beheren', 'class="btn btn-default"'); ?></td>
+                                </tr>
 
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -53,28 +53,28 @@
             <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
                 <div class="panel-body">
                     <div class="table-responsive">
-                    <table class="table table-beheer" style="margin-left: 50px">
-                        <thead>
-                            <tr>
-                                <th>Naam</th>
-                                <th>Stad</th>
-                                <th>Begindatum</th>
-                                <th>Einddatum</th>
-                                <th>Beheer</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($toekomenden as $toe) { ?>
+                        <table class="table table-beheer" style="margin-left: 50px">
+                            <thead>
                                 <tr>
-                                    <td style="width:250px"><?php echo $toe->naam ?></td>
-                                    <td style="width:200px"><?php echo $toe->stad ?></td>
-                                    <td style="width:200px"><?php echo toDDMMYYYY($toe->beginDatum) ?></td>
-                                    <td style="width:200px"><?php echo toDDMMYYYY($conferentie->eindDatum) ?></td>
-                                    <td style="width:150px"><?php echo anchor('admin/dashboard/' . $toe->id, 'Beheren', 'class="btn btn-default"'); ?></td>
+                                    <th>Naam</th>                                
+                                    <th>Periode</th>
+                                    <th>Stad</th>
+                                    <th>Land</th>
+                                    <th>Beheer</th>
                                 </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($toekomenden as $toe) { ?>
+                                    <tr>
+                                        <td style="width:200px"><?php echo $toe->naam ?></td>                                
+                                        <td style="width:250px"><?php echo toDDMMYYYY($toe->beginDatum) . " - " . toDDMMYYYY($conferentie->eindDatum) ?></td>
+                                        <td style="width:150px"><?php echo $toe->stad ?></td>
+                                        <td style="width:150px"><?php echo $toe->land->naam ?></td>
+                                        <td style="width:150px"><?php echo anchor('admin/dashboard/' . $toe->id, 'Beheren', 'class="btn btn-default"'); ?></td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -91,35 +91,35 @@
             <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
                 <div class="panel-body">
                     <div class="table-responsive">
-                    <table class="table table-beheer" style="margin-left: 50px">
-                        <thead>
-                            <tr>
-                                <th>Naam</th>
-                                <th>Stad</th>
-                                <th>Begindatum</th>
-                                <th>Einddatum</th>
-                                <th>Beheer</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($verleden as $ver) { ?>
+                        <table class="table table-beheer" style="margin-left: 50px">
+                            <thead>
                                 <tr>
-                                    <td style="width:150px"><?php echo $ver->naam ?></td>
-                                    <td style="width:130px"><?php echo $ver->stad ?></td>
-                                    <td style="width:130px"><?php echo toDDMMYYYY($ver->beginDatum) ?></td>
-                                    <td style="width:130px"><?php echo toDDMMYYYY($ver->eindDatum) ?></td>
-                                    <td style="width:100px"><?php echo anchor('admin/dashboard/' . $ver->id, 'Beheren', 'class="btn btn-default"'); ?></td>
+                                    <th>Naam</th>                                
+                                    <th>Periode</th>
+                                    <th>Stad</th>
+                                    <th>Land</th>
+                                    <th>Beheer</th>
                                 </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($verleden as $ver) { ?>
+                                    <tr>
+                                        <td style="width:200px"><?php echo $ver->naam ?></td>                                
+                                        <td style="width:250px"><?php echo toDDMMYYYY($ver->beginDatum) . " - " . toDDMMYYYY($conferentie->eindDatum) ?></td>
+                                        <td style="width:150px"><?php echo $ver->stad ?></td>
+                                        <td style="width:150px"><?php echo $ver->land->naam ?></td>
+                                        <td style="width:150px"><?php echo anchor('admin/dashboard/' . $ver->id, 'Beheren', 'class="btn btn-default"'); ?></td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    
+
     <?php echo anchor('conferentie/toevoegen', 'Nieuwe conferentie toevoegen', 'class="btn btn-default"'); ?>  
-    
+
 </div>
 
