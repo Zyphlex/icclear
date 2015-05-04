0b4c449f8a208d9914d18268eae1b5df7487aea2
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-04 14:53:15 +0000

    zalen planning fix

--- icclear/application/icclear/views/admin/planning/overzicht.php
+++ icclear/application/icclear/views/admin/planning/overzicht.php
@@ -163,10 +163,8 @@
                     <p><?php echo form_label('Zaal:', 'zaal'); ?></p>
                     <?php
                     $optionsZaal = array();
-                    foreach ($gebouwen->gebouw as $g) {
-                        foreach ($g->zalen as $z) {
+                    foreach ($gebouw->gebouw->zalen as $z) {
                             $optionsZaal[$z->id] = $z->naam . " (" . $z->naam . " , " . $z->maximumAantalPersonen . " maximum aantal plaatsen)";
-                        }
                     }
                     echo form_dropdown('zaal', $optionsZaal, '', 'id="zaal" class="form-control"');
                     ?>
