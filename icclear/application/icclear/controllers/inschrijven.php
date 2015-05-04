c5862c580b2db63767c3c414eeee1d8d083c6428
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-04 20:37:46 +0000

    voorstel indienen inschrijven naderhand

--- icclear/application/icclear/controllers/inschrijven.php
+++ icclear/application/icclear/controllers/inschrijven.php
@@ -162,10 +162,8 @@ class Inschrijven extends CI_Controller {
                             
         $this->email->from('donotreply@thomasmore.be');
         $this->email->to($user->emailadres);
-        $this->email->subject("Inschrijving voor " + $conferentie->naam);
-        $this->email->message("Beste " + $user->voornaam + " " + $user->familienaam +
-                " " +
-                "Met deze mail bevestigen wij uw inschrijving voor de conferentie  " + $conferentie->naam + " die loopt van " + $conferentie->beginDatum + " tot " + $conferentie->einddatum + "." );
+        $this->email->subject('Inschrijving voor ' + $conferentie->naam);
+        $this->email->message('Beste ' + $user->voornaam + '\n\n ' + $user->familienaam + '\n\n ' + ' Met deze mail bevestigen wij uw inschrijving voor de conferentie  ' + $conferentie->naam + ' die loopt van ' + $conferentie->beginDatum + ' tot ' + $conferentie->einddatum + '.' );
         $this->email->send();
 
         redirect('inschrijven/voorkeuren');
