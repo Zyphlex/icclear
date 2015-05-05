8a809752f0c4e24419ecacfd0ed67ecffdbffef1
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-05 20:55:39 +0000

    verwijderen inschrijving

--- icclear/application/icclear/views/admin/inschrijving/overzicht.php
+++ icclear/application/icclear/views/admin/inschrijving/overzicht.php
@@ -3,7 +3,7 @@
     function haaloverzicht() {
         $.ajax({type: "GET",
             url: site_url + "/inschrijven/overzicht",
-            success: function(result) {
+            success: function (result) {
                 $("#resultaat").html(result);
                 maakDetailClick();
                 maakDeleteClick();
@@ -21,7 +21,7 @@
 
     //Klikken op de Verwijderen knop
     function maakDeleteClick() {
-        $(".verwijderItem").click(function() {
+        $(".verwijderItem").click(function () {
             deleteid = $(this).data("id");
             $("#modalItemDelete").modal('show');
         });
@@ -29,7 +29,7 @@
 
     //Klikken op de Wijzig knop/Toevoeg knop
     function maakDetailClick() {
-        $(".wijzigItem").click(function() {
+        $(".wijzigItem").click(function () {
             var iddb = $(this).data("id");
             $("#id").val(iddb);
             if (iddb != 0) {
@@ -38,7 +38,7 @@
                     url: site_url + "/inschrijven/detail",
                     async: false,
                     data: {id: iddb},
-                    success: function(result) {
+                    success: function (result) {
                         var jobject = jQuery.parseJSON(result);
                         $("#gebruiker").val(jobject.gebruikerId);
                         $("#confonderdeel").val(jobject.conferentieOnderdeelId);
@@ -49,23 +49,26 @@
             } else {
                 // bij toevoegen gewoon vakken leeg maken
                 $("#gebruiker").val("");
+                $("#confonderdeel").val("");
+                $("#betalingId").val("");
+                $("#methode").val("");
             }
             // dialoogvenster openen
             $("#modalItemDetail").modal('show');
         });
     }
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         //Link leggen met de knoppen die gemaakt worden in lijst.php
         maakDetailClick();
         maakDeleteClick();
         //Lijst eerste maal ophalen en tonen
         haaloverzicht();
-        
+
         $('.table').DataTable();
-        
+
         //Klikken op "OPSLAAN" in de Detail modal
-        $(".opslaanItem").click(function() {
+        $(".opslaanItem").click(function () {
             var dataString = $("#JqAjaxForm:eq(0)").serialize();
             $.ajax({
                 type: "POST",
@@ -79,13 +82,13 @@
         });
 
         //Klikken op "BEVESTIG" in de Delete modal
-        $(".deleteItem").click(function() {
+        $(".deleteItem").click(function () {
             $.ajax({
                 type: "POST",
                 url: site_url + "/inschrijven/delete",
                 async: false,
                 data: {id: deleteid},
-                success: function(result) {
+                success: function (result) {
                     if (result == '0') {
                         alert("Er is iets foutgelopen!");
                     } else {
@@ -102,7 +105,7 @@
 <div class="col-md-10">
 
     <h1>Ingeschrevenen</h1>  
-    
+
     <div id="resultaat"></div>
 
     <?php echo anchor('admin/dashboard' . $conferentieId, 'Annuleren', 'class="btn btn-default"'); ?> 
@@ -126,7 +129,7 @@
                 <form id="JqAjaxForm">
                     <input type="hidden" name="id" id="id" />
                     <input type="hidden" name="betalingId" id="betalingId" />
-                    
+
                     <p><?php echo form_label('Gebruiker:', 'gebruiker'); ?></td>
                     <p><?php
                         $options = array();
