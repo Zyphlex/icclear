001306f806e35814023354039484448d92175854
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-05 16:37:48 +0000

    pers dos layout

--- icclear/application/icclear/views/gebruiker/wijzigen.php
+++ icclear/application/icclear/views/gebruiker/wijzigen.php
@@ -26,8 +26,8 @@
 
                 <ul class="nav nav-pills nav-stacked" role="tablist">
                     <li role="presentation" class="active"><a href="#account" aria-controls="account" role="tab" data-toggle="tab">Account</a></li>
-                    <li role="presentation"><a href="#conferenties" aria-controls="conferenties" role="tab" data-toggle="tab">Conferenties</a><span class="pull-right badge">14</span></li>
-                    <li role="presentation"><a href="#betalingen" aria-controls="betalingen" role="tab" data-toggle="tab">Betalingen<span class="pull-right badge">14</span></a></li>                        
+                    <li role="presentation"><a href="#conferenties" aria-controls="conferenties" role="tab" data-toggle="tab">Conferenties<span class="pull-right badge"><?php echo count($inschrijvingen); ?></span></a></li>
+                    <li role="presentation"><a href="#betalingen" aria-controls="betalingen" role="tab" data-toggle="tab">Betalingen<span class="pull-right badge"><?php echo count($inschrijvingen->betalingId != null); ?></span></a></li>                        
                 </ul>
             </div>   
         </div>
