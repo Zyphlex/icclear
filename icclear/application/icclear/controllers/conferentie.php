1ded73ebe93d38604e84311bbd029cf543bd8173
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-05 11:22:26 +0000

    conferentie wijzigen block

--- icclear/application/icclear/controllers/conferentie.php
+++ icclear/application/icclear/controllers/conferentie.php
@@ -47,9 +47,8 @@ class Conferentie extends CI_Controller {
         $this->load->model('land_model');
         $data['landen'] = $this->land_model->getAll();
 
-        $this->load->model('conferentie_model');
-        $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
-        $data['inschrijvingen'] = $this->conferentie_model->getAllInschijvingByConferentie($this->session->userdata('conferentieId'));
+        $this->load->model('inschrijving_model');
+        $data['inschrijvingen'] = $this->inschrijving_model->getAllInschijvingByConferentie($this->session->userdata('conferentieId'));
 
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'sidenav' => 'admin_sidenav', 'content' => 'admin/conferentie/wijzigen', 'footer' => 'main_footer');
         $this->template->load('admin_master', $partials, $data);
