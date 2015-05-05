876a39ded7ea8280f738c1788b738a2c9b42e1e6
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-05 19:56:16 +0000

    pers dossier

--- icclear/application/icclear/views/gebruiker/wijzigen.php
+++ icclear/application/icclear/views/gebruiker/wijzigen.php
@@ -17,10 +17,11 @@
                     data: {id: iddb},
                     success: function(result) {
                         var jobject = jQuery.parseJSON(result);
+                        alert(jobject[0]);
                         $("#conf1").html(jobject.conferentie.naam);                        
                         var tabel;
                         $.each(jobject, function(index,val) {
-                            tabel += '<tr><td>' + val.id + '</td><td>' + '</td><td>' + '</td></tr>';
+                            tabel += '<tr><td>' + val.id + '</td><td>' + val.activiteit.prijs + '</td><td>' + val.aantalPersonen + '</td></tr>';
                         });
                         $("#activiteiten1").html(tabel);
                     }
