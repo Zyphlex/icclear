0d299be0f537d3137b0bc45827a30dc174bdfb94
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-05 20:37:17 +0000

    pers dossier

--- icclear/application/icclear/views/gebruiker/lijst.php
+++ icclear/application/icclear/views/gebruiker/lijst.php
@@ -2,18 +2,17 @@
     <table class="table table-beheer">
         <thead>
             <tr>
-                <th>Tijdstip</th>
-                <th>Sessie</th>      
+                <th>Activiteit</th>
+                <th>Personen</th>      
             </tr>
         </thead>
         <tbody>
             <?php foreach ($act as $a) { ?>
-            <?php foreach ($a->activiteit as $item) { ?>
                 <tr>
-                    <td><?php echo $a->naam . " tot " . $planning->eindUur ?></td>
-                    <td><?php echo $planning->sessie->onderwerp ?></td>
+                    <td><?php echo $a->naam ?></td>
+                    <td><?php echo $a->aantalPersonen ?> (<span class="italic"><?php echo $a->prijs ?></span>pp)</td>
+                    <td>&euro; <?php echo ($a->prijs * $a->aantalPersonen) ?></td>
                 </tr>
-            <?php } ?>
             <?php } ?>
         </tbody>
     </table>  
