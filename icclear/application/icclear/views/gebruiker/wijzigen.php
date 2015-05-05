e9881ddb23e2cd594f96037053915339255c1ddb
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-05 16:35:57 +0000

    pers dos layout

--- icclear/application/icclear/views/gebruiker/wijzigen.php
+++ icclear/application/icclear/views/gebruiker/wijzigen.php
@@ -26,7 +26,7 @@
 
                 <ul class="nav nav-pills nav-stacked" role="tablist">
                     <li role="presentation" class="active"><a href="#account" aria-controls="account" role="tab" data-toggle="tab">Account</a></li>
-                    <li role="presentation"><a href="#conferenties" aria-controls="conferenties" role="tab" data-toggle="tab">Conferenties14</span></a><span class="pull-right badge"></li>
+                    <li role="presentation"><a href="#conferenties" aria-controls="conferenties" role="tab" data-toggle="tab">Conferenties</a><span class="pull-right badge">14</span></li>
                     <li role="presentation"><a href="#betalingen" aria-controls="betalingen" role="tab" data-toggle="tab">Betalingen<span class="pull-right badge">14</span></a></li>                        
                 </ul>
             </div>   
