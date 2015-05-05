15b03777742b70e287e0e9b5534093223adfd972
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-05 20:47:48 +0000

    sessiesvoorkeuren

--- icclear/application/icclear/views/inschrijving/voorkeuren.php
+++ icclear/application/icclear/views/inschrijving/voorkeuren.php
@@ -1,1 +1,39 @@
 <h1>Voorkeuren</h1>
+<form>
+
+</form>
+
+
+<div class="col-md-10">    
+    <?php
+    $attributes = array('name' => 'myform', 'id' => 'myform');
+    echo form_open('voorkeur/updat', $attributes);
+    ?>             
+    <div class="row">
+        <div class="col-md-6">  
+            <div class="row">
+                <div class="col-md-4">                       
+                    <?php echo form_label('Plenaire sessies:', 'sessies') ?>
+                </div>
+                <div class="col-md-8">   
+                    <?php
+                    foreach ($sessies as $sessie) {
+                        echo form_radio(array('type' => 'radio', 'name' => 'sessies', 'value' => $sessie->id));
+                    }
+                    ?>
+                </div>
+            </div>            
+            <input type="hidden" name="gebruiker" value="<?php echo $user->id ?>"/>
+            <input type="hidden" name="conferentie" value="<?php echo $conferentieId; ?>"/>
+        </div>      
+    </div>
+    <div class="row" style="margin-top: 15px;">
+        <div class="col-md-1"> </div>
+        <div class="col-md-3">
+            <input type="submit" value="Bevestigen" class="btn btn-default"/>
+            <?php echo anchor('home/', 'Annuleer', 'class="btn btn-default"'); ?>
+        </div>
+    </div>    
+    <?php echo form_close(); ?>                                     
+</div>
+
