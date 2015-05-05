ddb450f86ae1bb1ada160757b024cf98fcbf8941
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-05 11:26:49 +0000

    conferentie wijzigen block

--- icclear/application/icclear/views/admin/conferentie/lijst.php
+++ icclear/application/icclear/views/admin/conferentie/lijst.php
@@ -17,12 +17,17 @@
                 <td><?php echo $ond->prijs ?></td>
                 <td><?php echo $ond->korting ?></td>
                 <td>
+                <?php if(count($inschrijvingen) < 1) { ?>
                     <button data-toggle="tooltip" data-placement="bottom" title="Wijzigen" class="wijzigItem glyphicon glyphicon-pencil btn btn-primary" data-id="<?php echo $ond->id ?>"></button>
                     <button data-toggle="tooltip" data-placement="bottom" title="Verwijderen" class="verwijderItem glyphicon glyphicon-trash btn btn-danger" data-id="<?php echo $ond->id ?>"></button>  
+                <?php } ?>
                 </td>
             </tr>
         <?php } ?>
     </tbody>
 </table>
 </div>
-<button class="wijzigItem btn btn-primary" data-id="0"><span class="btn glyphicon glyphicon-plus white"></span> Toevoegen</button>
+
+<?php if(count($inschrijvingen) < 1) { ?>
+    <button class="wijzigItem btn btn-primary" data-id="0"><span class="btn glyphicon glyphicon-plus white"></span> Toevoegen</button>
+<?php } ?>
