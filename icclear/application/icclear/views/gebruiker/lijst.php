81ed75f2053623cd87f9a6578277617e50255bb0
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-05 20:38:30 +0000

    pers dossier

--- icclear/application/icclear/views/gebruiker/lijst.php
+++ icclear/application/icclear/views/gebruiker/lijst.php
@@ -10,7 +10,7 @@
             <?php foreach ($act as $a) { ?>
                 <tr>
                     <td><?php echo $a->naam ?></td>
-                    <td><?php echo $a->aantalPersonen ?> (<span class="italic"><?php echo $a->prijs ?></span>pp)</td>
+                    <td><?php echo $a->aantalPersonen ?> (&euro; <span class="italic"><?php echo $a->prijs ?></span> pp)</td>
                     <td>&euro; <?php echo ($a->prijs * $a->aantalPersonen) ?></td>
                 </tr>
             <?php } ?>
