b7aba97ac75fcc2ccfc3010dba8587e35b2a85a9
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-05 10:38:31 +0000

    footer css

--- icclear/application/icclear/views/main_footer.php
+++ icclear/application/icclear/views/main_footer.php
@@ -1,11 +1,18 @@
-<div class="row">
-    <div class="col-md-4">
-        <h4>HELP</h4>
+<div class="row">    
+    <div class="col-sm-3">        
+    </div>
+    
+    <div class="col-sm-3">
+    </div>
+    
+    <div class="col-sm-3">
+        <h4 class="red">HELP</h4>
         <p><?php echo anchor('faq', 'F.A.Q.'); ?></p>
         <p>User Guide</p>
     </div>
-    <div class="col-md-4">
-        <h4>CONTACT</h4>
+    
+    <div class="col-sm-3">
+        <h4 class="red">CONTACT</h4>
         <p><span class="glyphicon glyphicon-envelope"></span> email: <a href="mailto:support@icclear.test">support@icclear.test</a></p>
         <p><span class="glyphicon glyphicon-phone-alt"></span> tel: 017 55 94 781</p>
     </div>
@@ -14,7 +21,7 @@
 <br/><br/>
 
 <div class="row">
-    <div class="col-md-10 col-md-offset-2">       
+    <div class="col-sm-10 col-md-offset-2">       
         <p>PHP Project - IT Ninjas - Groep 23: Frederik Van Hooghten, Rob Oosthoek, Leslie Milants & Abderrahmane Ikrou - Opdrachtgever: Karine Nickolay</p>        
     </div>
 </div>
