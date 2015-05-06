36f86157f17c9bdefbc5f5d6769bc16b416b94f4
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-06 22:02:05 +0000

    sessie afkeuren

--- icclear/application/icclear/views/admin/sessies/keur_overzicht.php
+++ icclear/application/icclear/views/admin/sessies/keur_overzicht.php
@@ -57,8 +57,8 @@
                                 <td><?php echo $sessie->onderwerp; ?></td>
                                 <td><?php echo $sessie->spreker->familienaam . ' ' . $sessie->spreker->voornaam; ?></td>
                                 <td>
-                                    <a href="<?php echo base_url(); ?>icclear.php/sessies/toonDetails/<?php echo $sessie->id; ?>" data-toggle="modal" data-target="#myModal" class="btn btn-success">Details</a>
-                                    <?php echo anchor('sessies/goedkeuren/' . $sessie->id, 'Goedkeuren','class="btn btn-default"'); ?>                                    
+                                    <a href="<?php echo base_url(); ?>icclear.php/sessies/toonDetails/<?php echo $sessie->id; ?>" data-toggle="modal" data-target="#myModal" class="btn btn-default">Details</a>
+                                    <?php echo anchor('sessies/goedkeuren/' . $sessie->id, 'Goedkeuren','class="btn btn-success"'); ?>                                    
                                     <button class="verwijderVoorstel btn btn-danger" data-id="<?php echo $sessie->id ?>">Afkeuren</button>   
                                 </td>
                             </tr>
