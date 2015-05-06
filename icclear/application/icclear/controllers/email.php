43ea06fe5c228f8e93d16631a548ec8905ed5622
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-06 19:59:22 +0000

    emails

--- icclear/application/icclear/controllers/email.php
+++ icclear/application/icclear/controllers/email.php
@@ -68,7 +68,7 @@ class Email extends CI_Controller {
     public function verzenden() {               
         $onderwerp = $this->input->post('onderwerp');
         $inhoud = $this->input->post('boodschap');        
-        $ontvanger = $this->input->post('emailadresdan');
+        $ontvanger = $this->input->post('emailadreszend');
         
         $this->email->from('donotreply@thomasmore.be');
         $this->email->to($ontvanger);
