08b10bea24c42d8648e24bf8588ff1cfc05e1b89
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-06 14:16:38 +0000

    check

--- icclear/application/icclear/views/logon/user_bestaat.php
+++ icclear/application/icclear/views/logon/user_bestaat.php
@@ -1,6 +1,6 @@
-
-<h4><?php echo $title; ?></h4>
-
-<p>Email bestaat reeds. Probeer opnieuw!</p>
-
-<?php echo anchor('logon/register', 'Terug'); ?>
+    <div class="space-bottom alert alert-danger">        
+        <h1 class="space-bottom15 text-center">Oops! Er is een fout opgetreden.</h1>
+        <p class="space-bottom danger italic text-center">Email bestaat reeds. Probeer opnieuw!</p>      
+        
+        <p><a href="javascript:history.go(-1)" class="col-xs-offset-4 col-xs-4 btn btn-default">Terug</a></p>
+    </div>
