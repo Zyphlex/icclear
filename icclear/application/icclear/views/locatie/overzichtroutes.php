9c9972df49cc65970f897f8320f340406da9eb28
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-06 15:15:43 +0000

    home

--- icclear/application/icclear/views/locatie/overzichtroutes.php
+++ icclear/application/icclear/views/locatie/overzichtroutes.php
@@ -5,7 +5,10 @@
     <p><?php echo $route->beschrijving ?></p>
     </div>
     <div class="col-md-5">
-            <?php echo $route->url ?>
+            <iframe 
+                src="<?php echo $route->url ?>"
+                width="600" height="450" frameborder="0" style="border:0">
+            </iframe>
     </div>
     <hr/>
 </div>
