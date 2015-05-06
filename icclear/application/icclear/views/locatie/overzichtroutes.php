590f29af0de2c93692aeca7260340cf5faf5dbb6
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-06 15:17:42 +0000

    home

--- icclear/application/icclear/views/locatie/overzichtroutes.php
+++ icclear/application/icclear/views/locatie/overzichtroutes.php
@@ -1,13 +1,13 @@
 <?php foreach ($routes as $route) { ?>
 <div class="row">
-    <div class="col-md-7">
+    <div class="col-sm-12">
     <h3><?php echo $route->vertrekPunt ?></h3>
     <p><?php echo $route->beschrijving ?></p>
     </div>
-    <div class="col-md-5">
-            <iframe 
+    <div class="col-sm-12">
+            <iframe class="img-responsive"
                 src="<?php echo $route->url ?>"
-                width="600" height="450" frameborder="0" style="border:0">
+                frameborder="0" style="border:0">
             </iframe>
     </div>
     <hr/>
