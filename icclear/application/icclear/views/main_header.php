be0d04e37815f37bdf886d98b350cc950966ac85
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-06 12:44:22 +0000

    check

--- icclear/application/icclear/views/main_header.php
+++ icclear/application/icclear/views/main_header.php
@@ -177,6 +177,7 @@
                                     $("#emaildiv").addClass("has-success");
                                     alert('ALLEZ NU MOET HET NORMAAL TRUE ZIJN GODVERDOMME');
                                     result = true;
+                                    alert(result);
                                     //die alert wordt wel geactiveerd maar die result wilt nooit op true gaan
                                 }
                             });
