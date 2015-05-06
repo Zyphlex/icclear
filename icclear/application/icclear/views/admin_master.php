6696922fb429ca3088376b4a697c8ab6f63a3fd6
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-06 12:01:53 +0000

    no chart, new stats

--- icclear/application/icclear/views/admin_master.php
+++ icclear/application/icclear/views/admin_master.php
@@ -29,8 +29,7 @@
     <script src="<?php echo base_url() . APPPATH; ?>js/jquery-1.11.2.min.js"></script>  
     <script src="<?php echo base_url() . APPPATH; ?>js/bootstrap.js"></script>
     <script src="<?php echo base_url() . APPPATH; ?>js/jquery.dataTables.js"></script>    
-    <script src="<?php echo base_url() . APPPATH; ?>js/dataTables.bootstrap.js"></script>   
-    <script src="<?php echo base_url() . APPPATH; ?>js/highcharts.js"></script>
+    <script src="<?php echo base_url() . APPPATH; ?>js/dataTables.bootstrap.js"></script>  
     <script src="<?php echo base_url() . APPPATH; ?>js/script.js"></script>
     
     <script type="text/javascript">
