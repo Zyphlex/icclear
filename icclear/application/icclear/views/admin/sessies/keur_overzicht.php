0e1eeb569113a14f87dd61bd86917d9327fecc1e
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-06 21:16:17 +0000

    sessie afkeuren

--- icclear/application/icclear/views/admin/sessies/keur_overzicht.php
+++ icclear/application/icclear/views/admin/sessies/keur_overzicht.php
@@ -30,9 +30,35 @@
     <?php echo anchor('sessies', 'Terug naar sessies','class="btn btn-default"'); ?>                           
     
     
+    <button data-toggle="tooltip" data-placement="bottom" title="Verwijderen" class="verwijderLand glyphicon glyphicon-trash btn btn-danger" data-id="<?php echo $land->id ?>"></button>                                 
+    
 </div>
 
 <!--      Modal voor details       -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog"><div class="modal-content"></div></div>
 </div>
+
+<!-- MODAL VOOR VERWIJDEREN -->  
+<div class="modal fade" id="landDelete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">OPGELET!</h4>
+            </div>
+
+            <div class="modal-body">                  
+                <p>Bent u zeker dat u dit land wilt verwijderen?</p>  
+                <p class="italic">Dit kan niet ongedaan gemaakt worden!</p>                  
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="deleteLand btn btn-primary">Bevestig</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Annuleren</button>
+            </div>
+
+        </div>            
+    </div>
+</div>  
