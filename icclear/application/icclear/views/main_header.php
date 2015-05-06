162c99f08d8a07fcfc957017b9b9c4a3b89e93e5
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-06 14:57:40 +0000

    home

--- icclear/application/icclear/views/main_header.php
+++ icclear/application/icclear/views/main_header.php
@@ -1,7 +1,9 @@
 <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-8">                         
+        <a href="<?php echo base_url();?>icclear.php/home">
         <img id="ic-logo" src="<?php echo base_url() . APPPATH; ?>img/default/icclear.png" />
-    <span class="header-conf"><h1 class="red bold">Conferentie <?php echo $conferentie->naam ?></h1></span>
+        <span class="header-conf"><h1 class="red bold">Conferentie <?php echo $conferentie->naam ?></h1></span>
+        </a>
     </div>
 
     <?php if ($user == null) { // niet aangemeld ?>  
