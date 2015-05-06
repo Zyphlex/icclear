3c587ab845192be5ac10f6f13a87d25fb66c8616
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-06 13:58:31 +0000

    check

--- icclear/application/icclear/views/logon/user_klaar.php
+++ icclear/application/icclear/views/logon/user_klaar.php
@@ -1,9 +1,3 @@
-
-
-<h4><?php echo $title; ?></h4>
-
-<p></p>
-
     <div class="space-bottom alert alert-success">        
         <h1 class="space-bottom15 text-center">Gelukt! Uw account is aangemaakt!</h1>
         <p class="space-bottom success italic text-center">Er werd een email verstuurd met een activatielink.
