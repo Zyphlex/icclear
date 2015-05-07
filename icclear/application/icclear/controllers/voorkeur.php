7f7f462f0df9a4ee09bdadffd400219ca09f2d7e
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-07 13:20:00 +0000

    sessie/voorkeuren

--- icclear/application/icclear/controllers/voorkeur.php
+++ icclear/application/icclear/controllers/voorkeur.php
@@ -12,121 +12,21 @@ class Voorkeur extends CI_Controller {
             redirect('logon/aanmelden');
         }
     }
-
-    public function index() {
-
+   
+    public function insert() {
         $user = $this->authex->getUserInfo();
-        $data['user'] = $user;
-        $data['conferentieId'] = $this->session->userdata('conferentieId');
-        //Kijken of user reeds is ingeschreven, als dit zo is, knop verbergen op view
-        $this->load->model('inschrijving_model');
-        if ($user == null) {
-            $data['inschrijving'] = null;
-        } else {
-            $inschrijving = $this->inschrijving_model->IsGebruikerIngeschreven($user->id);
-            if ($inschrijving == null) {
-                $data['inschrijving'] = null;
-            } else {
-                $data['inschrijving'] = $inschrijving;
-            }
+        
+        $gebruikerId = $user->id;
+        $ids = $this->input->post('sessie');         
+        $this->load->model('sessies_model');
+        
+        foreach ($ids as $i){
+            $this->sessies_model->insert($gebruikerId, $i);
         }
-
-
-        $data['title'] = 'IC Clear - Sessie voorkeuren';
-        $data['active'] = 'sessie voorkeuren';
-
-        $this->load->model('gebruiker_model');
-        $data['gebruiker'] = $this->gebruiker_model->get($user->id);
-
-        $this->load->model('land_model');
-        $data['landen'] = $this->land_model->getAll();
-
-        $this->load->model('conferentie_model');
-        $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
-
-        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'voorkeur/overzicht', 'footer' => 'main_footer');
-        $this->template->load('main_master', $partials, $data);
+                                                       
+        redirect('home');
     }
-
-    public function update() {
-        $gebruiker = new stdClass();
-
-        $gebruiker->id = $this->input->post('id');
-        $gebruiker->voornaam = $this->input->post('voornaam');
-        $gebruiker->familienaam = $this->input->post('familienaam');
-        $gebruiker->geboortedatum = $this->input->post('geboortedatum');
-//        $gebruiker->emailadres = $this->input->post('emailadres');
-        $gebruiker->geslacht = $this->input->post('geslacht');
-//        $gebruiker->typeId = $this->input->post('type');
-        $gebruiker->landId = $this->input->post('land');
-        $gebruiker->gemeente = $this->input->post('gemeente');
-        $gebruiker->postcode = $this->input->post('postcode');
-        $gebruiker->straat = $this->input->post('straat');
-        $gebruiker->nummer = $this->input->post('huisnummer');
-
-        $this->load->model('gebruiker_model');
-
-        $this->gebruiker_model->update($gebruiker);
-
-        redirect('profiel/instellingen');
-    }
-
-    public function instellingen() {
-        $user = $this->authex->getUserInfo();
-        $data['user'] = $user;
-
-        $data['title'] = 'IC Clear - Profielinstellingen';
-        $data['active'] = '';
-
-        $this->load->model('gebruiker_model');
-        $data['gebruiker'] = $this->gebruiker_model->get($user->id);
-
-        $this->load->model('land_model');
-        $data['landen'] = $this->land_model->getAll();
-
-        //Kijken of user reeds is ingeschreven, als dit zo is, knop verbergen op view
-        $this->load->model('inschrijving_model');
-        if ($user == null) {
-            $data['inschrijving'] = null;
-        } else {
-            $inschrijving = $this->inschrijving_model->IsGebruikerIngeschreven($user->id);
-            if ($inschrijving == null) {
-                $data['inschrijving'] = null;
-            } else {
-                $data['inschrijving'] = $inschrijving;
-            }
-        }
-
-        $this->load->model('conferentie_model');
-        $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
-
-        $this->load->model('gebruiker_activiteit_model');
-        foreach ($data['inschrijving'] as $i) {
-            $confId = $i->confonderdeel->conferentieId;
-            $diff = (abs(strtotime($i->conferentie->beginDatum) - strtotime($i->datum))) / 86400;
-            if ($diff >= 30) {
-                $confprijs = $i->confonderdeel->prijs - (($i->confonderdeel->prijs / 100) * $i->confonderdeel->korting);
-            } else {
-                $confprijs = $i->confonderdeel->prijs;
-            }
-
-            $i->geld += $this->gebruiker_activiteit_model->getPrijsByConfGebruiker($user->id, $confId) + $confprijs;
-        }
-
-
-        $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'gebruiker/wijzigen', 'footer' => 'main_footer');
-        $this->template->load('main_master', $partials, $data);
-    }
-
-    public function detail() {
-        $id = $this->input->get('id');
-
-        $this->load->model('inschrijving_model');
-        $inschrijving = $this->inschrijving_model->getOnderdeelByInschrijving($id);
-
-        echo json_encode($inschrijving);
-    }
-
+    
 }
 
  
