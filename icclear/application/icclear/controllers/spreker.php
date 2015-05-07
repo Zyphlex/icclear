1c34c5dfdfe18f029bc2c1e10a5cee743b2ea6ff
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-07 10:01:01 +0000

    lijst met sprekers en sessie

--- icclear/application/icclear/controllers/spreker.php
+++ icclear/application/icclear/controllers/spreker.php
@@ -47,6 +47,19 @@ class Spreker extends CI_Controller {
         $data['title'] = 'IC Clear - Sprekers';
         $data['active'] = 'admin';
         
+        //Kijken of user reeds is ingeschreven, als dit zo is, knop verbergen op view
+        $this->load->model('inschrijving_model');
+        if ($user == null) {
+            $data['inschrijving'] = null;
+        } else {
+            $inschrijving = $this->inschrijving_model->IsGebruikerIngeschreven($user->id);
+            if ($inschrijving == null) {
+                $data['inschrijving'] = null;
+            } else {
+                $data['inschrijving'] = $inschrijving;
+            }
+        }
+        
         $this->load->model('planning_model');
         $data['programma'] = $this->planning_model->getOverzichtActieve();
         
