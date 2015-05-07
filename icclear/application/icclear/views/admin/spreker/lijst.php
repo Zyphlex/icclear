dbd683890a3c57bf9e8422a1dadf9e05fbb0c5a9
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-07 09:36:54 +0000

    lijst met sprekers en sessie

---
+++ icclear/application/icclear/views/admin/spreker/lijst.php
@@ -0,0 +1,27 @@
+<div class="table-responsive">
+    <table class="table table-beheer">
+        <thead>
+            <tr>                                
+                <th>Naam</th>
+                <th>Sessie</th>
+                <th>Beheer</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($programma as $prog) {
+                foreach ($prog as $p) {
+                    ?>
+
+                    <tr>
+                        <td><?php echo $p->sessie->spreker->voornaam . ' ' . $p->sessie->spreker->familienaam ?></td>
+                        <td><?php echo $p->sessie->onderwerp ?></td>
+                        <td>
+                            <button data-toggle="tooltip" data-placement="bottom" title="Wijzigen" class="wijzigSpreker glyphicon glyphicon-pencil btn btn-primary" data-id="<?php echo $p->sessie->spreker->id ?>"></button>
+                            <button data-toggle="tooltip" data-placement="bottom" title="Verwijderen" class="verwijderLand glyphicon glyphicon-trash btn btn-danger" data-id="<?php echo $p->sessie->id ?>"></button>                                 
+                        </td>
+                    </tr>
+                <?php } ?>
+<?php } ?>
+        </tbody>
+    </table>
+</div>
