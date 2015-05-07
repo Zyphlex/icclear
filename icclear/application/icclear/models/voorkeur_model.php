fb9b04d2a5c009d877fb05a727205a0c9e360dda
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-07 15:14:39 +0000

    sessie/voorkeuren

---
+++ icclear/application/icclear/models/voorkeur_model.php
@@ -0,0 +1,20 @@
+<?php
+
+class Sessies_model extends CI_Model {
+
+    function __construct() {
+        parent::__construct();
+    }
+            
+    function insert($voorkeur)
+    {
+        //Html entities en extra spaties verwijderen
+        $voorkeur = escape_html($voorkeur);        
+        $this->db->insert('voorkeur', $voorkeur);
+        return $this->db->insert_id();
+    }
+        
+    
+    
+}
+?>
