7d7c379dc65d02538e4b5f43494b549229fa7ffa
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-07 23:25:02 +0000

    sessie/voorkeuren

--- icclear/application/icclear/views/inschrijving/voorkeuren.php
+++ icclear/application/icclear/views/inschrijving/voorkeuren.php
@@ -9,27 +9,48 @@
             <div class="col-md-12">  
                 <div class="row">                
                     <div class="col-md-12"> 
-                        <table class="table">
-                            <tr>
-                                <th></th>
-                                <th>Tijdstip</th>
-                                <th>Sessie</th>                            
-                                <th>Spreker</th>
-                            </tr>                            
-                            <?php foreach ($dagen->planning as $p) { ?>
-                                <tr>
-                                    <td><?php echo form_checkbox(array('name' => 'gekozensessies[]', 'value' => $p->sessie->id)); ?></td>
-                                    <td>
-                                        <span class="label label-warning">
-                                            <span aria-hidden="true" class="glyphicon glyphicon-time"></span> <?php echo $p->beginUur . ' - ' . $p->eindUur ?>
-                                        </span>
-                                    </td>
-                                    <td><a href="<?php echo base_url(); ?>icclear.php/sessies/toonDetails/<?php echo $p->sessie->id; ?>" data-toggle="modal" data-target="#myModal" title="Details" class="glyphicon glyphicon-info-sign link-icon"></a><?php echo $s->onderwerp; ?></td>                                 
-                                    <td><?php echo $p->sessie->spreker->voornaam . " " . $p->sessie->spreker->familienaam; ?></td>
-                                </tr>
-                            <?php }  ?>
-                                
-                        </table>
+                        <?php
+                        $teller = 0;
+                        foreach ($programma as $d) {
+                            $teller++;
+                            ?>
+                            <h4>Conferentiedag <?php echo $teller ?> <span class="italic">(<?php echo toDDMMYYYY($d->datum); ?>)</span></h4>
+                            <div class="table-responsive space-bottom">
+                                <table class = "table-hover table-condensed table">
+                                    <thead>
+                                        <tr>
+                                            <th class="w15">Tijdstip</th>
+                                            <th class="w65">Onderwerp</th>                                                                                                                
+                                            <th class="w20">Spreker</th>
+                                        </tr>
+                                    </thead>  
+                                    <tbody>                        
+    <?php foreach ($d->programma as $p) { ?>                             
+                                            <tr class="under-link">
+                                                <td>
+                                                    <p>
+                                                        <span class="label label-warning">
+                                                            <span aria-hidden="true" class="glyphicon glyphicon-time"></span> <?php echo $p->beginUur . ' - ' . $p->eindUur ?>
+                                                        </span>
+                                                    </p>
+                                                </td> 
+                                                <td>
+                                                    <a href="" data-toggle="modal" class="toonItem" data-id="<?php echo $p->sessieId ?>">
+                                                        <span class="glyphicon glyphicon-info-sign link-icon"></span> <?php echo $p->sessie->onderwerp ?>
+                                                    </a>
+                                                </td>                                    
+                                                <td>
+                                                    <a href="" data-toggle="modal" class="toonSpreker" data-id="<?php echo $p->sessie->gebruikerIdSpreker ?>"> 
+                                                        <span class="glyphicon glyphicon-user link-icon"></span> <?php echo $p->sessie->spreker->voornaam . ' ' . $p->sessie->spreker->familienaam ?>
+                                                    </a>
+                                                </td>
+                                            </tr> 
+    <?php } ?>
+                                    </tbody>
+                                </table>   
+                            </div>
+<?php } ?>
+
                     </div>
                 </div>            
                 <input type="hidden" name="gebruiker" value="<?php echo $user->id ?>"/>
@@ -40,10 +61,10 @@
             <div class="col-md-1"> </div>
             <div class="col-md-3">
                 <input type="submit" value="Bevestigen" class="btn btn-default"/>
-                <?php echo anchor('home/', 'Annuleer', 'class="btn btn-default"'); ?>
+        <?php echo anchor('home/', 'Annuleer', 'class="btn btn-default"'); ?>
             </div>
         </div>    
-        <?php echo form_close(); ?>                                     
+<?php echo form_close(); ?>                                     
     </div>
 </div>
 
