ebc347db0abf3e867ed5c2bdee15c0a2a3d09e3f
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-07 13:46:55 +0000

    inschrijving toont details

--- icclear/application/icclear/views/admin/inschrijving/lijst.php
+++ icclear/application/icclear/views/admin/inschrijving/lijst.php
@@ -2,11 +2,11 @@
 <table class="table table-beheer">
     <thead>
         <tr>
-            <th class="w35">Naam</th>
+            <th class="w30">Naam</th>
             <th class="w25">Te betalen</th>
             <th class="w15">Datum</th>
             <th class="w15">Betaling</th>
-            <th class="w17">Beheer</th>
+            <th class="w18">Beheer</th>
         </tr>
     </thead>
     <tbody>
