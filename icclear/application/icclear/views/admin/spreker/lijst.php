53a77af74ed8bdf44c411f61ac8b591c60ec4c78
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-07 12:30:13 +0000

    beheer weg van sprekers

--- icclear/application/icclear/views/admin/spreker/lijst.php
+++ icclear/application/icclear/views/admin/spreker/lijst.php
@@ -4,7 +4,6 @@
             <tr>                                
                 <th>Naam</th>
                 <th>Sessie</th>
-                <th>Beheer</th>
             </tr>
         </thead>
         <tbody>
@@ -15,10 +14,6 @@
                     <tr>
                         <td><?php echo $p->sessie->spreker->voornaam . ' ' . $p->sessie->spreker->familienaam ?></td>
                         <td><?php echo $p->sessie->onderwerp ?></td>
-                        <td>
-                            <button data-toggle="tooltip" data-placement="bottom" title="Wijzigen" class="wijzigSpreker glyphicon glyphicon-pencil btn btn-primary" data-id="<?php echo $p->sessie->spreker->id ?>"></button>
-                            <button data-toggle="tooltip" data-placement="bottom" title="Verwijderen" class="verwijderSpreker glyphicon glyphicon-trash btn btn-danger" data-id="<?php echo $p->sessie->id ?>"></button>                                 
-                        </td>
                     </tr>
                 <?php } ?>
 <?php } ?>
