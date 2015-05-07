88092999f20bb421c03e89a9ba3c612100048f56
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-07 20:50:51 +0000

    inschrijven annuleren fix

--- icclear/application/icclear/views/inschrijving/inschrijving.php
+++ icclear/application/icclear/views/inschrijving/inschrijving.php
@@ -135,11 +135,11 @@ $attributes = array('name' => 'myform');
         <div id="overschr"></div>
 
         <div class="btn-group">
-            <a href="<?php echo base_url(); ?>icclear.php/dashboard/"<?php $conferentie->id ?> class="btn btn-default">Annuleren</a>
+            <a href="<?php echo base_url(); ?>icclear.php/home/" class="btn btn-default">Annuleren</a>
             <?php if ($user == null) { ?>
-                <?php echo form_submit('mysubmit', 'Aanmelden en betalen', 'class="btn btn-primary"'); ?>
+                <?php echo form_submit('mysubmit', 'Aanmelden en betalen', 'class="col-xs-3 btn btn-primary"'); ?>
             <?php } else { ?>
-                <?php echo form_submit('mysubmit', 'Inschrijven en betalen', 'class="btn btn-primary"'); ?>
+                <?php echo form_submit('mysubmit', 'Inschrijven en betalen', 'class="col-xs-6 btn btn-primary"'); ?>
             <?php } ?>
         </div>
     </div>
