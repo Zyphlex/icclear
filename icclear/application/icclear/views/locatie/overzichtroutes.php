862cc7a0a0d0726ba10ca7f4af25885e985a9abd
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 17:00:49 +0000

    routes

--- icclear/application/icclear/views/locatie/overzichtroutes.php
+++ icclear/application/icclear/views/locatie/overzichtroutes.php
@@ -4,7 +4,7 @@
         <h3><?php echo $route->vertrekPunt ?></h3>
         <p><?php echo $route->beschrijving ?></p>
     </div>
-    <div class="col-sm-12">
+    <div class="google-maps space-bottom15 col-sm-12">
             <iframe class="img-responsive"
                 src="<?php echo $route->url ?>"
                 width="100%" height="100%" frameborder="0" style="border:0">
