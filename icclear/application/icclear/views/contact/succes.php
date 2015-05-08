31d35c3215154f045ae69d87b840d3eb98fd3e2c
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 14:52:05 +0000

    CONTACT pagina maken

---
+++ icclear/application/icclear/views/contact/succes.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-md-12">
+        <h1 style="text-align: center;">Succesvol verzonden</h1>
+    </div>
+</div>
+
+<div class="row">  
+    <p>Uw vraag/boodschap is succesvol verzonden!<p>
+    <p>Wanneer we uw vraag/boodschap verwerkt hebben zal u een email ontvangen in uw mailbox.</p>
+</div>
+
+
+
+
