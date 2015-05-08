a9906a2117b00c4ba5ea4c35e0e1f4b89a2ad1eb
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 17:41:23 +0000

    Commentaarlijnen

--- icclear/application/icclear/models/type_model.php
+++ icclear/application/icclear/models/type_model.php
@@ -6,12 +6,14 @@ class Type_model extends CI_Model {
         parent::__construct();
     }
 
+    // Een type ophalen
     function get($id) {
         $this->db->where('id', $id);
         $query = $this->db->get('type');
         return $query->row();
     }
 
+    // Alle types ophalen
     function getAll() {
         $query = $this->db->get('type');
         return $query->result();
