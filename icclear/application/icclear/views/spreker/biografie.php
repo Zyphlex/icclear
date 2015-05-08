e6a772ee6a6d766f5156ddc7cbec29704898428b
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 18:25:44 +0000

    biografie

--- icclear/application/icclear/views/spreker/biografie.php
+++ icclear/application/icclear/views/spreker/biografie.php
@@ -1,11 +1,11 @@
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-sm-12">
         <h1><?php echo $spreker->familienaam . ' ' . $spreker->voornaam ?> - Biografie en foto</h1>
     </div>
 </div>    
 
 <div class="row">
-    <div class="col-md-8">
+    <div class="col-sm-8">
         <p>
             Van alle sprekers voor onze conferenties wordt verwacht dat ze een profielfoto uploaden. Ook een biografie
             is van elke spreker gewenst.
@@ -19,26 +19,26 @@ $attributes = array('name' => 'myform', 'enctype' => 'multipart/form-data');
 echo form_open('spreker/updateBiografie', $attributes);
 ?>
 <div class="row">
-    <div class="col-md-2">
+    <div class="col-sm-2">
         <label for="userfile" class="control-label">Foto: </label>
     </div>
-    <div class="col-md-2">
+    <div class="col-sm-2">
         <input id="userfile" type="file" name="userfile" class=""/>
     </div>
 </div>
-<div class="row col-md-8">
+<div class="row col-sm-8">
     Enkel JPG afbeeldingen toegestaan. Maximum 250x250 pixels.
 </div>
 
 <br/>
 
 <div class="row">
-    <div class="col-md-2">
+    <div class="col-sm-2">
         <label for="biografie" class="control-label">Biografie: </label>
     </div>
-    <div class="col-md-8">
+    <div class="col-sm-8">
         <textarea required id="biografie" name="biografie" rows="10" class="form-control"></textarea>
     </div>
 </div>
-<input type="submit" value="Bevestigen" class="btn btn-default"/>
+<input type="submit" value="Bevestigen" class="col-sm-offset-2 col-sm-4 btn btn-primary"/>
 </form>
