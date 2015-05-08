8ab7c58ee77a904f362c3a21a2c46932461b9c60
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 15:10:40 +0000

    CONTACT pagina maken

--- icclear/application/icclear/views/contact/start.php
+++ icclear/application/icclear/views/contact/start.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-md-12">
-        <h1 style="text-align: center;">Contact</h1>
+        <h1 style="text-align: center;">Neem contact op met ons</h1>
     </div>
 </div>
 
