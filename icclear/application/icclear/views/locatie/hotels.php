552283f8cfa59ed26540fe064fdbb6e53349083d
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 15:59:20 +0000

    hotels

--- icclear/application/icclear/views/locatie/hotels.php
+++ icclear/application/icclear/views/locatie/hotels.php
@@ -10,7 +10,7 @@
             <div class="panel panel-default">
                 <div class="row">
                     <div class="panel-body">
-                        <div class="col-md-3">
+                        <div class="col-md-5">
                             <?php if ($hotel->hotel->foto == 'hotel' . $hotel->hotel->id . '.jpg') { ?>
                                 <img class="img-responsive center-block" src="<?php echo base_url() . 'application/upload/fotos/hotels/' . $hotel->hotel->foto; ?>" 
                                  alt="Foto <?php echo $hotel->hotel->naam; ?>" 
@@ -21,12 +21,12 @@
                                  alt="Foto niet beschikbaar" title="Foto niet beschikbaar" data-placement="bottom">
                             <?php } ?>
                         </div>
-                        <div class="col-md-9">
-                            <h4><?php echo $hotel->hotel->naam ?></h4>        
+                        <div class="col-md-7">
+                            <h3><?php echo $hotel->hotel->naam ?></h3>        
 
                             <p><?php echo $hotel->hotel->straat . ' ' . $hotel->hotel->nummer ?></p>   
                             <p><?php echo $hotel->hotel->postcode . ' ' . $hotel->hotel->gemeente ?></p>   
-                            <p><?php echo anchor($hotel->hotel->website, 'Naar de website', array('target' => '_blank')) ?></p>  
+                            <p class="italic"><?php echo anchor($hotel->hotel->website, 'Naar de website', array('target' => '_blank')) ?></p>  
                         </div>
                     </div>      
                 </div> 
