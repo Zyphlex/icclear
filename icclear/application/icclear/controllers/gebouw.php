126bb401af75342d7ac3f0862d27e602506ddcb1
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 11:25:16 +0000

    gebouw upload test

--- icclear/application/icclear/controllers/gebouw.php
+++ icclear/application/icclear/controllers/gebouw.php
@@ -83,7 +83,7 @@ class Gebouw extends CI_Controller {
         
         // foto
         $config['upload_path'] = './application/upload/fotos/gebouwen';
-        $config['allowed_types'] = 'jpg';
+        $config['allowed_types'] = 'png';
         $config['file_name'] = 'gebouw' . $gebouw->id . '.jpg';
         $config['max_size'] = 200;
         $config['max_height'] = 700;
