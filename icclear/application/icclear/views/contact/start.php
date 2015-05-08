29e7178509f24c29823577732591723e0d9fec93
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 14:54:24 +0000

    CONTACT pagina maken

--- icclear/application/icclear/views/contact/start.php
+++ icclear/application/icclear/views/contact/start.php
@@ -8,8 +8,11 @@
     <form action="contact/insturen" method="post">        
         <p><?php echo form_label('Uw emailadres:', 'emailadresverzender'); ?></p>
         <p><?php echo form_input(array('name' => 'emailadresverzender', 'id' => 'emailadresverzender', 'class' => 'form-control')); ?></p>
-
-        <p><?php echo form_label('Boodschap:', 'boodschapcontact'); ?></p>
+        
+        <p><?php echo form_label('Onderwerp:', 'onderwerpverzender'); ?></p>
+        <p><?php echo form_input(array('name' => 'onderwerpverzender', 'id' => 'onderwerpverzender', 'class' => 'form-control')); ?></p>
+        
+        <p><?php echo form_label('Boodschap/vraag:', 'boodschapcontact'); ?></p>
         <p><?php echo form_textarea(array('name' => 'boodschapcontact', 'id' => 'boodschapcontact', 'rows' => '10', 'cols' => '50', 'class' => 'form-control')); ?></p>
         <p><input type="submit" value="Verzenden"/></p>
     </form>
