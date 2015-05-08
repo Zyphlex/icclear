02827ad85c4aaeba6cd67681785a101a1048c2ca
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 11:29:09 +0000

    -> Geregistreerde gebruikerdid in sessievariabele

--- icclear/application/icclear/views/contact/start.php
+++ icclear/application/icclear/views/contact/start.php
@@ -5,7 +5,14 @@
 </div>
 
 <div class="row">  
-    
+    <form id="JqAjaxForm">        
+        <p><?php echo form_label('Uw emailadres:', 'emailadresverzender'); ?></p>
+        <p><?php echo form_input(array('name' => 'emailadresverzender', 'id' => 'emailadresverzender', 'class' => 'form-control')); ?></p>
+
+        <p><?php echo form_label('Boodschap:', 'boodschapcontact'); ?></p>
+        <p><?php echo form_textarea(array('name' => 'boodschapcontact', 'id' => 'boodschapcontact', 'rows' => '10', 'cols' => '50', 'class' => 'form-control')); ?></p>
+
+    </form>
 </div>
 
 
