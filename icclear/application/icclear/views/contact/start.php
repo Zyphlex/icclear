d613a32a05edcb7531be20044836381221b8cf9a
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 16:07:11 +0000

    contact

--- icclear/application/icclear/views/contact/start.php
+++ icclear/application/icclear/views/contact/start.php
@@ -1,38 +1,46 @@
 <div class="row">
-    <div class="col-md-8">
-        <h1>Contactpagina</h1>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-8">
-        <h3>Adres:</h3>
-    </div>
-    <div class="col-md-8">
-        <p>ICClear Brussel</p>
-        <p>014/404040</p>
-        <p>Nieuwstraat 1</p>
-        <p>1000 Brussel</p>
-    </div>
-</div>
+    <div class="col-sm-6">
+        <div class="col-md-8">
+            <h1>Contactpagina</h1>
+        </div>
 
-<div class="row space-top15">
-    <div class="col-md-8">
-        <h1>Neem contact op met ons</h1>
+
+        <div class="col-md-8">
+            <h3>Adres:</h3>
+        </div>
+        <div class="col-md-8">
+            <p>ICClear Brussel</p>
+            <p>014/404040</p>
+            <p>Nieuwstraat 1</p>
+            <p>1000 Brussel</p>
+        </div>
+
+
+        <div class="col-md-8">
+            <h1>Neem contact op met ons</h1>
+        </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-md-8">
-        <form action="contact/insturen" method="post">        
-            <p><?php echo form_label('Uw emailadres:', 'emailadresverzender'); ?></p>
-            <p><?php echo form_input(array('name' => 'emailadresverzender', 'id' => 'emailadresverzender', 'class' => 'form-control', 'required' => 'required')); ?></p>
 
-            <p><?php echo form_label('Onderwerp:', 'onderwerpverzender'); ?></p>
-            <p><?php echo form_input(array('name' => 'onderwerpverzender', 'id' => 'onderwerpverzender', 'class' => 'form-control', 'required' => 'required')); ?></p>
+    <div class="col-sm-6">
 
-            <p><?php echo form_label('Boodschap/vraag:', 'boodschapcontact'); ?></p>
-            <p><?php echo form_textarea(array('name' => 'boodschapcontact', 'id' => 'boodschapcontact', 'rows' => '10', 'cols' => '50', 'class' => 'form-control', 'required' => 'required')); ?></p>
-            <p><input type="submit" value="Verzenden" class="btn btn-default"/></p>
-        </form>
+
+        <div class="col-md-8">            
+            <?php
+            $attributes = array('name' => 'myform');
+            echo form_open('contact/insturen', $attributes);
+            ?>    
+                <p class="col-sm-4"><?php echo form_label('Uw emailadres:', 'emailadresverzender'); ?></p>
+                <p class="col-sm-8"><?php echo form_input(array('name' => 'emailadresverzender', 'id' => 'emailadresverzender', 'class' => 'form-control', 'required' => 'required')); ?></p>
+
+                <p><?php echo form_label('Onderwerp:', 'onderwerpverzender'); ?></p>
+                <p><?php echo form_input(array('name' => 'onderwerpverzender', 'id' => 'onderwerpverzender', 'class' => 'form-control', 'required' => 'required')); ?></p>
+
+                <p><?php echo form_label('Boodschap/vraag:', 'boodschapcontact'); ?></p>
+                <p><?php echo form_textarea(array('name' => 'boodschapcontact', 'id' => 'boodschapcontact', 'rows' => '10', 'cols' => '50', 'class' => 'form-control', 'required' => 'required')); ?></p>
+                <p><input type="submit" value="Verzenden" class="btn btn-default"/></p>    
+                <?php echo form_submit('mysubmit', 'Bericht versturen', 'class="col-xs-8 btn btn-primary"'); ?>        
+            <?php echo form_close(); ?>
+        </div>
     </div>
 </div>
 
