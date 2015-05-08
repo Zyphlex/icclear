88d6b72c529f268a03816412a44ce257534321b9
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 18:58:43 +0000

    header val

--- icclear/application/icclear/views/main_header.php
+++ icclear/application/icclear/views/main_header.php
@@ -284,11 +284,11 @@
                             if (result == '0') {
                                 $('#feedbackemail').html("<p class='form-note form-note-used'>Niet beschikbaar</p>");
                                 $("#emaildiv").addClass("has-error");
-                                $("#emaildiv").removeClass("has-success");
+                                $("#msgReg").removeClass("hidden");
+                                $("#msgReg").html("Oops! Dit email adres is reeds in gebruik!");
                             } else {
                                 $('#feedbackemail').html("<p class='form-note form-note-ok'>Beschikbaar</p>");
                                 $("#emaildiv").removeClass("has-error");
-                                $("#emaildiv").addClass("has-success");
                                 ok = true;
                             }
                         }
