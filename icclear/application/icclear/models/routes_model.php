60ea73ce91555258c70c6ba4c1bd41867b09eb00
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 17:47:57 +0000

    Commentaarlijnen

--- icclear/application/icclear/models/routes_model.php
+++ icclear/application/icclear/models/routes_model.php
@@ -18,6 +18,7 @@ class Routes_model extends CI_Model {
         parent::__construct();
     }
       
+    // Alle routes met bijbehorende gebouwen ophalen
     function getRoutes()
     {        
         $query = $this->db->get('route');
@@ -31,6 +32,7 @@ class Routes_model extends CI_Model {
         return $routes;
     }
     
+    // Een route met bijbehorend gebouw ophalen
     function getRoute($id)
     {        
         $this->db->where('id', $id);
@@ -43,6 +45,7 @@ class Routes_model extends CI_Model {
         return $route;
     }
     
+    // Alle routes ophalen van de actieve conferentie, van elke conferentiedag (want 1 gebouw per dag)
     function getRoutesConferentie()
     {
         $this->load->model('conferentie_model');        
@@ -59,6 +62,7 @@ class Routes_model extends CI_Model {
         return $routes;
     }    
     
+    // Alle routes ophalen die bij een gebouw horen, samen met de info van dit gebouw
     function getRoutesGebouw($gebouwId) {              
         $this->db->where('gebouwId', $gebouwId); 
         $query = $this->db->get('route');   
@@ -72,6 +76,7 @@ class Routes_model extends CI_Model {
         return $routes;
     }
     
+    // Een route updaten
     function update($route) {
         //Html entities en extra spaties verwijderen
         $route = escape_html($route);
@@ -80,11 +85,13 @@ class Routes_model extends CI_Model {
         $this->db->update('route', $route);
     }
 
+    // Een route verwijderen
     function delete($id) {
         $this->db->where('id', $id);
         $this->db->delete('route');
     }
     
+    // Een nieuwe route toevoegen
     function insert($route)
     {
         //Html entities en extra spaties verwijderen
