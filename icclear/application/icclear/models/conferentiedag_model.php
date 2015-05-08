2276600a34a109d0dc7a07f80156a163ed1ef997
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 18:52:38 +0000

    Commentaarlijnen

--- icclear/application/icclear/models/conferentiedag_model.php
+++ icclear/application/icclear/models/conferentiedag_model.php
@@ -2,39 +2,33 @@
 
 class Conferentiedag_model extends CI_Model {
 
-    // +----------------------------------------------------------
-    // | Beershop - product_model
-    // +----------------------------------------------------------
-    // | Thomas More Kempen - 2 TI - 201x-201x
-    // +----------------------------------------------------------
-    // | Product model
-    // |
-    // +----------------------------------------------------------
-    // | K. Vangeel
-    // +----------------------------------------------------------
 
     function __construct() {
         parent::__construct();
     }
 
+    // Alle conferentiedagen ophalen
     function getAll() {
         $this->db->order_by('datum');
         $query = $this->db->get('conferentiedag');
         return $query->result();
     }
 
+    // Een conferentiedag ophalen
     function get($id) {
         $this->db->where('id', $id);
         $query = $this->db->get('conferentiedag');
         return $query->row();
     }
     
+    // Alle conferentiedagen van eenzelfde conferentie ophalen
     function getFromConferentie($conferentieId) {
         $this->db->where('conferentieId', $conferentieId);
         $query = $this->db->get('conferentiedag');
         return $query->result();
     }
     
+    // Voor elke conferentiedag het gebouw zoeken dat eraan is toegewezen
     function getGebouwenByDag(){
         $query = $this->db->get('conferentiedag');
         $dagen = $query->result();
@@ -48,12 +42,14 @@ class Conferentiedag_model extends CI_Model {
         return $dagen;
     }
     
+    // Een nieuwe conferentiedag toevoegen
     function insert($conferentiedag)
     {
         $this->db->insert('conferentiedag', $conferentiedag);
         return $this->db->insert_id();
     }
     
+    // Een conferentiedag verwijderen
     function update($conferentiedag)
     {        
         $this->db->where('id', $conferentiedag->id);
