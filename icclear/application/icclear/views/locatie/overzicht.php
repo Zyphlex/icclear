aa2748b9e0c292cba7a7875aeace30cb6bc4b7ac
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 17:12:39 +0000

    routes

--- icclear/application/icclear/views/locatie/overzicht.php
+++ icclear/application/icclear/views/locatie/overzicht.php
@@ -76,7 +76,7 @@
             </div>
             
              <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Sluiten</button>
+                <button type="button" class="col-sm-12 btn btn-default" data-dismiss="modal">Sluiten</button>
             </div>
             
         </div>            
