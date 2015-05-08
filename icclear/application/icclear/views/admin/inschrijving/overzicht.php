ceb50d8080d66d065203f5c6799ebf891f825507
ab_ikrou <ab_ikrou@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 12:27:17 +0000

    update inschrijving met en zonder betaalId

--- icclear/application/icclear/views/admin/inschrijving/overzicht.php
+++ icclear/application/icclear/views/admin/inschrijving/overzicht.php
@@ -40,6 +40,7 @@
                     async: false,
                     data: {id: iddb},
                     success: function (result) {
+                        alert(result);
                         var jobject = jQuery.parseJSON(result);
                         $("#gebruiker").val(jobject.gebruikerId);
                         $("#confonderdeel").val(jobject.conferentieOnderdeelId);
