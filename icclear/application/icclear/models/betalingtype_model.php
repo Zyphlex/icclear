942e3511344cd7b77dfe499e3b0a9bd05d008179
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 19:00:43 +0000

    Commentaarlijnen

--- icclear/application/icclear/models/betalingtype_model.php
+++ icclear/application/icclear/models/betalingtype_model.php
@@ -7,12 +7,14 @@ class Betalingtype_model extends CI_Model {
         parent::__construct();
     }
     
+    // Een betalingtype ophalen
     function get($id) {
         $this->db->where('id', $id);
         $query = $this->db->get('betalingtype');
         return $query->row();
     }
             
+    // Alle betalingtypes ophalen
     function getAll()
     {        
         $this->db->order_by('omschrijving');
