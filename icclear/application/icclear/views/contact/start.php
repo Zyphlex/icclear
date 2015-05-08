1583a1343b9ca6507163c6cde2aae7018dc50dae
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 15:14:52 +0000

    CONTACT pagina maken

--- icclear/application/icclear/views/contact/start.php
+++ icclear/application/icclear/views/contact/start.php
@@ -6,18 +6,27 @@
 
 <div class="row">
     <div class="col-lg-offset-1">
-    <form action="contact/insturen" method="post">        
-        <p><?php echo form_label('Uw emailadres:', 'emailadresverzender'); ?></p>
-        <p><?php echo form_input(array('name' => 'emailadresverzender', 'id' => 'emailadresverzender', 'class' => 'form-control', 'required' => 'required')); ?></p>
-        
-        <p><?php echo form_label('Onderwerp:', 'onderwerpverzender'); ?></p>
-        <p><?php echo form_input(array('name' => 'onderwerpverzender', 'id' => 'onderwerpverzender', 'class' => 'form-control', 'required' => 'required')); ?></p>
-        
-        <p><?php echo form_label('Boodschap/vraag:', 'boodschapcontact'); ?></p>
-        <p><?php echo form_textarea(array('name' => 'boodschapcontact', 'id' => 'boodschapcontact', 'rows' => '10', 'cols' => '50', 'class' => 'form-control', 'required' => 'required')); ?></p>
-        <p><input type="submit" value="Verzenden" class="btn btn-default"/></p>
-    </form>
-        </div>
+        <p>ICClear Brussel</p>
+        <p>014/404040</p>
+        <p>Nieuwstraat 1</p>
+        <p>1000 Brussel</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-offset-1">
+        <form action="contact/insturen" method="post">        
+            <p><?php echo form_label('Uw emailadres:', 'emailadresverzender'); ?></p>
+            <p><?php echo form_input(array('name' => 'emailadresverzender', 'id' => 'emailadresverzender', 'class' => 'form-control', 'required' => 'required')); ?></p>
+
+            <p><?php echo form_label('Onderwerp:', 'onderwerpverzender'); ?></p>
+            <p><?php echo form_input(array('name' => 'onderwerpverzender', 'id' => 'onderwerpverzender', 'class' => 'form-control', 'required' => 'required')); ?></p>
+
+            <p><?php echo form_label('Boodschap/vraag:', 'boodschapcontact'); ?></p>
+            <p><?php echo form_textarea(array('name' => 'boodschapcontact', 'id' => 'boodschapcontact', 'rows' => '10', 'cols' => '50', 'class' => 'form-control', 'required' => 'required')); ?></p>
+            <p><input type="submit" value="Verzenden" class="btn btn-default"/></p>
+        </form>
+    </div>
 </div>
 
 
