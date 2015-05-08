7431a6d9e0b0a3eb77447af50d31b532586b1857
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 16:12:20 +0000

    contact

--- icclear/application/icclear/views/contact/start.php
+++ icclear/application/icclear/views/contact/start.php
@@ -20,23 +20,23 @@
 
     <div class="col-sm-6">
         <h1 class="underline">Neem contact op met ons</h1>
-        <div class="col-md-8">            
+        
             <?php
             $attributes = array('name' => 'myform');
             echo form_open('contact/insturen', $attributes);
             ?>    
                 <p><?php echo form_label('Uw emailadres:', 'emailadresverzender'); ?></p>
-                <p><?php echo form_input(array('name' => 'emailadresverzender', 'id' => 'emailadresverzender', 'class' => 'form-control', 'required' => 'required')); ?></p>
+                <p class="col-sm-8"><?php echo form_input(array('name' => 'emailadresverzender', 'id' => 'emailadresverzender', 'class' => 'form-control', 'required' => 'required')); ?></p>
 
                 <p><?php echo form_label('Onderwerp:', 'onderwerpverzender'); ?></p>
-                <p><?php echo form_input(array('name' => 'onderwerpverzender', 'id' => 'onderwerpverzender', 'class' => 'form-control', 'required' => 'required')); ?></p>
+                <p class="col-sm-8"><?php echo form_input(array('name' => 'onderwerpverzender', 'id' => 'onderwerpverzender', 'class' => 'form-control', 'required' => 'required')); ?></p>
 
                 <p><?php echo form_label('Boodschap/vraag:', 'boodschapcontact'); ?></p>
-                <p><?php echo form_textarea(array('name' => 'boodschapcontact', 'id' => 'boodschapcontact', 'rows' => '10', 'cols' => '50', 'class' => 'form-control', 'required' => 'required')); ?></p>
+                <p class="col-sm-12"><?php echo form_textarea(array('name' => 'boodschapcontact', 'id' => 'boodschapcontact', 'rows' => '10', 'cols' => '50', 'class' => 'form-control', 'required' => 'required')); ?></p>
                 <?php echo anchor('home','Annuleren','class="col-xs-4 btn btn-default"') ?>
                 <?php echo form_submit('mysubmit', 'Bericht versturen', 'class="col-xs-8 btn btn-primary"'); ?>        
             <?php echo form_close(); ?>
-        </div>
+        
     </div>
 </div>
 
