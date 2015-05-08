acf280c2393582a60336dd66b74a01d83ff197b1
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 16:30:13 +0000

    contact

--- icclear/application/icclear/views/contact/start.php
+++ icclear/application/icclear/views/contact/start.php
@@ -13,9 +13,9 @@
                 <p>014 / 50 60 70</p>
             </div>
             
-            <div class="col-sm-10">
+            <div class="col-sm-12">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5004.388727366072!2d4.960560861577568!3d51.16020604258717!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c14c06d6fc9923%3A0x3632599b5a446df0!2sKleinhoefstraat+4%2C+2440+Geel%2C+Belgium!5e0!3m2!1sen!2s!4v1431101776023" 
-                        width="100%" height="auto" class="img-responsive" frameborder="0" style="border:0">                        
+                        width="100%" height="100%" class="img-responsive" frameborder="0" style="border:0">                        
                 </iframe>
             </div>
 
