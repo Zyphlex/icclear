065c620069320bda3ecdc3361f5ab84c40291eda
le_milants <le_milants@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 17:40:41 +0000

    Commentaarlijnen

--- icclear/application/icclear/models/voorkeur_model.php
+++ icclear/application/icclear/models/voorkeur_model.php
@@ -5,7 +5,8 @@ class Voorkeur_model extends CI_Model {
     function __construct() {
         parent::__construct();
     }
-            
+    
+    // Voorkeur naar database uploaden
     function insert($voorkeur)
     {
         //Html entities en extra spaties verwijderen
