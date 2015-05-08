2025e6d79a312b5ec45be81c0ca9ed1bff9acac9
ro_oosthoek <ro_oosthoek@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 16:17:13 +0000

    contact

--- icclear/application/icclear/views/contact/start.php
+++ icclear/application/icclear/views/contact/start.php
@@ -1,19 +1,15 @@
 <div class="row">
     <div class="col-sm-6">
-        <div class="col-md-8">
-            <h1>Contactpagina</h1>
-        </div>
+            <h1>Contactgegevens</h1>
 
-
-        <div class="col-md-8">
-            <h3>Adres:</h3>
-        </div>
-        <div class="col-md-8">
             <p>ICClear Brussel</p>
             <p>014/404040</p>
             <p>Nieuwstraat 1</p>
             <p>1000 Brussel</p>
-        </div>
+            
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5004.388727366072!2d4.960560861577568!3d51.16020604258717!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c14c06d6fc9923%3A0x3632599b5a446df0!2sKleinhoefstraat+4%2C+2440+Geel%2C+Belgium!5e0!3m2!1sen!2s!4v1431101776023" 
+                    width="600" height="450" frameborder="0" style="border:0">                        
+            </iframe>
 
 
     </div>
