a090e0319e19d65d8498f63bbc718b68846bf1a7
fr_vanhooghten <fr_vanhooghten@f75b6fcf-d013-4b5a-90ae-7ce3f290f795>
2015-05-08 15:00:53 +0000

    CONTACT pagina maken

--- icclear/application/icclear/controllers/contact.php
+++ icclear/application/icclear/controllers/contact.php
@@ -49,6 +49,30 @@ class Contact extends CI_Controller {
         $this->email->message($boodschap);
         $this->email->send();
         
+         $user = $this->authex->getUserInfo();
+        $data['user'] = $user;
+        if($user == null){
+            $data['user'] = '';
+        }
+        $data['title'] = 'IC Clear - Succesvol';
+        $data['active'] = 'home';
+
+        $this->load->model('conferentie_model');
+        $data['conferentie'] = $this->conferentie_model->getActieveConferentie();
+        
+        //Kijken of user reeds is ingeschreven, als dit zo is, knop verbergen op view
+        $this->load->model('inschrijving_model');
+        if ($user == null) {
+            $data['inschrijving'] = null;
+        } else {
+            $inschrijving = $this->inschrijving_model->IsGebruikerIngeschreven($user->id);
+            if ($inschrijving == null) {
+                $data['inschrijving'] = null;
+            } else {
+                $data['inschrijving'] = $inschrijving;
+            }
+        }
+                
         $partials = array('header' => 'main_header', 'nav' => 'main_nav', 'content' => 'contact/succes', 'footer' => 'main_footer');
         $this->template->load('main_master', $partials, $data);
     }
